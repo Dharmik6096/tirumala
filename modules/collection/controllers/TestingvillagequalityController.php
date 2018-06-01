71531d70c5b93f66440a47df19782242f47b1f2d
Hardik <hardik@desktop>
2018-06-01 16:10:50 +0530

    remove behaviours form village quality and village weight

--- modules/collection/controllers/TestingvillagequalityController.php
+++ modules/collection/controllers/TestingvillagequalityController.php
@@ -15,21 +15,6 @@ use yii\filters\VerbFilter;
 class TestingvillagequalityController extends \app\controllers\ChildController
 {
     /**
-     * @inheritdoc
-     */
-    public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['POST'],
-                ],
-            ],
-        ];
-    }
-
-    /**
      * Lists all Testingvillagequality models.
      * @return mixed
      */
--- modules/collection/controllers/TestingvillageweightController.php
+++ modules/collection/controllers/TestingvillageweightController.php
@@ -15,21 +15,6 @@ use yii\filters\VerbFilter;
 class TestingvillageweightController extends \app\controllers\ChildController
 {
     /**
-     * @inheritdoc
-     */
-    public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['POST'],
-                ],
-            ],
-        ];
-    }
-
-    /**
      * Lists all Testingvillageweight models.
      * @return mixed
      */
