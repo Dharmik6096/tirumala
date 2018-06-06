8d257a812d2f25f38a7e5c56ce3d345d1bc32c14
Hardik <hardik@desktop>
2018-06-06 18:23:03 +0530

    collection completed widget separate from society widgets

--- modules/webservice/ho/v2/controllers/WidgetController.php
+++ modules/webservice/ho/v2/controllers/WidgetController.php
@@ -23,10 +23,6 @@ class WidgetController extends ChildController {
         $response['society_collection'] = $this->getSpData($sp, $param);
         $sp = 'sp_app_ho_widget_member_diff';
         $response['member_diff'] = $this->getSpData($sp, $param);
-        $sp = 'sp_app_ho_collection_vs_completed';
-        $param[0] = (date('H:i:s') <= '16:00:00') ? date('Y-m-d 06:00:00') : date('Y-m-d 18:00:00');
-        unset($param[1]);
-        $response['collection_completed'] = $this->getSpData($sp, $param);
         return $this->response['data'] = $response;
     }
 
@@ -46,6 +42,21 @@ class WidgetController extends ChildController {
         $response['bmc_collection'] = $this->getSpData($sp, $param);
         $sp = 'sp_app_ho_widget_society_diff';
         $response['society_diff'] = $this->getSpData($sp, $param);
+        return $this->response['data'] = $response;
+    }
+    
+    public function actionCollectionCompleted() {
+        $param = [];
+        $response = [];
+        $data = $this->getOrgCodes($this->post_data);
+        $param[] = (date('H:i:s') <= '16:00:00') ? date('Y-m-d 06:00:00') : date('Y-m-d 18:00:00');
+        $param[] = !empty($data['union']) ? ',' . implode(',', $data['union']) . ',' : 0;
+        $param[] = !empty($data['plant']) ? ',' . implode(',', $data['plant']) . ',' : 0;
+        $param[] = !empty($data['mcc']) ? ',' . implode(',', $data['mcc']) . ',' : 0;
+        $param[] = !empty($data['bmc']) ? ',' . implode(',', $data['bmc']) . ',' : 0;
+        $param[] = !empty($data['dcs']) ? ',' . implode(',', $data['dcs']) . ',' : 0;
+        $sp = 'sp_app_ho_collection_vs_completed';
+        $response[] = $this->getSpData($sp, $param);
         return $this->response['data'] = $response;
     }
 
