e21b42d6b108690f39dba1b45879357b20771a04
Hardik <hardik@desktop>
2018-06-12 10:01:00 +0530

    comparision and weekly collection widget for society and bmc in ho

--- modules/webservice/ho/v2/controllers/WidgetController.php
+++ modules/webservice/ho/v2/controllers/WidgetController.php
@@ -58,5 +58,81 @@ class WidgetController extends ChildController {
         $sp = 'sp_app_ho_collection_vs_completed';
         return $this->response['data'] = $this->getSpData($sp, $param);
     }
+    
+    public function actionSocietyComparision(){
+        $param = [];
+        $response = [];
+        $data = $this->getOrgCodes($this->post_data);
+        $to_date1 = date('Y-m-d');
+        $from_date1 = date('Y-m-d', strtotime("-6 day", strtotime($to_date1)));
+        $to_date2 = date('Y-m-d', strtotime("-1 day", strtotime($from_date1)));
+        $from_date2 = date('Y-m-d', strtotime("-6 day", strtotime($to_date2)));
+        $param[] = $from_date1;
+        $param[] = $to_date1;
+        $param[] = $from_date2;
+        $param[] = $to_date2;
+        $param[] = !empty($data['union']) ? ',' . implode(',', $data['union']) . ',' : 0;
+        $param[] = !empty($data['plant']) ? ',' . implode(',', $data['plant']) . ',' : 0;
+        $param[] = !empty($data['mcc']) ? ',' . implode(',', $data['mcc']) . ',' : 0;
+        $param[] = !empty($data['bmc']) ? ',' . implode(',', $data['bmc']) . ',' : 0;
+        $param[] = !empty($data['dcs']) ? ',' . implode(',', $data['dcs']) . ',' : 0;
+        $sp = 'sp_app_ho_widget_society_comparison';
+        return $this->response['data'] = $this->getSpData($sp, $param);
+    }
+    
+    public function actionBmcComparision(){
+        $param = [];
+        $response = [];
+        $data = $this->getOrgCodes($this->post_data);
+        $to_date1 = date('Y-m-d');
+        $from_date1 = date('Y-m-d', strtotime("-6 day", strtotime($to_date1)));
+        $to_date2 = date('Y-m-d', strtotime("-1 day", strtotime($from_date1)));
+        $from_date2 = date('Y-m-d', strtotime("-6 day", strtotime($to_date2)));
+        $param[] = $from_date1;
+        $param[] = $to_date1;
+        $param[] = $from_date2;
+        $param[] = $to_date2;
+        $param[] = !empty($data['union']) ? ',' . implode(',', $data['union']) . ',' : 0;
+        $param[] = !empty($data['plant']) ? ',' . implode(',', $data['plant']) . ',' : 0;
+        $param[] = !empty($data['mcc']) ? ',' . implode(',', $data['mcc']) . ',' : 0;
+        $param[] = !empty($data['bmc']) ? ',' . implode(',', $data['bmc']) . ',' : 0;
+        $param[] = !empty($data['dcs']) ? ',' . implode(',', $data['dcs']) . ',' : 0;
+        $sp = 'sp_app_ho_widget_bmc_comparison';
+        return $this->response['data'] = $this->getSpData($sp, $param);
+    }
+    
+    public function actionSocietyWeeklyCollection(){
+        $param = [];
+        $response = [];
+        $data = $this->getOrgCodes($this->post_data);
+        $to_date = date('Y-m-d');
+        $from_date = date('Y-m-d', strtotime("-6 day", strtotime($to_date)));
+        $param[] = $from_date;
+        $param[] = $to_date;
+        $param[] = !empty($data['union']) ? ',' . implode(',', $data['union']) . ',' : 0;
+        $param[] = !empty($data['plant']) ? ',' . implode(',', $data['plant']) . ',' : 0;
+        $param[] = !empty($data['mcc']) ? ',' . implode(',', $data['mcc']) . ',' : 0;
+        $param[] = !empty($data['bmc']) ? ',' . implode(',', $data['bmc']) . ',' : 0;
+        $param[] = !empty($data['dcs']) ? ',' . implode(',', $data['dcs']) . ',' : 0;
+        $sp = 'sp_app_ho_widget_society_weekly_collection';
+        return $this->response['data'] = $this->getSpData($sp, $param);
+    }
+    
+    public function actionBmcWeeklyCollection(){
+        $param = [];
+        $response = [];
+        $data = $this->getOrgCodes($this->post_data);
+        $to_date = date('Y-m-d');
+        $from_date = date('Y-m-d', strtotime("-6 day", strtotime($to_date)));
+        $param[] = $from_date;
+        $param[] = $to_date;
+        $param[] = !empty($data['union']) ? ',' . implode(',', $data['union']) . ',' : 0;
+        $param[] = !empty($data['plant']) ? ',' . implode(',', $data['plant']) . ',' : 0;
+        $param[] = !empty($data['mcc']) ? ',' . implode(',', $data['mcc']) . ',' : 0;
+        $param[] = !empty($data['bmc']) ? ',' . implode(',', $data['bmc']) . ',' : 0;
+        $param[] = !empty($data['dcs']) ? ',' . implode(',', $data['dcs']) . ',' : 0;
+        $sp = 'sp_app_ho_widget_bmc_weekly_collection';
+        return $this->response['data'] = $this->getSpData($sp, $param);
+    }
 
 }
