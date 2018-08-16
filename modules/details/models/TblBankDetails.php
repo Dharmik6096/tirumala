538b9e9b7801fe6545344c5444127857e3bc44c4
IT25 (IT/RO) <it25@EIPL.LOCAL>
2018-08-16 16:50:14 +0530

    banck account check unique on active

--- modules/details/models/TblBankDetails.php
+++ modules/details/models/TblBankDetails.php
@@ -5,6 +5,7 @@ namespace app\modules\details\models;
 use Yii;
 use app\modules\organisation\models\TblBanks;
 use app\modules\organisation\models\TblBranch;
+
 /**
  * This is the model class for table "tbl_bank_details".
  *
@@ -24,41 +25,40 @@ use app\modules\organisation\models\TblBranch;
  * @property TblBanks $bankCode
  * @property TblBranch $branchCode
  */
-class TblBankDetails extends \app\models\ChildModel
-{
+class TblBankDetails extends \app\models\ChildModel {
+
     /**
      * @inheritdoc
      */
-    public static function tableName()
-    {
+    public static function tableName() {
         return 'tbl_bank_details';
     }
 
     /**
      * @inheritdoc
      */
-    public function rules()
-    {
+    public function rules() {
         return [
-            [['branch_code', 'bank_account_no', 'ifsc'], 'required','on'=>'bank_selected'],
-            [['branch_code', 'bank_account_no', 'ifsc','bank_code'], 'required','on'=>'additional'],
-            /*[['branch_code', 'bank_account_no', 'ifsc'], 'required','when' => function($model) {
-                    return !empty($this->bank_code)?true:false;
-            }, 'whenClient' => "function (attribute, value) { return $('#tblbankdetails-bank_code').val()!==''}"],*/
+            [['branch_code', 'bank_account_no', 'ifsc'], 'required', 'on' => 'bank_selected'],
+            [['branch_code', 'bank_account_no', 'ifsc', 'bank_code'], 'required', 'on' => 'additional'],
+            /* [['branch_code', 'bank_account_no', 'ifsc'], 'required','when' => function($model) {
+              return !empty($this->bank_code)?true:false;
+              }, 'whenClient' => "function (attribute, value) { return $('#tblbankdetails-bank_code').val()!==''}"], */
             [['detail_code'], 'integer'],
             [['module_name', 'module_code', 'bank_code', 'branch_code', 'bank_account_no', 'ifsc', 'created_by', 'updated_by'], 'string'],
-            [['created_at', 'updated_at','is_default','is_active'], 'safe'],
-            ['bank_account_no', 'unique', 'targetAttribute' => ['bank_account_no','ifsc'],'message'=> Yii::t('app/validation', '{attribute} has already been taken.'), 'when' => function($model) {
-                    return $model->module_name==$this->module_name;
-            }],
+            [['created_at', 'updated_at', 'is_default', 'is_active'], 'safe'],
+//            ['bank_account_no', 'unique', 'targetAttribute' => ['bank_account_no', 'ifsc'], 'message' => Yii::t('app/validation', '{attribute} has already been taken.'), 'when' => function($model) {
+//            return $model->module_name == $this->module_name;
+//        }],
+            [['bank_account_no'], 'CheckDuplicate'],
             [['ifsc'], function ($attribute, $params) {
-                    Yii::$app->general->validateIfsc($this, $attribute,$params);
-                },'skipOnEmpty'=> true],
+            Yii::$app->general->validateIfsc($this, $attribute, $params);
+        }, 'skipOnEmpty' => true],
             [['bank_account_no'], function ($attribute, $params) {
-                    $error = TblBanks::validateAccountNo($this->bank_code, $this->$attribute);
-                    if ($error !== TRUE)
-                        $this->addError($attribute, $error);
-                }],
+            $error = TblBanks::validateAccountNo($this->bank_code, $this->$attribute);
+            if ($error !== TRUE)
+                $this->addError($attribute, $error);
+        }],
             [['bank_code'], 'exist', 'skipOnError' => true, 'targetClass' => TblBanks::className(), 'targetAttribute' => ['bank_code' => 'bank_code']],
             [['branch_code'], 'exist', 'skipOnError' => true, 'targetClass' => TblBranch::className(), 'targetAttribute' => ['branch_code' => 'branch_code']],
         ];
@@ -67,8 +67,7 @@ class TblBankDetails extends \app\models\ChildModel
     /**
      * @inheritdoc
      */
-    public function attributeLabels()
-    {
+    public function attributeLabels() {
         return [
             'detail_code' => Yii::t('app', 'Detail Code'),
             'module_name' => Yii::t('app', 'Module Name'),
@@ -89,16 +88,14 @@ class TblBankDetails extends \app\models\ChildModel
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getBankCode()
-    {
+    public function getBankCode() {
         return $this->hasOne(TblBanks::className(), ['bank_code' => 'bank_code']);
     }
 
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getBranchCode()
-    {
+    public function getBranchCode() {
         return $this->hasOne(TblBranch::className(), ['branch_code' => 'branch_code']);
     }
 
@@ -106,18 +103,28 @@ class TblBankDetails extends \app\models\ChildModel
      * @inheritdoc
      * @return TblBankDetailsQuery the active query used by this AR class.
      */
-    public static function find()
-    {
+    public static function find() {
         return new TblBankDetailsQuery(get_called_class());
     }
-    
-    public function setModel($module, $module_code,$default=1)
-    {
-        $this->module_name=$module;
-        $this->module_code=$module_code;
-        $this->detail_code= Yii::$app->general->getCodeAutoIncrement($this);
-        $this->is_active=1;
-        $this->is_default=$default;
+
+    public function setModel($module, $module_code, $default = 1) {
+        $this->module_name = $module;
+        $this->module_code = $module_code;
+        $this->detail_code = Yii::$app->general->getCodeAutoIncrement($this);
+        $this->is_active = 1;
+        $this->is_default = $default;
     }
-    
+
+    public function CheckDuplicate($attribute, $param) {
+        if (!empty($this->bank_account_no)) {
+            $data = $this->find()->where(['or', ['bank_account_no' => $this->bank_account_no], ['bank_account_no' => \Yii::$app->general->encryptData($this->bank_account_no)]])
+                            ->where(['or', ['ifsc' => $this->ifsc], ['ifsc' => \Yii::$app->general->encryptData($this->ifsc)]])
+                            ->andWhere(['<>', 'detail_code', $this->detail_code])
+                            ->andWhere(['is_active' => 1])->one();
+            if (!empty($data)) {
+                $this->addError($attribute, Yii::t('app/validation', 'Bank Account No has already been taken.'));
+            }
+        }
+    }
+
 }
