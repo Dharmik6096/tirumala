f052de92323e64c0ad4df4017332c7d41a0ef8eb
unknown <it20@IT20.EIPL.LOCAL>
2018-08-31 10:03:05 +0530

    change webservice supervisor instead of tirumala

--- themes/pcdf/layouts/templates/tpl_navigation.php
+++ themes/pcdf/layouts/templates/tpl_navigation.php
@@ -132,7 +132,7 @@ echo GhostMenu::widget([
 //                        ['label' => '115-' . Yii::t('app', 'Payment Authorization'), 'url' => ['/jasperreports/default/payment-authorization']],
                         ['label' => '116-' . Yii::t('app', 'Society Wise Collection vs Dispatch - Graph'), 'url' => ['/jasperreports/default/dcs-collection-vs-dispatch-graph']],
                         ['label' => '117-' . Yii::t('app', 'Society Details'), 'url' => ['/jasperreports/default/society-details']],
-                    ]
+                        ]
                 ],
                 [
                     'options' => ['class' => 'dropdown-submenu'],
