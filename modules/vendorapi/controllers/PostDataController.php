c6cb8e52dc5d70d68208cdcf010edc464a7b6e5a
IT25 (IT/RO) <it25@EIPL.LOCAL>
2018-10-16 18:18:30 +0530

    namste api call header set

--- modules/vendorapi/controllers/PostDataController.php
+++ modules/vendorapi/controllers/PostDataController.php
@@ -45,9 +45,13 @@ class PostDataController extends \yii\web\Controller {
         $api = new WebApi();
         $api->serverUrl = Yii::$app->params['namaste_collection_url'];
         $api->authentication = FALSE;
-        $api->vendor_code='NAMASTEINDIA';
+        $api->vendor_code = 'NAMASTEINDIA';
         $api->body = $body;
-        $response=$api->POSTDATA();
+        $header_array = Yii::$app->params['namaste_api_header'];
+        foreach ($header_array as $key => $value) {
+            $api->header_info[] = $key . ': ' . $value;
+        }
+        $response = $api->POSTDATA();
         var_dump($response);
     }
 
