b653695eeeadc139e1ccacb295835899a5150229
Hardik <hardik@everestinstruments.com>
2019-01-30 17:35:34 +0530

    change label

--- themes/pcdf/layouts/templates/tpl_navigation.php
+++ themes/pcdf/layouts/templates/tpl_navigation.php
@@ -266,7 +266,7 @@ echo GhostMenu::widget([
                         ['label' => Yii::t('app', 'SAP Data Export'), 'url' => ['/misreports/default/sap-report'], 'active' => ($cntrl == 'default' && $action == 'sap-report')],
 //                        ['label' => 'Member Credit', 'url' => ['/payment/tbl-member-credit-limit/index']],
                         ['label' => Yii::t('app', 'Company Configuration '), 'url' => ['/configuration/tbl-milk-collection-config/tabs']],
-                        ['label' => Yii::t('app', 'vendorapi'), 'url' => ['/vendorapi/tbl-vendor-api-data/index'], 'active' => ($cntrl == 'tbl-vendor-api-data')],
+                        ['label' => Yii::t('app', 'SAP Data'), 'url' => ['/vendorapi/tbl-vendor-api-data/index'], 'active' => ($cntrl == 'tbl-vendor-api-data')],
                     ]
                 ],
                 [
