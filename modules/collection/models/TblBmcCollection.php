d878b928d3c593619734d8371d5e92a97597a86a
Hardik <hardik@everestinstruments.com>
2019-03-18 15:51:29 +0530

    rexolve issue for send sap data

--- modules/collection/models/TblBmcCollection.php
+++ modules/collection/models/TblBmcCollection.php
@@ -74,13 +74,13 @@ class TblBmcCollection extends \app\models\ChildModel {
             return $model->collection_type == '2';
         }, 'whenClient' => "function (attribute, value) { 
               return $('#tblbmccollection-collection_type').val() == '2'; 
-          }"],
-            [['dcs_code'], 'validateDcs'],
+          }", 'except' => ['post_sap_data']],
+            [['dcs_code'], 'validateDcs', 'except' => ['post_sap_data']],
             [['dcs_code'], 'unique', 'targetAttribute' => ['date_time_of_collection', 'shift_code', 'dcs_code', 'sample_no'], 'skipOnEmpty' => true, 'message' => Yii::t('app/validation', '{attribute} has already been taken.'), 'when' => function() {
             return $this->shift_code;
         }],
             [['collection_type'], 'default', 'value' => 1, 'on' => ['saveCreamyData', 'saveSapData']],
-            [['fat', 'snf', 'rtpl', 'qty', 'dcs_code', 'shift_code', 'milk_type_code', 'date_time_of_collection', 'milk_quality_type_code'], 'required', 'except' => 'saveSapData'],
+            [['fat', 'snf', 'rtpl', 'qty', 'dcs_code', 'shift_code', 'milk_type_code', 'date_time_of_collection', 'milk_quality_type_code'], 'required', 'except' => ['saveSapData', 'post_sap_data']],
             [['date_time_of_collection', 'date_time_of_recieve', 'dt_date', 'sms_timestamp', 'transporter_code', 'vehicle_code', 'collection_type', 'date', 'weigh_time', 'testing_time', 'bmc_code', 'data_post_id', 'data_post_status', 'picked_datetime', 'resp_status', 'resp_desc'], 'safe'],
             [['density', 'clr', 'lactose', 'protein', 'qlty_auto', 'qty_mode', 'qty_auto', 'no_of_can', 'avg_qlty_param', 'qlty_time', 'qlty_times_no', 'qty_time', 'date_time_of_testing', 'converted_qty'], 'safe'],
         ];
--- modules/vendorapi/controllers/PostDataController.php
+++ modules/vendorapi/controllers/PostDataController.php
@@ -11,8 +11,9 @@ use app\components\WebApi;
 class PostDataController extends \yii\web\Controller {
 
     public function actionIndex() {
-		$data = $this->setDataKey();
+        $data = $this->setDataKey();
         foreach ($data as $key => $value) {
+            $body = [];
             $key = $value['key'];
             $sp_name = $value['sp_name'];
             $json_array_key = $value['json_array_key'];
@@ -23,7 +24,7 @@ class PostDataController extends \yii\web\Controller {
             $sp_param[] = $start_date;
             $sp_param[] = $end_date;
             $output = \Yii::$app->general->getSpData($sp_name, $sp_param);
-			if (!empty($output)) {
+            if (!empty($output)) {
                 $modelName = $value['model_name'];
                 $model_name = Yii::$app->path->define($modelName);
                 $model = new $model_name();
@@ -35,7 +36,7 @@ class PostDataController extends \yii\web\Controller {
                 $model->updateAll(['data_post_status' => 1, 'picked_datetime' => date('Y-m-d H:i:s')], [$modelKey => $update_ids]);
                 $body[$json_array_key] = $output;
                 $body = json_encode($body);
-				//var_dump($body);die;
+                //var_dump($body);die;
 //            $body['milkcollection'][] = ['uuid' => '004fdcee-58c7-4ef9-bc53-6c7992240676',
 //            'transaction_date' => '19.09.2018',
 //            'shift' => 'M',
@@ -62,18 +63,20 @@ class PostDataController extends \yii\web\Controller {
                 }
                 $response = $api->POSTDATA();
 //                $response = '{"MCC_Milk_Collection_Response":{"MCC_Milk_Collection":[{"uuid":"239d4c6c-d0e7-4ffb-aed5-0484006cdd36","status":"SUCCESS","desc":"GR Successful"},{"uuid":"166470cb-9211-41bb-832a-d10cb8dcd391","status":"ERROR","desc":"GR Successful"}]}}';
-                $responseData = json_decode($response);
+                // echo '<pre/>';print_r($response);die;
+                // $responseData = json_decode($response, true, 512, JSON_OBJECT_AS_ARRAY);
+                $responseData = json_decode(json_encode($response), true);
                 foreach ($responseData as $resp_data) {
                     foreach ($resp_data as $resp) {
                         foreach ($resp as $key => $value) {
-                            $status = (isset($value->status) && strtolower($value->status) == 'success') ? 2 : 3;
+                            $status = (isset($value['status']) && strtolower($value['status']) == 'success') ? 2 : 3;
                             $model = new $model_name();
-                            $model = $model->find()->where([$modelKey => $value->$sapKey])->one();
+                            $model = $model->find()->where([$modelKey => $value[$sapKey]])->one();
                             if (!empty($model)) {
                                 $model->scenario = 'post_sap_data';
                                 $model->data_post_status = $status;
-                                $model->resp_status = !empty($value->status) ? $value->status : NULL;
-                                $model->resp_desc = !empty($value->desc) ? $value->desc : NULL;
+                                $model->resp_status = !empty($value['status']) ? $value['status'] : NULL;
+                                $model->resp_desc = !empty($value['desc']) ? $value['desc'] : NULL;
                                 $model->save();
                             }
                         }
@@ -85,13 +88,13 @@ class PostDataController extends \yii\web\Controller {
 
     public function setDataKey() {
         $data = [
-            /*'milkCollection' => [
-              'key' => 'MilkCollectionData',
-              'sp_name' => 'sp_vendor_milk_coll_data',
-              'json_array_key' => 'milkcollection',
-              'model_name' => 'TblMilkCollection',
-              'update_key' => 'uuid:data_post_id'
-              ],*/
+            'milkCollection' => [
+                'key' => 'MilkCollectionData',
+                'sp_name' => 'sp_vendor_milk_coll_data',
+                'json_array_key' => 'milkcollection',
+                'model_name' => 'TblMilkCollection',
+                'update_key' => 'uuid:data_post_id'
+            ],
             'bmcCollection' => [
                 'key' => 'MCCMilkCollection',
                 'sp_name' => 'sp_vendor_bmc_coll_data',
