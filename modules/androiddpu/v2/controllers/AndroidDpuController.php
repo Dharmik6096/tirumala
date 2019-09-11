4b3f06fab326ae2483c353a88f3f827666786843
Asmita (IT/RO) <it25@EIPL.LOCAL>
2019-09-11 10:24:36 +0530

    seprate animal type and dpu time config

--- modules/androiddpu/v2/controllers/AndroidDpuController.php
+++ modules/androiddpu/v2/controllers/AndroidDpuController.php
@@ -251,12 +251,12 @@ class AndroidDpuController extends \app\modules\androiddpu\v1\controllers\Androi
                         $res_data['collectionConfig']['e_lock_time'] = $att['e_lock_time'];
                         $res_data['collectionConfig']['inc_rate'] = $att['inc_rate'];
                         $res_data['collectionConfig']['inc_deduction'] = $att['inc_deduction'];
-                        $animalType = [];
-                        foreach ($model_data->tblDcsMilkType as $milktype) {
-                            $animalType[] = ['milk_type_code' => $milktype->milk_type_code, 'milk_type_name' => $milktype->milkTypeCode->animal_type_name];
-                        }
-                        $res_data['collectionConfig']['allowedMilkType'] = $animalType;
                     }
+                    $animalType = [];
+                    foreach ($model_data->tblDcsMilkType as $milktype) {
+                        $animalType[] = ['milk_type_code' => $milktype->milk_type_code, 'milk_type_name' => $milktype->milkTypeCode->animal_type_name];
+                    }
+                    $res_data['collectionConfig']['allowedMilkType'] = $animalType;
                 }
             } else if ($org_type == 'BMC') {
                 $model = new TblDcsBmc();
