5ce720a38a97e5eb659f0e121cdf48871f7122a8
Hardik (IT/RO) <it23@EIPL.LOCAL>
2019-11-06 16:37:10 +0530

    remove encryption from embeded dpu services

--- components/GeneralFunctions.php
+++ components/GeneralFunctions.php
@@ -783,7 +783,7 @@ class GeneralFunctions extends Component {
     }
 
     public function base64url_decode($data) {
-        if (in_array(explode('/', $data)[0], ['restservices', 'webservice', 'androiddpu'])) {
+        if (in_array(explode('/', $data)[0], ['restservices', 'webservice', 'androiddpu', 'embededdpu'])) {
             return $data;
         }
         return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
