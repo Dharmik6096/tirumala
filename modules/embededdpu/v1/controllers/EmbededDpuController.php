1a77748fe48b750e8961fa52106d44230b1f17e9
Hardik (IT/RO) <it23@EIPL.LOCAL>
2019-11-06 17:58:26 +0530

    create service for save collection data and member list

--- modules/embededdpu/v1/controllers/EmbededDpuController.php
+++ modules/embededdpu/v1/controllers/EmbededDpuController.php
@@ -8,6 +8,7 @@ use Yii;
 use app\modules\organisation\models\TblDcs;
 use app\modules\embededdpu\controllers\RestController;
 use app\modules\organisation\models\TblDcsHistory;
+use app\modules\collection\models\TblMilkCollection;
 
 /**
  * Default controller for the `vendorapi` module
@@ -29,7 +30,7 @@ class EmbededDpuController extends RestController {
         return $this->response;
     }
 
-    public function actionAcknowledgeNameRequest() {
+    public function actionAcknowledgement() {
         $req_data = Yii::$app->request->getRawBody();
         $stationId = !empty($req_data['station_id']) ? $req_data['station_id'] : '0';
         $message = 'Unable to save!';
@@ -38,11 +39,40 @@ class EmbededDpuController extends RestController {
         if (!empty($modelData)) {
             $model = $modelData;
             $model->scenario = 'customImportUpdate';
-            $model->is_name_request = 0;
+            if (!empty($req_data['ack_type']) && $req_data['ack_type'] == 'rate') {
+                $model->rate_flag = 0;
+            } else if (!empty($req_data['ack_type']) && $req_data['ack_type'] == 'name') {
+                $model->is_name_request = 0;
+            }
             $transaction = $this->generalModel->saveTransaction([$model], [], ['transactional data', 'create']);
             if ($transaction == 'customRedirect') {
                 $message = 'Successfully Saved!';
             }
+        }
+        $this->response['message'] = [$message];
+        return $this->response;
+    }
+
+    public function actionMemberList() {
+        $req_data = Yii::$app->request->getRawBody();
+        $stationId = !empty($req_data['station_id']) ? $req_data['station_id'] : '0';
+        $sp_name = 'sp_app_embeded_dpu_v1_member_list';
+        $sp_param = [];
+        $sp_param[] = $stationId;
+        $response = \Yii::$app->general->getSpData($sp_name, $sp_param);
+        $this->response['data'] = $response;
+        return $this->response;
+    }
+
+    public function actionSaveCollectionData() {
+        $req_data = Yii::$app->request->getRawBody();
+        $message = 'Unable to save!';
+        $model = new TblMilkCollection();
+        $model->setAttributes($req_data);
+        $model->milk_quality_type_code = 1;
+        $transaction = $this->generalModel->saveTransaction([$model], [], ['transactional data', 'create']);
+        if ($transaction == 'customRedirect') {
+            $message = 'Successfully Saved!';
         }
         $this->response['message'] = [$message];
         return $this->response;
