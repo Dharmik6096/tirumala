e8e00e90e5b96e54a6f31be44cadbe1f4f29c237
sunita <sunita@sunita>
2020-02-05 18:26:56 +0530

    list screen revise of BMC Testing and Weight data

--- components/DropDown.php
+++ components/DropDown.php
@@ -466,7 +466,7 @@ class DropDown extends Component {
             'multiSelectOptions' => [
                 'id' => $id,
                 'clientOptions' =>
-                    [
+                [
                     'includeSelectAllOption' => true,
                     'numberDisplayed' => 1,
                     'disableIfEmpty' => true,
@@ -672,6 +672,11 @@ class DropDown extends Component {
                 'prompt' => Yii::t('app', 'Select'),
                 'data' => [0 => Yii::t('app', 'Not Require'), 1 => Yii::t('app', 'Require'), 2 => Yii::t('app', 'Auto')],
             ],
+            'is_quality_auto' => [
+                'name' => 'is_quality_auto',
+                'prompt' => Yii::t('app', 'Select'),
+                'data' => [0 => Yii::t('app', 'MANUAL'), 1 => Yii::t('app', 'AUTOMATIC')],
+            ],
         ];
         return $records[$l];
     }
@@ -822,7 +827,7 @@ class DropDown extends Component {
             'multiSelectOptions' => [
                 'id' => $id,
                 'clientOptions' =>
-                    [
+                [
                     'includeSelectAllOption' => true,
                     'numberDisplayed' => 0,
                     'disableIfEmpty' => true,
