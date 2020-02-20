458d655ecd1118d6c40f27b3e6d207bb70aec15d
sunita <sunita@sunita>
2020-02-20 17:38:59 +0530

    remove add button of create rate relculation

--- modules/dcsoperation/views/tbl-rate-recalculation/index.php
+++ modules/dcsoperation/views/tbl-rate-recalculation/index.php
@@ -1,6 +1,6 @@
 <?php
 $this->title = Yii::t('app', Yii::$app->label->title('list', 'Rate Recalculation'));
-$this->params['menu'][]=Yii::$app->controls->add('Rate Recalculation');
+//$this->params['menu'][]=Yii::$app->controls->add('Rate Recalculation');
 $this->params['menu'][]=Yii::$app->controls->add('Custom Rate Recalculation',['create-recalc']);
 ?>
 <div class="panel panel-default panel-grid panel-main">
