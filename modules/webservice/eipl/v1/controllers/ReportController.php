a343c9210c0f73ce5ef32f64329d37c0d5363fee
sunita <sunita@sunita>
2020-04-30 17:17:07 +0530

    new report API for HO

--- modules/webservice/eipl/v1/controllers/ReportController.php
+++ modules/webservice/eipl/v1/controllers/ReportController.php
@@ -30,13 +30,20 @@ class ReportController extends MasterController {
         $sp_name = $data['sp'];
         $sp_param = [];
         $param = !empty($data['param']) ? explode('#', $data['param']) : [];
-        $org_codes = $this->getOrgCodes();
+//        $org_codes = $this->getOrgCodes();
         foreach ($param as $value) {
             $array_val = explode(':', $value);
             $param_val = !empty($array_val[1]) ? $array_val[1] : (isset($req_data[$value]) ? $req_data[$value] : NULL);
             $param_val = empty($param_val) && isset($org_codes[$value]) ? (!empty($org_codes[$value]) ? (is_array($org_codes[$value]) ? (',' . implode(',', $org_codes[$value]) . ',') : $org_codes[$value]) : '0' ) : ((is_array($param_val) ? (',' . implode(',', $param_val) . ',') : $param_val));
             $sp_param[] = $param_val;
         }
+//        echo "<pre>";
+//        print_r($sp_name);
+//        echo "</pre>";
+//        echo "<pre>";
+//        print_r($sp_param);
+//        echo "</pre>";
+//        die;
         $result = \Yii::$app->general->getSpData($sp_name, $sp_param);
         $response['main_data'] = [];
         $response['summary_line'] = '';
@@ -47,6 +54,86 @@ class ReportController extends MasterController {
         }
         $this->response->setData($response);
         return $this->response;
+    }
+
+    public function actionMemberCollectionShift() {
+        return $this->prepareResposne();
+    }
+
+    public function actionMemberCollectionDateShiftSummary() {
+        return $this->prepareResposne();
+    }
+
+    public function actionMemberCollectionDateWiseSummary() {
+        return $this->prepareResposne();
+    }
+
+    public function actionMemberCollectionConsolidated() {
+        return $this->prepareResposne();
+    }
+
+    public function actionSocietyCollectionDateShiftSummary() {
+        return $this->prepareResposne();
+    }
+
+    public function actionSocietyCollectionDateWiseSummary() {
+        return $this->prepareResposne();
+    }
+
+    public function actionSocietyCollectionConsolidated() {
+        return $this->prepareResposne();
+    }
+
+    public function actionManualMilkEntrySocietyDateShiftWise() {
+        return $this->prepareResposne();
+    }
+
+    public function actionManualMilkEntryMemberDateShiftWise() {
+        return $this->prepareResposne();
+    }
+
+    public function actionManualMilkEntrySocietyDateWise() {
+        return $this->prepareResposne();
+    }
+
+    public function actionManualMilkEntryMemberDateWise() {
+        return $this->prepareResposne();
+    }
+
+    public function actionManualMilkEntrySocietyConsolidated() {
+        return $this->prepareResposne();
+    }
+
+    public function actionManualMilkEntryMemberConsolidated() {
+        return $this->prepareResposne();
+    }
+
+    public function actionBmcCollectionShiftReport() {
+        return $this->prepareResposne();
+    }
+
+    public function actionBmcCollectionDateShiftSummary() {
+        return $this->prepareResposne();
+    }
+
+    public function actionBmcCollectionDateWiseSummary() {
+        return $this->prepareResposne();
+    }
+
+    public function actionBmcCollectionConsolidated() {
+        return $this->prepareResposne();
+    }
+
+    public function actionUnionCollectionDateShiftSummary() {
+        return $this->prepareResposne();
+    }
+
+    public function actionUnionCollectionDateSummary() {
+        return $this->prepareResposne();
+    }
+
+    public function actionUnionCollectionConsolidated() {
+        return $this->prepareResposne();
     }
 
 }
