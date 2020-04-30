69ce2759ff6fe327f52af53507440e978f16128a
sunita <sunita@sunita>
2020-04-30 17:20:36 +0530

    remove commented code

--- modules/webservice/eipl/v1/controllers/ReportController.php
+++ modules/webservice/eipl/v1/controllers/ReportController.php
@@ -30,20 +30,13 @@ class ReportController extends MasterController {
         $sp_name = $data['sp'];
         $sp_param = [];
         $param = !empty($data['param']) ? explode('#', $data['param']) : [];
-//        $org_codes = $this->getOrgCodes();
+        $org_codes = $this->getOrgCodes();
         foreach ($param as $value) {
             $array_val = explode(':', $value);
             $param_val = !empty($array_val[1]) ? $array_val[1] : (isset($req_data[$value]) ? $req_data[$value] : NULL);
             $param_val = empty($param_val) && isset($org_codes[$value]) ? (!empty($org_codes[$value]) ? (is_array($org_codes[$value]) ? (',' . implode(',', $org_codes[$value]) . ',') : $org_codes[$value]) : '0' ) : ((is_array($param_val) ? (',' . implode(',', $param_val) . ',') : $param_val));
             $sp_param[] = $param_val;
         }
-//        echo "<pre>";
-//        print_r($sp_name);
-//        echo "</pre>";
-//        echo "<pre>";
-//        print_r($sp_param);
-//        echo "</pre>";
-//        die;
         $result = \Yii::$app->general->getSpData($sp_name, $sp_param);
         $response['main_data'] = [];
         $response['summary_line'] = '';
