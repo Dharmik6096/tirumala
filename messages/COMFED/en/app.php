ce6a540ac8450e37554509f19c5a2a9118f2839d
sunita <sunita@sunita>
2020-06-12 22:17:51 +0530

    change label in report

--- messages/COMFED/en/app.php
+++ messages/COMFED/en/app.php
@@ -131,4 +131,5 @@ return [
     'Company' => 'Union',
     '108 - Company Wise Collection vs Dispatch' => '108 - Union Wise Collection vs Dispatch',
     'Company Configuration ' => 'Union Configuration',
+    'Company Wise Collection vs Dispatch' => 'Union Wise Collection vs Dispatch',
 ];
