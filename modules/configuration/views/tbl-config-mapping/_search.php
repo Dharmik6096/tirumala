93bbfde64ee39272b5f29fc87ad433c5d7c37804
trupal <trupalpatel64@gmail.com>
2020-07-24 11:03:51 +0530

    changed in col width

--- modules/configuration/views/tbl-config-mapping/_search.php
+++ modules/configuration/views/tbl-config-mapping/_search.php
@@ -26,23 +26,23 @@ use yii\bootstrap\ActiveForm;
                 ?>
                 <div class="row margin_0">
                     <div class="modal-body">
-                        <div class="col-sm-6">
+                        <div class="col-sm-4">
                             <?= Yii::$app->dropdown->configFor($searchModel, $form, 'config_for', $searchModel->getAttributeLabel('config_for'), false, ['VLC', 'PORTAL']); ?>
                         </div>
-                        <div class="col-sm-6">
+                        <div class="col-sm-4">
                             <?= Html::hiddenInput('input', 0, ['id' => 'input']); ?>
                             <?= Yii::$app->dropdown->processName($searchModel, $form, 'tblconfigsearch-config_for,input', 'process_name', $model->getAttributeLabel('process_name')); ?>
                         </div>
-                        <div class="col-sm-6">
+                        <div class="col-sm-4">
                             <?= Yii::$app->dropdown->federation_union($model, $form, 'union_code', $model->getAttributeLabel('union_code'), TRUE); ?>
                         </div>
-                        <div class="col-sm-6">
+                        <div class="col-sm-4">
                             <?= Yii::$app->dropdown->union_plant($model, $form, 'tblconfigmapping-union_code', 'plant_code', $model->getAttributeLabel('plant_code'), FALSE, ''); ?>
                         </div>
-                        <div class="col-sm-6">
+                        <div class="col-sm-4">
                             <?= Yii::$app->dropdown->plant_mcc($model, $form, 'tblconfigmapping-plant_code', 'mcc_plant_code', $model->getAttributeLabel('mcc_plant_code'), FALSE, ''); ?>
                         </div>
-                        <div class="col-sm-6 bmc_class">
+                        <div class="col-sm-4 bmc_class">
                             <?= Yii::$app->dropdown->mcc_bmc($model, $form, 'tblconfigmapping-mcc_plant_code', 'bmc_code', Yii::t('app', 'BMC'), FALSE); ?>
                         </div>
                     </div>
