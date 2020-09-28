d064951b6f7d574e5a95df2b3476f08f4d4a7ecf
sunita <sunita@sunita>
2020-09-28 12:18:08 +0530

    remove validation from DCS of client NIFPL

--- components/CustomValidation.php
+++ components/CustomValidation.php
@@ -27,7 +27,11 @@ class CustomValidation extends Component {
                 'TblPlant' => [],
                 'TblMccPlant' => [],
                 'TblDcsBmc' => [],
-                'TblDcs' => [],
+                'TblDcs' => [
+                    'default' => [
+                        [['valid_from'], 'required', 'except' => ['importCsv', 'deactivate', 'routeMapping', 'saveCreamyData', 'customImport', 'customImportUpdate']],
+                    ]
+                ],
                 'TblContactDetails' => [
                     'dcs-create' => [],
                     'dcs-import' => [],
@@ -56,6 +60,7 @@ class CustomValidation extends Component {
                     [['hamlet_code', 'pincode', 'dcs_type_code'], 'required'],
                     [['district_code', 'sub_district_code', 'village_code'], 'required', 'except' => ['importCsv', 'customImport']],
                     [['contact_person', 'mobile_no'], 'required', 'on' => ['importCsv']],
+                    [['valid_from'], 'required', 'except' => ['importCsv', 'deactivate', 'routeMapping', 'saveCreamyData', 'customImport', 'customImportUpdate']],
                 ],
                 'TblContactDetails' => [
                     [['firstname', 'mobile_no'], 'required'],
@@ -69,6 +74,22 @@ class CustomValidation extends Component {
                 'TblBranch' => [
                     [['hamlet_code'], 'required'],
                     [['district_code', 'sub_district_code', 'village_code'], 'required', 'except' => 'importCsv'],
+                ],
+            ],
+            'NIFPL' => [
+                'TblDcs' => [
+                    'default' => [
+                        [['dcs_type_code'], 'required'],
+                    ]
+                ],
+                'TblContactDetails' => [
+                    'dcs-create' => [
+                        [['firstname'], 'required'],
+                    ],
+                    'dcs-import' => [],
+                    'default' => [
+                        [['firstname'], 'required'],
+                    ],
                 ],
             ],
         ];
