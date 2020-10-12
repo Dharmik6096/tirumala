6da757fa93319a3d14c0aa6036c3712e5f36ec6f
Trupal64 <trupalpatel64@gmail.com>
2020-10-12 10:23:58 +0530

    change in value getting method in controller

--- modules/geo/controllers/TblBlocksController.php
+++ modules/geo/controllers/TblBlocksController.php
@@ -83,8 +83,8 @@ class TblBlocksController extends ChildController
         $this->model = $this->findModel($id);
         $this->viewFile = 'update';
         $this->model->scenario = 'add';
-        $this->model->state = $this->model->subDistrictCode['districtCode']['stateCode']['state_code'];
-        $this->model->district = $this->model->subDistrictCode['districtCode']['district_code'];
+        $this->model->state = $this->model->subDistrictCode->districtCode->stateCode->state_code;
+        $this->model->district = $this->model->subDistrictCode->districtCode->district_code;
         $validate = 1;
 
         if (Yii::$app->request->post()) {
--- modules/geo/controllers/TblVillagesController.php
+++ modules/geo/controllers/TblVillagesController.php
@@ -64,8 +64,8 @@ class TblVillagesController extends ChildController {
         $this->model = $this->findModel($id);
         $this->viewFile = 'update';
         $this->model->scenario = 'add';
-        $this->model->state = $this->model->subDistrictCode['districtCode']['stateCode']['state_code'];
-        $this->model->district = $this->model->subDistrictCode['districtCode']['district_code'];
+        $this->model->state = $this->model->subDistrictCode->districtCode->stateCode->state_code;
+        $this->model->district = $this->model->subDistrictCode->districtCode->district_code;
         $validate = 1;
 
         if (Yii::$app->request->post()) {
