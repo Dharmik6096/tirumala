d9629743b3e143b93fd191eea4b090984c02fd17
sunita <sunita@sunita>
2020-10-21 15:17:16 +0530

    chnage in rate chart calculation

--- modules/dcsoperation/models/TblDcsPurchaseRateApplicabitity.php
+++ modules/dcsoperation/models/TblDcsPurchaseRateApplicabitity.php
@@ -292,7 +292,7 @@ class TblDcsPurchaseRateApplicabitity extends \app\models\ChildModel {
                         ->join('LEFT JOIN', 'tbl_dcs_purchase_rate_details dprd', 'dprd.purchase_rate_code = tbl_dcs_purchase_rate_applicability.purchase_rate_code')
                         ->where(['tbl_dcs_purchase_rate_applicability.is_active' => 1, 'tbl_dcs_purchase_rate_applicability.applicable_code' => $data['appl_code'], 'tbl_dcs_purchase_rate_applicability.applicable_for' => $data['appl_for'], 'tbl_dcs_purchase_rate.shift_applicability' => [3, $data['shift']]])
                         ->andWhere(['<=', 'tbl_dcs_purchase_rate_applicability.wef_date', $this->wef_date])
-                        ->andWhere(['dprd.milk_type_code' => $data['milk_type'], 'dprd.milk_quality_type_code' => $data['milk_quality_type_code']])
+//                        ->andWhere(['dprd.milk_type_code' => $data['milk_type'], 'dprd.milk_quality_type_code' => $data['milk_quality_type_code']])
                         ->orderBy('tbl_dcs_purchase_rate_applicability.wef_date desc')
                         ->one();
     }
