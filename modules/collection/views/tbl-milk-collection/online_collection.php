0114f681ce26598788112c1dc3233f382b533956
Hardik (IT23/RO) <it23@EIPL.LOCAL>
2020-10-22 12:59:15 +0530

    change map default zoom and map api key change

--- modules/collection/views/tbl-milk-collection/online_collection.php
+++ modules/collection/views/tbl-milk-collection/online_collection.php
@@ -41,11 +41,11 @@ $latLongArray = json_encode($latLongArray);
 $asset_path = Yii::$app->general->base64url_decode(Url::to(['/themes/pcdf/assets/']));
 $mapIcon = $this->theme->getUrl('/assets/images/map_marker.png');
 $googleMapKey = Yii::$app->params['google_map_api_key'];
-//AIzaSyAMTTxu_z6hetI1B83CmW30vGv6dibQWLU
+//AIzaSyAMTTxu_z6hetI1B83CmW30vGv6dibQWLU--//callback=initMap&
 ?>
 <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 <script
-    src="https://maps.googleapis.com/maps/api/js?key=<?= $googleMapKey ?>&callback=initMap&libraries=&v=weekly"
+    src="https://maps.googleapis.com/maps/api/js?key=<?= $googleMapKey ?>&libraries=&v=weekly"
     defer
 ></script>
 <!--<link rel="stylesheet" href="<?= '/themes/pcdf/assets/css/' ?>leaflet.css" />
@@ -137,45 +137,13 @@ $googleMapKey = Yii::$app->params['google_map_api_key'];
 
 
 <?php
-//$script = "
-//var asset_path = '/themes/pcdf/assets/';
-//var locations = '" . $latLongArray . "';
-//var locations=$.parseJSON(locations);
-//var gmap = L.map('map').setView([locations[0][1], locations[0][2]], 12);
-//
-//var LeafIcon = L.Icon.extend({
-//    options: {
-//       iconSize:     [25, 41],
-//       shadowSize:   [40, 40],
-//       iconAnchor:   [0, 0],
-//       shadowAnchor: [0, 0],
-//       popupAnchor:  [12, 0]
-//    }
-//});
-//var greenIcon = new LeafIcon({
-//    iconUrl: asset_path + '/images/marker-icon.png',
-//    shadowUrl: asset_path + '/images/marker-shadow.png'
-//})
-//L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-//    minZoom: 8,
-//    maxZoom: 40,
-//    attribution: '',
-//    id: 'mapbox.streets'
-//}).addTo(gmap);
-//$.each(locations, function(key, val) {
-//    L.marker([val[1],val[2]]).addTo(gmap)
-//            .bindPopup(val[0]);
-//});
-////    google.maps.event.addDomListener(window, 'load', initMap);
-//";
-
 $script = "
     $('.mis_report_modal_toggle').on('click', function(){
         $('#mis_report_search_filter').modal('toggle');
     });
     var locations = '" . $latLongArray . "';
     var locations=$.parseJSON(locations);
-//    function initMap() {
+  //  function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
           center: new google.maps.LatLng(locations[0][1], locations[0][2]),
           zoom: 15
@@ -214,30 +182,8 @@ $script = "
 
         });
         
-//    }
+  // }
 
-//    var map = new google.maps.Map(document.getElementById('map'), {
-//        zoom: 13,
-//        center: {lat: 26.4500688, lng: 80.3331902},
-//        mapTypeId: 'terrain',
-//         minZoom: 13
-//    });
-//
-//    var flightPlanCoordinates = [
-//        {lat: 26.4500688,lng: 80.3331902},
-//        {lat: 26.4502754,lng: 80.3362687},
-//        {lat: 26.4489887,lng: 80.3417592},
-//        {lat: 26.4565144,lng: 80.3471883},
-//    ];
-//    var flightPath = new google.maps.Polyline({
-//        path: flightPlanCoordinates,
-//        geodesic: true,
-//        strokeColor: '#FF0000',
-//        strokeOpacity: 1.0,
-//        strokeWeight: 2
-//    });
-//
-//    flightPath.setMap(map);
 ";
 
 if ($defaultToggle) {
