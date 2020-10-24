6f1346439e4040b72546e31311cf9aa0702d2363
Trupal64 <trupalpatel64@gmail.com>
2020-10-24 09:04:09 +0530

    resolved bug of check all in payment cycle applicability

--- modules/applicability/views/default/_form.php
+++ modules/applicability/views/default/_form.php
@@ -223,7 +223,7 @@ $modelName = 'TblDcsPurchaseRateApplicabitity';
                     $this->render('_checkbox_list', [
                         'model' => $model, 'form' => $form, 'field_name' => $main_field_name,
                         'list' => $dcs_list, 'selected' => $selected, 'selectedData' => $selectedCodes, 'checkboxClass' => $checkboxClass,
-                        'checkboxClass' => ' flt-checkbox dcsCheckboxes'
+                        'checkboxClass' => ' flt-checkboxa dcsCheckboxes'
                     ])
                     ?>
                 </div>
