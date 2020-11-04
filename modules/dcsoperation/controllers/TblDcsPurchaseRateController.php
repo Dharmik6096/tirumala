a7a508d146a8057a68fcdfeca22adf49cb2821cc
sunita <sunita@sunita>
2020-11-04 19:00:51 +0530

    validate import file of rate chart

--- modules/dcsoperation/controllers/TblDcsPurchaseRateController.php
+++ modules/dcsoperation/controllers/TblDcsPurchaseRateController.php
@@ -228,10 +228,10 @@ class TblDcsPurchaseRateController extends \app\controllers\ChildController {
         foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
             $sheetTitle = strtolower($worksheet->getTitle());
             $sheetTitlearray = explode('-', $sheetTitle);
-            $rateCatogory = ["1", "2", "3"];
-            if ((count($sheetTitlearray) == 2 && in_array($sheetTitlearray[0], array_map('strtolower', $SheetNames)) && in_array($sheetTitlearray[1], array_map('strtolower', $QualityType))) || (count($sheetTitlearray) == 3 && in_array($sheetTitlearray[2], $rateCatogory))) {
+            $rateCatogory = ["A", "B", "C"];
+            if ((count($sheetTitlearray) == 2 && in_array($sheetTitlearray[0], array_map('strtolower', $SheetNames)) && in_array($sheetTitlearray[1], array_map('strtolower', $QualityType))) || (count($sheetTitlearray) == 3 && in_array(strtoupper($sheetTitlearray[2]), $rateCatogory))) {
                 $allowCopy = strtolower($sheetTitlearray[1]) == 'good' ? TRUE : FALSE;
-                $rateClass = !empty($sheetTitlearray[2]) ? $sheetTitlearray[2] : 0;
+                $rateClass = !empty($sheetTitlearray[2]) ? strtoupper($sheetTitlearray[2]) : 0;
                 $FormulaType = strtoupper($worksheet->getCell('A1')->getValue());
                 if (!isset($ratearray[$sheetTitlearray[0]])) {
                     $ratearray[$sheetTitlearray[0]] = $FormulaType;
