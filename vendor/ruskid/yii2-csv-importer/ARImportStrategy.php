f19b326019cbb77bd189457b3eee9321f32f3f0e
sunita <sunita@sunita>
2020-11-06 20:42:48 +0530

    remove code of restrict getCode while import

--- vendor/ruskid/yii2-csv-importer/ARImportStrategy.php
+++ vendor/ruskid/yii2-csv-importer/ARImportStrategy.php
@@ -208,14 +208,14 @@ class ARImportStrategy extends BaseImportStrategy implements ImportInterface {
                     } else {
                         if ($this->isIncrement == 1) {
                             $model->{$primaryKey} = \Yii::$app->general->getCodeAutoIncrement($model);
-                        } else if ($this->isIncrement != 0 && method_exists($model, 'getCode')) {
+                        } else if (method_exists($model, 'getCode')) {
                             $model->{$primaryKey} = $model->getCode();
                         }
                     }
                 } else {
                     if ($this->isIncrement == 1) {
                         $model->{$primaryKey} = \Yii::$app->general->getCodeAutoIncrement($model);
-                    } else if ($this->isIncrement != 0 && method_exists($model, 'getCode')) {
+                    } else if (method_exists($model, 'getCode')) {
                         $model->{$primaryKey} = $model->getCode();
                     }
                 }
