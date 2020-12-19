c3ad9da2701788dc40dbc623d7dc8309ba4ed9ae
Hardik (IT23/RO) <it23@EIPL.LOCAL>
2020-12-19 18:50:04 +0530

    change in search

--- modules/bkgprocess/models/TblFtpTxnLogSearch.php
+++ modules/bkgprocess/models/TblFtpTxnLogSearch.php
@@ -120,16 +120,16 @@ class TblFtpTxnLogSearch extends TblFtpTxnLog {
             $query->andFilterWhere(['<=', 'cast(tbl_file_creator.applicable_date as date)', $to_date]);
         }
         // grid filtering conditions
-        $query->andFilterWhere([
-            'module_code' => $this->dcs_code,
-        ]);
+//        $query->andFilterWhere([
+//            'module_code' => $this->dcs_code,
+//        ]);
 
 
-        $query->andFilterWhere(['like', 'total_count', $this->total_count])
-                ->andFilterWhere(['like', 'success_count', $this->success_count])
-                ->andFilterWhere(['like', 'error_count', $this->error_count])
-                ->andFilterWhere(['like', 'file_name', $this->file_name])
-                ->andFilterWhere(['like', 'file_status', $this->file_status]);
+        $query->andFilterWhere(['like', 'tbl_ftp_txn_log.total_count', $this->total_count])
+                ->andFilterWhere(['like', 'tbl_ftp_txn_log.success_count', $this->success_count])
+                ->andFilterWhere(['like', 'tbl_ftp_txn_log.error_count', $this->error_count])
+                ->andFilterWhere(['like', 'tbl_ftp_txn_log.file_name', $this->file_name])
+                ->andFilterWhere(['like', 'tbl_ftp_txn_log.file_status', $this->file_status]);
         return $dataProvider;
     }
 
