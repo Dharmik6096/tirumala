3df99cc242114a375976c40b45e860600aafc39d
sunita <sunita@sunita>
2021-02-23 15:09:45 +0530

    remove validate function

--- modules/collection/models/TblQualityCollection.php
+++ modules/collection/models/TblQualityCollection.php
@@ -77,7 +77,6 @@ class TblQualityCollection extends \app\models\ChildModel {
             [['sample_no'], 'unique', 'targetAttribute' => ['date_time_of_collection', 'shift_code', 'mcc_plant_code', 'sample_no', 'doc_no'], 'skipOnEmpty' => true, 'message' => Yii::t('app/validation', '{attribute} has already been taken.'), 'except' => ['androidsync']],
             [['protein', 'density', 'lactose', 'adt_param', 'adt_value', 'received_timestamp'], 'safe'],
             [['uuid'], 'validateBmcCode'],
-            [['plant_code'], 'validateQualityData'],
         ];
     }
 
