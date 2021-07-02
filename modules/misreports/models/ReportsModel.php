2d3d47886e4b1ec3c2bed139386a748ab19cc448
Sunita Shiyal (IT30/RO) <it30@EIPL.LOCAL>
2021-07-02 17:05:31 +0530

    remove bmc from required in PM Advance

--- modules/misreports/models/ReportsModel.php
+++ modules/misreports/models/ReportsModel.php
@@ -68,7 +68,7 @@ class ReportsModel extends Model {
             [['union_code', 'plant_code', 'from_date', 'from_shift', 'to_date', 'to_shift', 'report_type'], 'required', 'on' => ['VendorWiseSummary', 'BmcWiseSummary']],
             [['union_code', 'plant_code', 'mcc_code', 'from_date', 'from_shift', 'to_date', 'to_shift', 'report_type'], 'required', 'on' => ['MilkCollectionNotExistsDetail', 'MilkCollectionNotExistsSummary']],
             [['union_code', 'plant_code', 'from_date', 'from_shift', 'to_date', 'to_shift', 'report_type'], 'required', 'on' => ['DayWiseQtyDetail', 'DayWiseQtySummary']],
-            [['union_code', 'plant_code', 'mcc_code', 'bmc_code', 'from_date', 'to_date', 'product_code'], 'required', 'on' => ['AdvancePm']],
+            [['union_code', 'plant_code', 'mcc_code', 'from_date', 'to_date', 'product_code'], 'required', 'on' => ['AdvancePm']],
             [['to_date'], 'validateToDate', 'on' => ['AdvancePm']],
         ];
     }
