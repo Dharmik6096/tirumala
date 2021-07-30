9255a1a02bb748ff99bbf46b26f06f5f392776fd
Hardik - Laptop (IT42/RO) <it42@EIPL.LOCAL>
2021-07-30 18:59:19 +0530

    transporter primary payment

--- modules/payment/views/tbl-transporter-payment/_form_grid.php
+++ modules/payment/views/tbl-transporter-payment/_form_grid.php
@@ -8,38 +8,38 @@ use yii\web\View;
 <?php
 
 $attribute = [
-    ['attribute' => 'union_code', 'value' => function($model) {
+        ['attribute' => 'union_code', 'value' => function($model) {
             return Yii::$app->general->getforeignkey($model->unionCode, 'union_name');
         }, 'filter' => FALSE, 'visible' => FALSE],
-    ['attribute' => 'plant_code', 'value' => function ($model) {
+        ['attribute' => 'plant_code', 'value' => function ($model) {
             return Yii::$app->general->getforeignkey($model->plantCode, 'name');
         }, 'filter' => FALSE, 'visible' => FALSE
     ],
-    ['attribute' => 'mcc_plant_code', 'value' => function ($model) {
+        ['attribute' => 'mcc_plant_code', 'value' => function ($model) {
             return Yii::$app->general->getforeignkey($model->mccPlantCode, 'name');
         }, 'filter' => FALSE, 'visible' => FALSE
     ],
-    ['attribute' => 'bmc_code', 'value' => function ($model) {
+        ['attribute' => 'bmc_code', 'value' => function ($model) {
             return Yii::$app->general->getforeignkey($model->bmcCode, 'bmc_name');
         }
     ],
-    ['attribute' => 'bill_no'],
-    ['attribute' => 'route_code', 'value' => function ($model) {
+        ['attribute' => 'bill_no'],
+        ['attribute' => 'route_code', 'value' => function ($model) {
             return Yii::$app->general->getforeignkey($model->routeCode, 'ref_code');
         }, 'label' => Yii::t('app', 'Route Code')],
-    ['attribute' => 'route_name', 'value' => function ($model) {
+        ['attribute' => 'route_name', 'value' => function ($model) {
             return Yii::$app->general->getforeignkey($model->routeCode, 'route_name');
         }
     ],
-    ['attribute' => 'transporter_code', 'value' => function($model) {
+        ['attribute' => 'transporter_code', 'value' => function($model) {
             return Yii::$app->general->getforeignkey($model->transporterCode, 'transporter_name');
         }],
-    ['attribute' => 'vehicle_code',
+        ['attribute' => 'vehicle_code',
         'value' => function ($model) {
             return Yii::$app->general->getforeignkey($model->vehicleCode, 'parsing_no');
         },
     ],
-    [
+        [
         'label' => 'Period',
         'filterType' => GridView::FILTER_DATE,
         'filterWidgetOptions' => [
@@ -47,27 +47,27 @@ $attribute = [
                 'autoclose' => true]
         ],
         'value' => function($model) {
-    return Yii::$app->controls->view_date($model->from_date) . ' To ' . Yii::$app->controls->view_date($model->to_date);
-}],
-    ['attribute' => 'billing_method', 'visible' => FALSE],
-    ['attribute' => 'no_of_days'],
-    ['attribute' => 'total_kms'],
-    ['attribute' => 'total_qty'],
-    ['attribute' => 'rec_kg_fat'],
-    ['attribute' => 'rec_kg_snf'],
-    ['attribute' => 'fixed_rent'],
-    ['attribute' => 'fuel_consumption'],
-    ['attribute' => 'vehicle_average'],
-    ['attribute' => 'fuel_rate'],
-    ['attribute' => 'total_amount'],
-    ['attribute' => 'fixed_amount'],
-    ['attribute' => 'total_addition'],
-    ['attribute' => 'total_deduction'],
-    ['attribute' => 'net_amount'],
-    ['attribute' => 'adjust_amount'],
-    ['attribute' => 'final_amount'],
-    ['attribute' => 'adjust_remark'],
-    [
+            return Yii::$app->controls->view_date($model->from_date) . ' To ' . Yii::$app->controls->view_date($model->to_date);
+        }],
+        ['attribute' => 'billing_method', 'visible' => FALSE],
+        ['attribute' => 'no_of_days'],
+        ['attribute' => 'total_kms'],
+        ['attribute' => 'total_qty'],
+        ['attribute' => 'rec_kg_fat'],
+        ['attribute' => 'rec_kg_snf'],
+        ['attribute' => 'fixed_rent'],
+        ['attribute' => 'fuel_consumption'],
+        ['attribute' => 'vehicle_average'],
+        ['attribute' => 'fuel_rate'],
+        ['attribute' => 'total_amount'],
+        ['attribute' => 'fixed_amount'],
+        ['attribute' => 'total_addition'],
+        ['attribute' => 'total_deduction'],
+        ['attribute' => 'net_amount'],
+        ['attribute' => 'adjust_amount'],
+        ['attribute' => 'final_amount'],
+        ['attribute' => 'adjust_remark'],
+        [
         'attribute' => 'payment_date',
         'filterType' => GridView::FILTER_DATE,
         'filterWidgetOptions' => [
@@ -75,9 +75,9 @@ $attribute = [
                 'autoclose' => true]
         ],
         'value' => function($model) {
-    return Yii::$app->controls->view_date($model->payment_date);
-}],
-    ['attribute' => 'status'],
+            return Yii::$app->controls->view_date($model->payment_date);
+        }],
+        ['attribute' => 'status'],
 ];
 
 
@@ -86,15 +86,15 @@ $grid_option = [
     'attributes' => $attribute,
     'active_column' => false,
     'actions' => [
-        'view' => true
-//        'lock-bill' => function ($url, $model) {
-//            $icon = ($model->status == 'processed') ? '<i class="fa fa-unlock"></i>' : '<i class="fa fa-lock"></i>';
-//            $class = ($model->status == 'processed') ? '' : 'disabled';
-//            $url = Url::to(['lock-bill']);
-//            $name = Yii::$app->general->getforeignkey($model->transporterCode, 'transporter_name') . '(' . Yii::$app->controls->view_date($model->from_date) . ' to ' . Yii::$app->controls->view_date($model->to_date) . ')';
-//            $options = ['data-toggle' => 'tooltip', 'data-placement' => 'top', 'data-original-title' => Yii::t('app', 'Lock Bill'), 'class' => 'lock-bill ' . $class, 'data-val' => $model->transporter_payment_code, 'data-name' => $name, 'data-url' => $url];
-//            return GhostHtml::a_alert($icon, ['/payment/tbl-transporter-payment/lock-bill'], $options);
-//        },
+        'view' => true,
+        'lock-bill' => function ($url, $model) {
+            $icon = ($model->status == 'processed') ? '<i class="fa fa-unlock"></i>' : '<i class="fa fa-lock"></i>';
+            $class = ($model->status == 'processed') ? '' : 'disabled';
+            $url = Url::to(['lock-bill']);
+            $name = Yii::$app->general->getforeignkey($model->transporterCode, 'transporter_name') . '(' . Yii::$app->controls->view_date($model->from_date) . ' to ' . Yii::$app->controls->view_date($model->to_date) . ')';
+            $options = ['data-toggle' => 'tooltip', 'data-placement' => 'top', 'data-original-title' => Yii::t('app', 'Lock Bill'), 'class' => 'lock-bill ' . $class, 'data-val' => $model->transporter_payment_code, 'data-name' => $name, 'data-url' => $url];
+            return GhostHtml::a_alert($icon, ['/payment/tbl-transporter-payment/lock-bill'], $options);
+        },
 //                'print-bill' => function ($url, $model) {
 //            $options = ['target' => '_blank', 'title' => Yii::t('app', 'View Bill'), 'data-toggle' => 'tooltip', 'data-placement' => 'top', 'data-original-title' => Yii::t('app', 'View Bill')];
 //            return GhostHtml::a('<i class="fa fa-file-pdf-o"></i>', ['/payment/tbl-transporter-payment/view-bill', 'union_code' => $model->union_code, 'transporter_code' => $model->transporter_code, 'from_date' => $model->from_date, 'to_date' => $model->to_date, 'transporter_type' => $model->transporter_type, 'route_code' => $model->route_code], $options);
@@ -132,7 +132,9 @@ $(document).ready(function(){
                         type: 'post',
                         url: url,
                         data:{'id':id},
-                        success: function(data) {                          
+                        success: function(data) {   
+                                var obj1 = $.parseJSON(data);
+                                bootbox.alert(\"<div class=\'row\'><div class=\'col-sm-12\'><div class=\'bg-success\'><i class=\'fa fa-check\'></i></div><span>\"+obj1.msg+\"</span></div></div>\");
                                 $.pjax.reload({container: '#transporter-payment-grid'});                          
                         },
                         error:function(data){
