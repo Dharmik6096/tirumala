06fb86b061526b14a1889f77e3ee29fc6192e7b7
Sunita Shiyal (IT30/RO) <it30@EIPL.LOCAL>
2021-09-10 11:58:31 +0530

    dcs closing

---
+++ modules/collection/views/tbl-dcs-closing/_form_grid.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+use yii\helpers\Html;
+use kartik\grid\GridView;
+
+?>
+<?php
+
+$attribute = [
+    ['attribute' => 'union_code', 'value' => function($model) {
+            return Yii::$app->general->getforeignkey($model->unionCode, 'union_name');
+        }, 'vAlign' => 'middle', 'filter' => false, 'visible' => false],
+    ['attribute' => 'plant_code', 'value' => function($model) {
+            return Yii::$app->general->getforeignkey($model->plantCode, 'name');
+        }, 'vAlign' => 'middle', 'filter' => false, 'visible' => false],
+    ['attribute' => 'mcc_plant_code', 'value' => function($model) {
+            return Yii::$app->general->getforeignkey($model->mccPlantCode, 'name');
+        }, 'vAlign' => 'middle', 'filter' => false],
+    ['attribute' => 'bmc_name', 'value' => function($model) {
+            return Yii::$app->general->getforeignkey($model->bmcCode, 'bmc_name');
+        }, 'vAlign' => 'middle', 'filter' => false],
+    ['label' => Yii::t('app', 'Soc. Code'), 'visible' => TRUE, 'attribute' => 'dcs_code', 'filter' => false],
+    ['label' => Yii::t('app', 'Old Soc. Code'), 'attribute' => 'dcs_code',
+        'value' => function($model) {
+            return Yii::$app->general->getforeignkey($model->dcsCode, 'dcs_code_ex');
+        }, 'filter' => false],
+    ['label' => Yii::t('app', 'Ref. Code'), 'attribute' => 'ref_code',
+        'value' => function($model) {
+            return Yii::$app->general->getforeignkey($model->dcsCode, 'ref_code');
+        },],
+    ['attribute' => 'dcs_code', 'label' => Yii::t('app', 'Society Name'),
+        'value' => function($model) {
+            return Yii::$app->general->getforeignkey($model->dcsCode, 'dcs_name');
+        }, 'filter' => false],
+    [
+        'attribute' => 'transaction_date',
+        'filterType' => GridView::FILTER_DATE,
+        'filterWidgetOptions' => [
+            'pluginOptions' => ['format' => 'dd-mm-yyyy',
+                'autoclose' => true]
+        ],
+        'value' => function($model) {
+            return Yii::$app->controls->view_date($model->transaction_date);
+        }],
+    [
+        'attribute' => 'to_date',
+        'filterType' => GridView::FILTER_DATE,
+        'filterWidgetOptions' => [
+            'pluginOptions' => ['format' => 'dd-mm-yyyy',
+                'autoclose' => true]
+        ],
+        'value' => function($model) {
+            return Yii::$app->controls->view_date($model->to_date);
+        }],
+    ['attribute' => 'to_shift_code', 'value' => 'shiftCode.shift', 'vAlign' => 'middle', 'filter' => false],
+    ['attribute' => 'milk_type_code', 'value' => function($model) {
+            return isset($model->milkType) ? $model->milkType->animal_type_name : '';
+        }, 'vAlign' => 'middle', 'filter' => false],
+    ['attribute' => 'qty', 'value' => 'qty', 'vAlign' => 'middle', 'filter' => false],
+    ['attribute' => 'fat', 'value' => 'fat', 'vAlign' => 'middle', 'filter' => false],
+    ['attribute' => 'snf', 'value' => 'snf', 'vAlign' => 'middle', 'filter' => false],
+    ['attribute' => 'remarks', 'filter' => false, 'visible' => false],
+];
+
+$grid_option = [
+    'id' => 'dcs-closing',
+    'attributes' => $attribute,
+    'active_column' => false,
+    'actions' => [
+//        'view' => TRUE,
+        'update' => true,
+        'delete' => ['option' => 'dcs_closing_code,dcs_closing_code,/collection/tbl-dcs-closing/delete'],
+    ]
+];
+
+Yii::$app->grid->bind($dataProvider, $searchModel, $grid_option);
+?>
