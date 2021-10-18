6e5c3c7549dcab0d9f3c870dfd69576cde18dbe1
Hardik - Laptop (IT42/RO) <it42@EIPL.LOCAL>
2021-10-18 15:34:08 +0530

    change message

--- modules/payment/views/tbl-member-payment/process_lock_dcs_payment_data.php
+++ modules/payment/views/tbl-member-payment/process_lock_dcs_payment_data.php
@@ -541,10 +541,14 @@ $(document).on("click", "#adjust-lock-dcs-data", function(){
     var negativeVal = "No";
     
     var message = "' . $message . '";
-    var negativeCount = ' . $negativeValCount . ';
-    if(negativeCount > 0 || negativeVal == "Yes") {
+    var negativeCount = "' . $negativeContent . '";
+    if(negativeVal == "Yes") {
         var dispMessage = "' . Yii::t('app', 'Net Payable must be Positive for each Member.') . '";
         bootbox.alert("<div class=\"bg-danger\"><i class=\"fa fa-times-circle\"></i></div><span>"+dispMessage+"</span>");
+    } else if (negativeCount == "yes") {
+        var negativeMsg = "' . $errMsg . '";
+        var dispMessage = "' . Yii::t('app', 'Net Payable must be Positive for each Member.') . '";
+        bootbox.alert("<div class=\"bg-danger\"><i class=\"fa fa-times-circle\"></i></div><span>"+negativeMsg+"</span>");
     } else {
         bootbox.confirm({
             message: "<div class=\"bg-danger\"><i class=\"fa fa-question-circle\"></i></div><span>"+message+"</span>",
