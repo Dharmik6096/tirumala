97685c86665b524b656fa0698ab0be625cc16ec5
Sunita Shiyal (IT30/RO) <it30@EIPL.LOCAL>
2021-10-28 18:38:00 +0530

    resolve issue at milk and bmc collection redirection

--- modules/collection/views/tbl-milk-collection/index_allow.php
+++ modules/collection/views/tbl-milk-collection/index_allow.php
@@ -4,9 +4,9 @@ use webvimark\modules\UserManagement\components\GhostHtml;
 use yii\helpers\Url;
 
 $this->title = Yii::t('app', Yii::$app->label->title('list', 'Milk Collections'));
-$this->params['menu'][] = GhostHtml::a(Yii::t('app', '<i class="fa fa-plus"></i> Add ' . Yii::t('app', 'Create Allow')), Url::to("create-allow"), ['class' => 'btn btn-danger btn-block apply-shortcut', 'shortcut_key' => 'ctrl+alt+c']);
-$this->params['menu'][] = Yii::$app->controls->custombutton('Update Milk Collection Allow', 'update-collection-allow', '', 'btn btn-danger btn-block', '<i class="fa fa-pencil"></i>');
-$this->params['menu'][] = Yii::$app->controls->custombutton('Delete Milk Collection Allow', 'delete-collection-allow', '', 'btn btn-danger btn-block', '<i class="fa fa-trash"></i>');
+$this->params['menu'][] = Yii::$app->controls->custombutton('Add Milk Collection', 'create-allow', '', 'btn btn-danger btn-block', '<i class="fa fa-plus"></i>');
+$this->params['menu'][] = Yii::$app->controls->custombutton('Update Milk Collection', 'update-collection-allow', '', 'btn btn-danger btn-block', '<i class="fa fa-pencil"></i>');
+$this->params['menu'][] = Yii::$app->controls->custombutton('Delete Milk Collection', 'delete-collection-allow', '', 'btn btn-danger btn-block', '<i class="fa fa-trash"></i>');
 if (Yii::$app->general->getUnionConfiguration(explode(',', Yii::$app->session->get('Unions')), 'qlty_wise_collection', 'PORTAL') == 1) {
     $this->params['menu'][] = Yii::$app->controls->import('milk-collection-qlty-allow-bulk', $this);
 } else {
