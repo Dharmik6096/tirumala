02dcff4d3b58a6a7be794bcbb23971b2889ede18
vivek <vivek@everestinstruments.com>
2021-11-23 14:22:07 +0530

    add validations in inventory transfer import process

--- modules/product/models/TblInventoryTransfer.php
+++ modules/product/models/TblInventoryTransfer.php
@@ -57,31 +57,58 @@ class TblInventoryTransfer extends \app\models\ChildModel {
      */
     public function rules() {
         return [
-            [['inventory_transfer_no', 'inventory_transfer_date', 'from_type', 'from_code', 'to_type', 'to_code'], 'required'],
-            [['inventory_transfer_code'], 'safe'],
-            [['inventory_transfer_date', 'created_at', 'updated_at', 'from_mcc_plant_code', 'from_bmc_code', 'from_dcs_code', 'to_mcc_plant_code', 'to_bmc_code', 'to_dcs_code', 'product_code', 'qty', 'available_stock', 'unit_code'], 'safe'],
-            [['remarks'], 'string'],
-            [['originating_type'], 'integer'],
-            [['inventory_transfer_code', 'inventory_transfer_no'], 'string', 'max' => 30],
-            [['union_code'], 'string', 'max' => 3],
-            [['created_by', 'updated_by'], 'string', 'max' => 14],
-            [['originating_org_code', 'originating_org_type'], 'string', 'max' => 15],
-            [['x_col1', 'x_col2', 'x_col3', 'x_col4', 'x_col5'], 'string', 'max' => 255],
-            [['from_type', 'to_type'], function ($attribute, $params) {
+                [['inventory_transfer_no', 'inventory_transfer_date', 'from_type', 'from_code', 'to_type', 'to_code'], 'required'],
+                [['inventory_transfer_code'], 'safe'],
+                [['inventory_transfer_date', 'created_at', 'updated_at', 'from_mcc_plant_code', 'from_bmc_code', 'from_dcs_code', 'to_mcc_plant_code', 'to_bmc_code', 'to_dcs_code', 'product_code', 'qty', 'available_stock', 'unit_code'], 'safe'],
+                [['remarks'], 'string'],
+                [['originating_type'], 'integer'],
+                [['inventory_transfer_code', 'inventory_transfer_no'], 'string', 'max' => 30],
+                [['union_code'], 'string', 'max' => 3],
+                [['created_by', 'updated_by'], 'string', 'max' => 14],
+                [['originating_org_code', 'originating_org_type'], 'string', 'max' => 15],
+                [['x_col1', 'x_col2', 'x_col3', 'x_col4', 'x_col5'], 'string', 'max' => 255],
+                [['from_type', 'to_type'], function ($attribute, $params) {
                     Yii::$app->general->validateGlobalStatic($this, $attribute, 'org_type');
                 }, 'on' => 'importCsv'],
-            [['inventory_transfer_no'], 'setImport', 'on' => ['importCsv']],
-            [['from_mcc_plant_code'], 'exist', 'skipOnError' => true, 'targetClass' => TblMccPlant::className(), 'targetAttribute' => ['from_mcc_plant_code' => 'mcc_plant_code'], 'on' => 'importCsv'],
-            [['from_bmc_code'], 'exist', 'skipOnError' => true, 'targetClass' => TblDcsBmc::className(), 'targetAttribute' => ['from_bmc_code' => 'bmc_code'], 'on' => 'importCsv'],
-            [['from_dcs_code'], 'exist', 'skipOnError' => true, 'targetClass' => TblDcs::className(), 'targetAttribute' => ['from_dcs_code' => 'dcs_code'], 'on' => 'importCsv'],
-            [['to_mcc_plant_code'], 'exist', 'skipOnError' => true, 'targetClass' => TblMccPlant::className(), 'targetAttribute' => ['to_mcc_plant_code' => 'mcc_plant_code'], 'on' => 'importCsv'],
-            [['to_bmc_code'], 'exist', 'skipOnError' => true, 'targetClass' => TblDcsBmc::className(), 'targetAttribute' => ['to_bmc_code' => 'bmc_code'], 'on' => 'importCsv'],
-            [['to_dcs_code'], 'exist', 'skipOnError' => true, 'targetClass' => TblDcs::className(), 'targetAttribute' => ['to_dcs_code' => 'dcs_code'], 'on' => 'importCsv'],
-            [['inventory_transfer_no'], 'checkStock', 'on' => ['importCsv']],
-            [['inventory_transfer_date'], 'convertDateDot', 'on' => ['importCsv']],
-            [['inventory_transfer_date'], 'date', 'format' => 'php:d.m.Y', 'message' => Yii::t('app/validation', 'Please enter date in valid format e.g. 01.12.2018'), 'on' => ['importCsv']],
-            [['inventory_transfer_date'], 'convertDate', 'on' => ['importCsv']],
-            [['to_code'], 'validateToTransfer', 'except' => ['androidsync']]
+                [['inventory_transfer_no'], 'unique'],
+                [['from_code'], function ($attribute, $params) {
+                    if (strtoupper($this->from_type == 'BMC')) {
+                        return Yii::$app->general->validateBMC($this, $attribute, 'bmc_code');
+                    } else if (strtoupper($this->from_type == 'MCC')) {
+                        $mcc = new TblMccPlant();
+                        $this->from_code = $mcc->getValidMcc($this->from_code);
+                        return !empty($this->from_code) ? $this->from_code : $this->addError('from_code', Yii::t('app/validation', $this->getAttributeLabel($attribute) . ' is Invalid.'));
+                    } else if (strtoupper($this->from_type == 'DCS')) {
+                        $dcs = new TblDcs();
+                        $this->from_code = $dcs->getValidDcs($this->from_code);
+                        return !empty($this->from_code) ? $this->from_code : $this->addError('from_code', Yii::t('app/validation', $this->getAttributeLabel($attribute) . ' is Invalid.'));
+                    }
+                }, 'on' => ['importCsv']],
+                [['to_code'], function ($attribute, $params) {
+                    if (strtoupper($this->to_type == 'BMC')) {
+                        return Yii::$app->general->validateBMC($this, 'to_code', 'bmc_code');
+                    } else if (strtoupper($this->to_type == 'MCC')) {
+                        $mcc = new TblMccPlant();
+                        $this->to_code = $mcc->getValidMcc($this->to_code);
+                        return !empty($this->to_code) ? $this->to_code : $this->addError('to_code', Yii::t('app/validation', $this->getAttributeLabel($attribute) . ' is Invalid.'));
+                    } else if (strtoupper($this->to_type == 'DCS')) {
+                        $dcs = new TblDcs();
+                        $this->to_code = $dcs->getValidDcs($this->to_code);
+                        return !empty($this->to_code) ? $this->to_code : $this->addError('to_code', Yii::t('app/validation', $this->getAttributeLabel($attribute) . ' is Invalid.'));
+                    }
+                }, 'on' => ['importCsv']],
+                [['inventory_transfer_no'], 'setImport', 'on' => ['importCsv']],
+                [['from_mcc_plant_code'], 'exist', 'skipOnError' => true, 'targetClass' => TblMccPlant::className(), 'targetAttribute' => ['from_mcc_plant_code' => 'mcc_plant_code'], 'on' => 'importCsv'],
+                [['from_bmc_code'], 'exist', 'skipOnError' => true, 'targetClass' => TblDcsBmc::className(), 'targetAttribute' => ['from_bmc_code' => 'bmc_code'], 'on' => 'importCsv'],
+                [['from_dcs_code'], 'exist', 'skipOnError' => true, 'targetClass' => TblDcs::className(), 'targetAttribute' => ['from_dcs_code' => 'dcs_code'], 'on' => 'importCsv'],
+                [['to_mcc_plant_code'], 'exist', 'skipOnError' => true, 'targetClass' => TblMccPlant::className(), 'targetAttribute' => ['to_mcc_plant_code' => 'mcc_plant_code'], 'on' => 'importCsv'],
+                [['to_bmc_code'], 'exist', 'skipOnError' => true, 'targetClass' => TblDcsBmc::className(), 'targetAttribute' => ['to_bmc_code' => 'bmc_code'], 'on' => 'importCsv'],
+                [['to_dcs_code'], 'exist', 'skipOnError' => true, 'targetClass' => TblDcs::className(), 'targetAttribute' => ['to_dcs_code' => 'dcs_code'], 'on' => 'importCsv'],
+                [['inventory_transfer_no'], 'checkStock', 'on' => ['importCsv']],
+                [['inventory_transfer_date'], 'convertDateDot', 'on' => ['importCsv']],
+                [['inventory_transfer_date'], 'date', 'format' => 'php:d.m.Y', 'message' => Yii::t('app/validation', 'Please enter date in valid format e.g. 01.12.2018'), 'on' => ['importCsv']],
+                [['inventory_transfer_date'], 'convertDate', 'on' => ['importCsv']],
+                [['to_code'], 'validateToTransfer', 'except' => ['androidsync']]
         ];
     }
 
