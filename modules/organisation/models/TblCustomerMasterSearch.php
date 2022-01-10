ecbf7b9d13cf6ba5319f9ab50e452fd8062bcb91
Vivek Patel (IT25/RO) <it25@EIPL.LOCAL>
2022-01-10 13:26:09 +0530

    add search filter for ts_code in DCS and customer master

--- modules/organisation/models/TblCustomerMasterSearch.php
+++ modules/organisation/models/TblCustomerMasterSearch.php
@@ -22,7 +22,7 @@ class TblCustomerMasterSearch extends TblCustomerMaster {
         return [
                 [['customer_code', 'customer_name', 'address', 'state_code', 'district_code', 'sub_district_code', 'village_code', 'hamlet_code', 'local_name', 'local_address', 'gst_no', 'union_code', 'created_at', 'created_by', 'updated_at', 'updated_by', 'customer_type', 'sap_code', 'refference_code', 'x_col1', 'x_col2', 'x_col3', 'x_col4', 'x_col5', 'originating_org_code', 'originating_org_type', 'customer_code_ex', 'route_code', 'ref_code', 'aadhaar_no', 'master_type'], 'safe'],
                 [['is_active', 'originating_type'], 'integer'],
-                [['bmc_code', 'mcc_plant_code', 'plant_code'], 'safe'],
+                [['bmc_code', 'mcc_plant_code', 'plant_code', 'ts_code_m', 'ts_code_e'], 'safe'],
                 [['customer_type', 'union_code', 'plant_code', 'mcc_plant_code', 'mcc_code', 'bmc_code'], 'required', 'on' => ['deleteMapRoute']],
                 [['union_code', 'plant_code', 'mcc_plant_code', 'mcc_code', 'bmc_code'], 'required', 'on' => ['verification']]
         ];
@@ -62,6 +62,8 @@ class TblCustomerMasterSearch extends TblCustomerMaster {
             'tbl_customer_master.is_active' => $this->is_active,
             'tbl_customer_master.route_code' => $this->route_code,
             'tbl_customer_master.x_col2' => $this->x_col2,
+            'tbl_customer_master.ts_code_m' => $this->ts_code_m,
+            'tbl_customer_master.ts_code_e' => $this->ts_code_e,
         ]);
 
         $query->andFilterWhere(['like', 'tbl_customer_master.customer_code', $this->customer_code])
