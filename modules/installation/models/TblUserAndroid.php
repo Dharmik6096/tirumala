62405557511dc9ce3cd899e0556d73077944ad5c
Asmita Karmur (IT36/RO) <it36@EIPL.LOCAL>
2022-02-16 15:05:35 +0530

    AMCS - resolve issue of vlc user sync in bmc

--- modules/installation/models/TblUserAndroid.php
+++ modules/installation/models/TblUserAndroid.php
@@ -61,7 +61,7 @@ class TblUserAndroid extends \yii\db\ActiveRecord {
         return [
                 [['user_code', 'username', 'name', 'password', 'mobile_no', 'repeat_password', 'plant_code', 'mcc_plant_code'], 'required', 'except' => ['installation', 'importCsv']],
                 [['username', 'name', 'password', 'repeat_password', 'mobile_no', 'org_type', 'org_code'], 'required', 'on' => 'importCsv'],
-                [['created_at', 'updated_at', 'user_code', 'password', 'org_type', 'org_code', 'originating_org_code', 'role_code', 'is_active', 'mobile_no','device_id'], 'safe'],
+                [['created_at', 'updated_at', 'user_code', 'password', 'org_type', 'org_code', 'originating_org_code', 'role_code', 'is_active', 'mobile_no', 'device_id'], 'safe'],
                 [['originating_type'], 'integer'],
                 [['username'], 'unique'],
                 [['created_by', 'updated_by'], 'string', 'max' => 14],
@@ -124,9 +124,9 @@ class TblUserAndroid extends \yii\db\ActiveRecord {
     public function getExistData($org_type, $data, $notIn = '') {
         $query = $this->find()->where(['union_code' => $data->union_code, 'is_active' => 1]);
         if (strtoupper($org_type == 'MCC')) {
-            $query->andWhere(['plant_code' => $data->plant_code, 'mcc_plant_code' => $data->mcc_plant_code]);
+            $query->andWhere(['plant_code' => $data->plant_code, 'mcc_plant_code' => $data->mcc_plant_code])->andWhere(['=', 'ISNULL(bmc_code,\'\')', '']);
         } elseif ($org_type == 'BMC') {
-            $query->andWhere(['plant_code' => $data->plant_code, 'mcc_plant_code' => $data->mcc_plant_code, 'bmc_code' => $data->bmc_code]);
+            $query->andWhere(['plant_code' => $data->plant_code, 'mcc_plant_code' => $data->mcc_plant_code, 'bmc_code' => $data->bmc_code])->andWhere(['=', 'ISNULL(dcs_code,\'\')', '']);
         } elseif ($org_type == 'VLC') {
             $query->andWhere(['plant_code' => $data->plant_code, 'mcc_plant_code' => $data->mcc_plant_code, 'bmc_code' => $data->bmc_code, 'dcs_code' => $data->dcs_code]);
         }
