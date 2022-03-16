846facbc68c7a6a53865e66edb8c55d8abb339eb
Hardik Savani (IT42/RO) <it42@EIPL.LOCAL>
2022-03-16 13:20:02 +0530

    resolve issue for file not upload on ftp on shift lock

--- modules/bkgprocess/Bkgprocess.php
+++ modules/bkgprocess/Bkgprocess.php
@@ -30,9 +30,17 @@ class Bkgprocess extends \yii\base\Module {
               'param2' => 'union_code,mcc_code,date:string:shift',
               'export_title' => true,
               'module_name' => 'TblMilkCollection'], */
+            'TblMilkCollection' => [
+                'ext' => '.csv',
+                'param' => 'union_code,mcc_plant_code,bmc_code,from_date,to_date',
+                'sp_name' => 'rpt_MIS_SDSAPReport',
+                'export_title' => 'mcc_plant_code+_+SD+_+applicable_date:date+_+shift_code',
+                'file_path' => str_replace('\\', '/', realpath(\Yii::$app->basePath . '/../')) . \Yii::$app->params['FTPDirPath'] . 'upload/',
+                'ftp_path' => 'SD/Inbound'
+            ],
             'TblBmcCollection' => [
                 'ext' => '.csv',
-                'param' => 'union_code,mcc_plant_code,applicable_date',
+                'param' => 'union_code,mcc_plant_code,bmc_code,from_date,to_date',
                 'sp_name' => 'rpt_MIS_WQSAPReport',
                 'sp_check_data' => 'sp_checkDatacompleteness_BMC',
                 'param2' => 'union_code,mcc_plant_code,shift_date:date:shift_code',
@@ -41,6 +49,17 @@ class Bkgprocess extends \yii\base\Module {
                 'file_path' => str_replace('\\', '/', realpath(\Yii::$app->basePath . '/../')) . \Yii::$app->params['FTPDirPath'] . 'upload/',
                 'ftp_path' => 'WQ/Inbound'
             ],
+//            'TblBmcCollection' => [
+//                'ext' => '.csv',
+//                'param' => 'union_code,mcc_plant_code,applicable_date',
+//                'sp_name' => 'rpt_MIS_WQSAPReport',
+//                'sp_check_data' => 'sp_checkDatacompleteness_BMC',
+//                'param2' => 'union_code,mcc_plant_code,shift_date:date:shift_code',
+//                'export_title' => 'mcc_plant_code+_+WQ+_+applicable_date:date+_+shift_code',
+//                'summary_model' => 'TblShiftSummary',
+//                'file_path' => str_replace('\\', '/', realpath(\Yii::$app->basePath . '/../')) . \Yii::$app->params['FTPDirPath'] . 'upload/',
+//                'ftp_path' => 'WQ/Inbound'
+//            ],
         ];
     }
 
