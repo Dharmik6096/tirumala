6e9f8aac0e1aaae717ecb6b8abb6b13119d29315
Hardik Savani (IT42/RO) <it42@EIPL.LOCAL>
2022-06-21 17:41:49 +0530

    erp integration while lock bmc data from mcc shift lock

--- modules/collection/controllers/TblMccShiftLockController.php
+++ modules/collection/controllers/TblMccShiftLockController.php
@@ -149,7 +149,7 @@ class TblMccShiftLockController extends \app\controllers\ChildController {
             $this->model->x_col4 = $famount;
             $existData = $this->model->getExistData();
             $Recovery = TRUE;
-            if (Yii::$app->session->get('eiplCode') == 'MMDfsfd') {
+            if (Yii::$app->session->get('eiplCode') == 'MMD') {
                 $recoveryModel = new TblVspTransitRecovery();
                 $existRecovery = $recoveryModel->getExistData($this->model);
                 $Recovery = $existRecovery > 0 ? TRUE : FALSE;
@@ -190,7 +190,10 @@ class TblMccShiftLockController extends \app\controllers\ChildController {
                 $saveModel[] = $this->model;
                 $transaction = $this->generalModel->saveTransaction($saveModel, ['Shift Lock', 'edit']);
                 if ($transaction == 'customRedirect') {
-                    if (Yii::$app->session->get('eiplCode') == 'MMDfdf') {
+                    if (Yii::$app->session->get('eiplCode') == 'MMD') {
+                        $this->setMmdErpData();
+                    }
+                    if (false && Yii::$app->session->get('eiplCode') == 'MMD') {
                         $api = new WebApi();
                         $api->serverUrl = 'https://login.microsoftonline.com/2c11ed1f-0dff-46b9-94e9-8cbe83717417/oauth2/token';
                         $api->authentication = FALSE;
@@ -360,6 +363,13 @@ class TblMccShiftLockController extends \app\controllers\ChildController {
         $model->x_col4 = $famnt;
         $model->{$updateField} = $val;
         $modelData = $model->getExistData();
+        $Recovery = FALSE;
+        if (Yii::$app->session->get('eiplCode') == 'MMD') {
+            $recoveryModel = new TblVspTransitRecovery();
+            $existRecovery = $recoveryModel->getExistData($model);
+            $Recovery = $existRecovery > 0 ? TRUE : FALSE;
+        }
+
         $title = $model->{$updateField} == 1 ? $lockMessage : $unlockMessage;
 
         if (!empty($modelData)) {
@@ -376,8 +386,35 @@ class TblMccShiftLockController extends \app\controllers\ChildController {
             $saveModel[] = $model;
         }
 
+        if ($Recovery) {
+            $staging = new TblMccShiftLockStaging();
+            $attribute = $model->attributes;
+            unset($attribute['x_col1']);
+            unset($attribute['x_col2']);
+            $staging->setAttributes($attribute);
+            $stagingData = $staging->find()->where(['shift_lock_code' => $this->model->shift_lock_code])->one();
+            if (!empty($stagingData)) {
+                $stagingData->setAttributes($attribute);
+//                $stagingData->x_col1 = null;
+//                $stagingData->x_col2 = null;
+                $stagingData->data_post_status = 0;
+                $stagingData->picked_datetime = NULL;
+                $stagingData->response_datetime = NULL;
+                $stagingData->resp_status = NULL;
+                $stagingData->resp_desc = NULL;
+                $saveModel[] = $stagingData;
+            } else {
+                $ConcateDate = date('d', strtotime($date)) . '' . date('m', strtotime($date)) . '' . date('y', strtotime($date));
+                $ConcateShift = $shift;
+                $staging->staging_code = $model->mcc_plant_code . '-' . $ConcateDate . '-' . $ConcateShift;
+                $saveModel[] = $staging;
+            }
+        }
         $transaction = $this->generalModel->saveTransaction($saveModel, [$title, 'edit']);
         if ($transaction == 'customRedirect') {
+            if (Yii::$app->session->get('eiplCode') == 'MMD') {
+                $this->setMmdErpData();
+            }
             $record = ['status' => 'success', 'msg' => $title . ' Successfully.'];
         } else {
             $record = ['status' => 'error', 'msg' => $title . 'Not Successfully.'];
@@ -534,6 +571,79 @@ class TblMccShiftLockController extends \app\controllers\ChildController {
         $ftp_model = new TblFtpTxnLog();
         $ftp_model->exportData($data_array);
 //        }
+    }
+
+    public function setMmdErpData() {
+        $api = new WebApi();
+        $api->serverUrl = 'https://login.microsoftonline.com/2c11ed1f-0dff-46b9-94e9-8cbe83717417/oauth2/token';
+        $api->authentication = FALSE;
+//                        $bodyData = [
+//                            'grant_type' => 'client_credentials',
+//                            'client_id' => '20e569c1-4277-462b-b32c-01fc8516b4a8',
+//                            'client_secret' => '4wD7Q~o~2sb6uJY77edU7GBNPHDHFs0KFCxz3',
+//                            'resource' => 'https://mmd-test.sandbox.operations.dynamics.com'
+//                        ];
+        $bodyData = [
+            'grant_type' => 'client_credentials',
+            'client_id' => '1ebf2967-31cd-48aa-a95d-47872d9c1660',
+            'client_secret' => 'wSz7Q~.xR387Nc4bpNLaIQeJucLqv7.4a0Z5b',
+            'resource' => 'https://mmd-prd.operations.dynamics.com'
+        ];
+        $api->body = json_encode($bodyData);
+        $api->header_info = ['Cookie: buid=0.ASoAH-0RLP8NuUaU6Yy-g3F0FxUAAAAAAAAAwAAAAAAAAAAqAAA.AQABAAEAAAD--DLA3VO7QrddgJg7WevrvVFNQrKzy_CROckT6gVKweNqD3cIE_2e6sZvqDLziOl8pO63n7RLdlIlGAuwxD62Enrb1pzwLrCCeemK4klCumlwbqCg2J9DH0skWUTDYnkgAA; esctx=AQABAAAAAAD--DLA3VO7QrddgJg7Wevr6ffEbthd6xIgF9p_ALeJBUHpIFF8fjoU4RbhU6__vXSrMIrFkvh22Pkix_9Le-2mYya7B8dKnuUP_rbwfpzClwoS9Ky7NfwLUT_ZHQKSykQ94qj7dGTJP5ADjUi---2djJon1PEOjTv7Y6o3MstQTGOfn3_UANRXj-6c84mvRDIgAA; x-ms-gateway-slice=estsfd; stsservicecookie=estsfd; fpc=AmO0_u8SW0RBlh7R1d1Hu_TyqFelAQAAACCE-NgOAAAAMek5pAEAAACJhPjYDgAAAA'];
+
+        $response = $api->SapDataIntegration();
+        $responseData = json_decode(json_encode($response), true);
+
+        if (!empty($responseData['token_type']) && !empty($responseData['resource']) && !empty($responseData['access_token'])) {
+            $shiftLock = new TblMccShiftLockStaging();
+            $shiftLockData = $shiftLock->getLockShift(10);
+
+            foreach ($shiftLockData as $key => $value) {
+                $body = [];
+                $loopData = [];
+                $loopDetailData = [];
+                $value->updateAll(['data_post_status' => 1, 'picked_datetime' => date('Y-m-d H:i:s')], ['staging_code' => $value->staging_code]);
+
+                $loopData['orderNumber'] = $value->staging_code;
+                $mccVendor = Yii::$app->general->getforeignkey($value->mccPlantCode, 'vendor_code');
+                $loopData['vendorAccount'] = !empty($mccVendor) ? $mccVendor : '';
+                $loopData['orderDate'] = date('m-d-Y', strtotime($value->date_time_of_collection));
+                $loopData['companyId'] = '';
+                $loopData['sourceSystem'] = '';
+
+                $loopDetailData['itemNumber'] = 'RM000001';
+                $loopDetailData['quantity'] = $value->qty;
+                $loopDetailData['lineAmount'] = $value->amount;
+                $loopDetailData['locationId'] = '';
+                $loopDetailData['FAT'] = $value->avg_fat;
+                $loopDetailData['SNF'] = $value->avg_snf;
+
+                $body['purchaseOrderHeaderRequest'] = $loopData;
+                $body['purchaseOrderLineRequestList']['list'][] = $loopDetailData;
+                $postData = [];
+                $postData = json_encode($body);
+
+                $tokenType = $responseData['token_type'];
+                $token = $responseData['access_token'];
+                $resource = $responseData['resource'];
+                $request_url = $resource . '/api/services/TECServiceGroup/TECServices/savePurchaseOrder';
+
+                $api->serverUrl = $request_url;
+                $api->header_info = ['Authorization: ' . $tokenType . ' ' . $token];
+                $api->authentication = FALSE;
+                $api->body = $postData;
+
+                $response = $api->ExchangeData();
+                $responseData = json_decode(json_encode($response), false);
+
+                if (!empty($responseData)) {
+                    $status = $responseData->status;
+                    $resp_desc = $responseData->statusDescription;
+                    $value->updateAll(['data_post_status' => 2, 'resp_status' => $status, 'resp_desc' => $resp_desc, 'response_datetime' => date('Y-m-d H:i:s'), 'x_col1' => $responseData->guidD365, 'x_col2' => $responseData->fnoOrderNumber], ['staging_code' => $value->staging_code]);
+                }
+            }
+        }
     }
 
 }
