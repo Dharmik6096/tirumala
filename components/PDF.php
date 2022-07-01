7ca6c318ded970bc65755f8804ea07432aaf836f
Hardik Savani (IT42/RO) <it42@EIPL.LOCAL>
2022-07-01 12:34:29 +0530

    change in number format

--- components/PDF.php
+++ components/PDF.php
@@ -620,7 +620,7 @@ class PDF extends TCPDF {
                 $textContent .= $memberName;
                 $textContent .= ' ' . $acNo; //str_pad($acNo, 20, ' ', STR_PAD_RIGHT);
                 $inPad = 82 - (13 + strlen($memberName) + 1 + strlen($acNo));
-                $textContent .= str_pad(++$i, $inPad, ' ', STR_PAD_LEFT);
+                $textContent .= str_pad( ++$i, $inPad, ' ', STR_PAD_LEFT);
                 $textContent .= "\n";
                 $textContent .= str_pad('', 13, ' ', STR_PAD_LEFT);
                 $textContent .= $acIfsc; //str_pad($acIfsc, 22, ' ', STR_PAD_RIGHT);
@@ -819,13 +819,15 @@ class PDF extends TCPDF {
 //                }
                 $textContent .= "\n";
                 $textContent .= str_pad('', 11, ' ', STR_PAD_LEFT);
-                $textContent .= str_pad(number_format($total_qty_pm, 2), 8, ' ', STR_PAD_LEFT);
+//                $textContent .= str_pad(number_format($total_qty_pm, 2), 8, ' ', STR_PAD_LEFT);
+                $textContent .= str_pad($total_qty_pm, 8, ' ', STR_PAD_LEFT);
                 $textContent .= str_pad((!empty($eDevideCount) ? number_format(($total_FAT_pm / $eDevideCount), 2) : 0), 7, ' ', STR_PAD_LEFT);
                 $textContent .= str_pad((!empty($eDevideCount) ? number_format(($total_SNF_pm / $eDevideCount), 2) : 0), 7, ' ', STR_PAD_LEFT);
                 $textContent .= str_pad((!empty($total_qty_pm) && false ? number_format(($total_bm_amount_pm / $total_qty_pm), 2) : ''), 9, ' ', STR_PAD_LEFT);
 //                $textContent .= str_pad(number_format($total_bm_amount_pm, 2), 9, ' ', STR_PAD_LEFT);
                 $textContent .= str_pad($total_bm_amount_pm, 9, ' ', STR_PAD_LEFT);
-                $textContent .= str_pad(number_format($total_qty_am, 2), 8, ' ', STR_PAD_LEFT);
+//                $textContent .= str_pad(number_format($total_qty_am, 2), 8, ' ', STR_PAD_LEFT);
+                $textContent .= str_pad($total_qty_am, 8, ' ', STR_PAD_LEFT);
                 $textContent .= str_pad((!empty($mDevideCount) ? number_format(($total_FAT_am / $mDevideCount), 2) : 0), 6, ' ', STR_PAD_LEFT);
                 $textContent .= str_pad((!empty($mDevideCount) ? number_format(($total_SNF_am / $mDevideCount), 2) : 0), 7, ' ', STR_PAD_LEFT);
                 $textContent .= str_pad((!empty($total_qty_am) && false ? number_format(($total_bm_amount_am / $total_qty_am), 2) : ''), 9, ' ', STR_PAD_LEFT);
@@ -1141,7 +1143,7 @@ class PDF extends TCPDF {
                 $textContent .= str_pad('', 33, ' ', STR_PAD_LEFT);
                 $textContent .= $memName; //(!empty($value['basic']) && !empty($value['basic'][0]) ? $value['basic'][0]['member_name'] : '');
                 $inPad = 87 - (33 + strlen($memName));
-                $textContent .= str_pad( ++$i, $inPad, ' ', STR_PAD_LEFT);
+                $textContent .= str_pad(++$i, $inPad, ' ', STR_PAD_LEFT);
                 $textContent .= "\n";
                 $textContent .= str_pad('', 33, ' ', STR_PAD_LEFT);
                 $textContent .= 'Route Code: ' . (!empty($value['basic']) && !empty($value['basic'][0]) ? $value['basic'][0]['route'] : '');
