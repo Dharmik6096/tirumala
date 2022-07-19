21c0feb97f8d6864c2ae1656f0628ca94da5b622
Asmita Karmur (IT36/RO) <it36@EIPL.LOCAL>
2022-07-19 13:49:06 +0530

    unique check on milk collection summary

--- modules/collection/models/TblMilkCollectionSummary.php
+++ modules/collection/models/TblMilkCollectionSummary.php
@@ -60,11 +60,12 @@ class TblMilkCollectionSummary extends \app\models\ChildModel {
      */
     public function rules() {
         return [
-            [['date_time_of_collection', 'created_at', 'updated_at'], 'safe', 'on' => ['androidsync']],
-            [['milk_collection_summary_code', 'date_time_of_collection', 'created_at', 'updated_at'], 'safe'],
-            [['shift_code', 'sample_count', 'auto_count', 'manual_count'], 'safe'],
-            [['avg_fat', 'avg_snf', 'kg_fat', 'kg_snf', 'total_qty', 'avg_rate', 'total_amount', 'received_timestamp'], 'safe'],
-            [['union_code', 'plant_code', 'mcc_plant_code', 'bmc_code', 'dcs_code', 'created_by', 'updated_by', 'originating_org_code', 'originating_org_type', 'originating_type', 'x_col1', 'x_col2', 'x_col3', 'x_col4', 'x_col5'], 'safe'],
+                [['date_time_of_collection', 'created_at', 'updated_at'], 'safe', 'on' => ['androidsync']],
+                [['milk_collection_summary_code', 'date_time_of_collection', 'created_at', 'updated_at'], 'safe'],
+                [['shift_code', 'sample_count', 'auto_count', 'manual_count'], 'safe'],
+                [['avg_fat', 'avg_snf', 'kg_fat', 'kg_snf', 'total_qty', 'avg_rate', 'total_amount', 'received_timestamp'], 'safe'],
+                [['union_code', 'plant_code', 'mcc_plant_code', 'bmc_code', 'dcs_code', 'created_by', 'updated_by', 'originating_org_code', 'originating_org_type', 'originating_type', 'x_col1', 'x_col2', 'x_col3', 'x_col4', 'x_col5'], 'safe'],
+                [['dcs_code'], 'unique', 'targetAttribute' => ['date_time_of_collection', 'shift_code', 'dcs_code']]
         ];
     }
 
