4b54c5863ce0a3aedefc78251fe480c0aac0bcf1
Sunita Shiyal (IT30/CAPITAL) <it30@EIPL.LOCAL>
2022-08-30 15:26:07 +0530

    add sap route code + chagne lable of bmc to mcc in dashboard

--- modules/organisation/models/TblRouteMappingSearch.php
+++ modules/organisation/models/TblRouteMappingSearch.php
@@ -22,7 +22,7 @@ class TblRouteMappingSearch extends TblRouteMapping {
             [['route_code', 'morning_start_time', 'morning_end_time', 'route_name', 'union_code', 'local_name', 'evening_start_time', 'evening_end_time', 'route_type', 'from_type', 'from_dest', 'to_type', 'to_dest', 'created_at', 'created_by', 'updated_at', 'updated_by', 'unit', 'valid_from'], 'safe'],
             [['capacity', 'vehicle_type_code', 'is_active'], 'integer'],
             [['route_length_kms'], 'number'],
-            [['route_code_ex', 'ref_code'], 'safe'],
+            [['route_code_ex', 'ref_code', 'sap_route_code'], 'safe'],
         ];
     }
 
@@ -143,6 +143,7 @@ class TblRouteMappingSearch extends TblRouteMapping {
                 ->andFilterWhere(['like', 'tbl_route_mapping.from_type', $this->from_type])
                 ->andFilterWhere(['like', 'tbl_route_mapping.from_dest', $this->from_dest])
                 ->andFilterWhere(['like', 'tbl_route_mapping.to_type', $this->to_type])
+                ->andFilterWhere(['like', 'tbl_route_mapping.sap_route_code', $this->sap_route_code])
                 ->andFilterWhere(['like', 'tbl_route_mapping.to_dest', $this->to_dest]);
 
         return $dataProvider;
