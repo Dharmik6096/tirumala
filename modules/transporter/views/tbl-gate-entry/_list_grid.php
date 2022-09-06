944ddc946f4193321245ae7829d4acc755a1e316
Asmita Karmur (IT36/RO) <it36@EIPL.LOCAL>
2022-09-06 16:35:10 +0530

    Gate Entry Print Option in transaction grid

--- modules/transporter/views/tbl-gate-entry/_list_grid.php
+++ modules/transporter/views/tbl-gate-entry/_list_grid.php
@@ -38,6 +38,10 @@ use webvimark\modules\UserManagement\components\GhostHtml;
                 $options = ['data-toggle' => 'tooltip', 'data-placement' => 'top', 'data-original-title' => 'Edit', 'class' => 'edit-record', 'data-val' => $model->gate_entry_code, 'data-name' => $model->gate_entry_code, 'title' => Yii::t('app', 'Edit')];
                 return GhostHtml::a_alert('<i class="fa fa-pencil"></i>', ['/transporter/tbl-gate-entry/update'], $options);
             },
+            'print-gate-pass' => function ($url, $model) {
+                $options = ['target' => '_blank', 'title' => Yii::t('app', 'Print Gate Pass'), 'data-toggle' => 'tooltip', 'data-placement' => 'top', 'data-original-title' => Yii::t('app', 'Print Gate Pass')];
+                return GhostHtml::a('<i class="fa fa-file-pdf-o"></i>', ['/transporter/tbl-gate-entry/print-gate-pass', 'id' => $model->gate_entry_code], $options);
+            },
         ]
     ];
     Yii::$app->grid->bind($dataProvider, $searchModel, $grid_option);
