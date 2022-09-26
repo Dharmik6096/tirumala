6f438403eea06dba6604130679ab3cbd2fe1436e
Sunita Shiyal (IT30/CAPITAL) <it30@EIPL.LOCAL>
2022-09-26 16:57:50 +0530

    change extention file which upload at FTP - dodla

--- modules/bkgprocess/Bkgprocess.php
+++ modules/bkgprocess/Bkgprocess.php
@@ -74,14 +74,14 @@ class Bkgprocess extends \yii\base\Module {
 //                'ftp_path' => 'WQ/Inbound'
 //            ],
             'TblBmcCollection_dodla_VM' => [
-                'ext' => '.csv',
+                'ext' => '.xlsx',
                 'param' => 'union_code,mcc_plant_code,bmc_code,from_date',
                 'sp_name' => 'mis_bmc_collection_vm',
                 'export_title' => 'mcc_plant_code+_+WQ+_+applicable_date:date+_+shift_code',
                 'file_path' => str_replace('\\', '/', realpath(\Yii::$app->basePath . '/../')) . \Yii::$app->params['FTPDirPath'] . 'upload/',
             ],
             'TblBmcCollection_dodla_WQ' => [
-                'ext' => '.csv',
+                'ext' => '.xlsx',
                 'param' => 'union_code,mcc_plant_code,bmc_code,from_date',
                 'sp_name' => 'mis_bmc_collection_wq',
                 'export_title' => 'mcc_plant_code+_+WQ+_+applicable_date:date+_+shift_code',
