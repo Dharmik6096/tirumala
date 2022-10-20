50b6e25da08690c522af5e75084bf107b835a7af
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2022-10-20 18:19:32 +0530

    BMC Collection Detail in Shift Lock View

--- modules/collection/models/TblBmcCollectionSearch.php
+++ modules/collection/models/TblBmcCollectionSearch.php
@@ -119,6 +119,33 @@ class TblBmcCollectionSearch extends TblBmcCollection {
         return $dataProvider;
     }
 
+    public function bmcwisesearch($params) {
+        $query = TblBmcCollection::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'mcc_plant_code' => $this->mcc_plant_code,
+            'date_time_of_collection' => $this->date_time_of_collection,
+            'shift_code' => $this->shift_code,
+        ]);
+        
+        return $dataProvider;
+    }
+
     public function gridsearch($params) {
         $this->load($params);
 
