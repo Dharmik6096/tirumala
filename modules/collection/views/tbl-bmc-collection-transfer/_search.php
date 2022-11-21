8e996d74f29fce107a537baee57457c9ecfed516
Sunita Shiyal (IT30/CAPITAL) <it30@EIPL.LOCAL>
2022-11-21 18:20:42 +0530

    resolve issue of route not load + apply solution of pass mcc as bmc

--- modules/collection/views/tbl-bmc-collection-transfer/_search.php
+++ modules/collection/views/tbl-bmc-collection-transfer/_search.php
@@ -26,7 +26,7 @@ use yii\widgets\ActiveForm;
         <?= Yii::$app->dropdown->plant_mcc($model, $form, 'tblbmccollectiontransfersearch-from_plant_code', 'from_mcc_plant_code', $model->getAttributeLabel('from_mcc_plant_code'), FALSE, ''); ?>
     </div>  
     <div class="col-sm-2">
-        <?= Yii::$app->dropdown->all_routes($model, $form, 'tblbmccollectiontransfersearch-from_plant_code,tblbmccollectiontransfersearch-from_mcc_plant_code', 'route_code', $model->getAttributeLabel('route_code')); ?>
+        <?= Yii::$app->dropdown->all_routes($model, $form, 'tblbmccollectiontransfersearch-from_plant_code,tblbmccollectiontransfersearch-from_mcc_plant_code,tblbmccollectiontransfersearch-from_mcc_plant_code', 'route_code', $model->getAttributeLabel('route_code')); ?>
     </div>
     <div class="col-sm-2">
         <?= Yii::$app->controls->date($model, $form, 'from_date', 'form-group col-sm-2 padding-left-5 padding-right-5', false, false, false, TRUE); ?>
