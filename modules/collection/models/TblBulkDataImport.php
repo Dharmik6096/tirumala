7c00e9020664cbc9d86add8b5861956b713df5f5
Sunita Shiyal (IT30/CAPITAL) <it30@EIPL.LOCAL>
2023-04-06 15:50:19 +0530

    resolve issue of validate antibiotic

--- modules/collection/models/TblBulkDataImport.php
+++ modules/collection/models/TblBulkDataImport.php
@@ -72,9 +72,9 @@ class TblBulkDataImport extends \yii\db\ActiveRecord {
             [['antibiotic'], 'required', 'when' => function ($model) {
                     return \Yii::$app->session->get('eiplCode') == 'PRABHAT';
                 }, 'on' => ['bmc_collection_route', 'bmc_collection_can', 'bmc_collection_bmc_route', 'bmc_collection_bmc_can', 'bmc_collection_route_can', 'bmc_collection_bmc_route_can', 'bmc_collection_mapped', 'bmc_collection']],
-            [['antibiotic'], 'required', 'when' => function ($model) {
+            [['antibiotic'], function ($attribute, $params) {
                     Yii::$app->general->validateGlobalStatic($this, $attribute, 'antibiotic');
-                }, 'on' => ['bmc_collection_route', 'bmc_collection_can', 'bmc_collection_bmc_route', 'bmc_collection_bmc_can', 'bmc_collection_route_can', 'bmc_collection_bmc_route_can', 'bmc_collection_mapped', 'bmc_collection']]
+                }, 'on' => ['bmc_collection_route', 'bmc_collection_can', 'bmc_collection_bmc_route', 'bmc_collection_bmc_can', 'bmc_collection_route_can', 'bmc_collection_bmc_route_can', 'bmc_collection_mapped', 'bmc_collection']],
         ];
     }
 
