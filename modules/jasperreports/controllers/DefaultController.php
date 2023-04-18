2f603ec13e7ab43c0de94412b3621817961e177b
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2023-04-18 15:36:44 +0530

    Milk bill report for JGF

--- modules/jasperreports/controllers/DefaultController.php
+++ modules/jasperreports/controllers/DefaultController.php
@@ -301,6 +301,11 @@ class DefaultController extends \app\controllers\ChildController {
         return $this->actionIndex();
     }
 
+    public function actionVendorMilkBillJgf() {
+        $this->report = 'VendorMilkBillJgf';
+        return $this->actionIndex();
+    }
+
     /* Jasper Call */
 
     private function LoadReport($model) {
@@ -720,6 +725,12 @@ class DefaultController extends \app\controllers\ChildController {
                 'param' => 'p_union_code,p_plant_code,p_mcc_code,p_bmc_code,p_customer_type,p_customer_code,p_payment_cycle_code:type_check,p_language_code,p_report_name',
                 'path' => 'vsp/VendorMilkBillSNMilk',
                 'scenario' => 'VendorMilkBillSnmilk',
+                'title' => '616 - Milk Bill',
+            ],
+            'VendorMilkBillJgf' => [
+                'param' => 'p_union_code,p_plant_code,p_mcc_code,p_bmc_code,p_customer_type,p_customer_code,p_payment_cycle_code:type_check,p_language_code,p_report_name',
+                'path' => 'vsp/VSPPaymentJGF',
+                'scenario' => 'VendorMilkBillJgf',
                 'title' => '616 - Milk Bill',
             ],
         ];
