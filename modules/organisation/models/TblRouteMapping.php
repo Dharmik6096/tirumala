5fdeedc3f031a81a65311923284a063d3508adaf
Atul Patel (IT46/CAPITAL) <it46@EIPL.LOCAL>
2023-05-10 15:28:33 +0530

    sendbox created but app side issue fixed

--- modules/organisation/models/TblRouteMapping.php
+++ modules/organisation/models/TblRouteMapping.php
@@ -346,10 +346,10 @@ class TblRouteMapping extends \app\models\ChildModel {
 
     public function afterSave($insert, $changedAttributes) {
         $sentboxArray = [];
-        $type = trim($this->to_type);
-        if ($type == 'bmc') {
+        $this->to_type = trim($this->to_type);
+        if ($this->to_type == 'bmc') {
             $sentboxArray = Yii::$app->general->getSentBoxCodes('', '', $this->to_dest);
-        } else if ($type == 'mcc') {
+        } else if ($this->to_type == 'mcc') {
             $sentboxArray = Yii::$app->general->getSentBoxCodes('', $this->to_dest, '');
         }
         foreach ($sentboxArray as $sent) {
