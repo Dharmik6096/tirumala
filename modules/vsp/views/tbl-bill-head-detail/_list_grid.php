6e044fd6ae0909281ce93bc4bc7a1ad18b597080
Asmita Chavda (IT36/CAPITAL) <it36@EIPL.LOCAL>
2023-07-06 16:34:57 +0530

    Bill Head Detail - Add Remarks Column

--- modules/vsp/views/tbl-bill-head-detail/_list_grid.php
+++ modules/vsp/views/tbl-bill-head-detail/_list_grid.php
@@ -4,17 +4,17 @@ use yii\helpers\Html;
 use kartik\grid\GridView;
 
 $attribute = [
-    ['attribute' => 'customer_type', 'value' => 'customer_type', 'value' => function($model) {
+        ['attribute' => 'customer_type', 'value' => 'customer_type', 'value' => function($model) {
             return Yii::$app->general->getforeignkey($model->customerType, 'customer_desc');
         }, 'filter' => false],
-    ['attribute' => 'customer_code', 'filter' => false, 'label' => Yii::t('app', 'Code')],
-    ['attribute' => 'customer_name', 'label' => Yii::t('app', 'Name'), 'value' => function($model) {
+        ['attribute' => 'customer_code', 'filter' => false, 'label' => Yii::t('app', 'Code')],
+        ['attribute' => 'customer_name', 'label' => Yii::t('app', 'Name'), 'value' => function($model) {
             return Yii::$app->general->getCustomer($model, $model->customer_type);
         }, 'filter' => false],
-    ['attribute' => 'bill_head_code', 'value' => function($model) {
+        ['attribute' => 'bill_head_code', 'value' => function($model) {
             return Yii::$app->general->getforeignkey($model->billHeadCode, 'bill_head_name');
         }, 'filter' => false,],
-    [
+        [
         'attribute' => 'transaction_date',
         'filterType' => GridView::FILTER_DATE,
         'filterWidgetOptions' => [
@@ -28,7 +28,8 @@ $attribute = [
 //            return Yii::$app->controls->view_date(Yii::$app->general->getforeignkey($model->paymentCycleCode, 'from_date')) . ' to ' . Yii::$app->controls->view_date(Yii::$app->general->getforeignkey($model->paymentCycleCode, 'to_date'));
 //        }, 'filter' => false],
     ['attribute' => 'no_installment', 'filter' => false],
-    ['attribute' => 'amount', 'filter' => false],
+        ['attribute' => 'amount', 'filter' => false],
+        ['attribute' => 'remarks', 'filter' => false],
 ];
 $grid_option = [
     'id' => 'bill-head-detail-list-grid',
