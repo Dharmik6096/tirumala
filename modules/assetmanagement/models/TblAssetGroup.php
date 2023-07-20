fb6fd2587b5dbe0cbcd7ef95e3a5a8f62e7cf791
Tejas Thakkar <tejas.thakkar@yamatech.in>
2023-07-20 11:27:53 +0530

    Milk Rate bug fix

--- modules/assetmanagement/models/TblAssetGroup.php
+++ modules/assetmanagement/models/TblAssetGroup.php
@@ -31,14 +31,14 @@ class TblAssetGroup extends \app\models\ChildModel {
      */
     public function rules() {
         return [
-            [['asset_group_name', 'asset_group_code', 'union_code', 'reference_code'], 'required'],
+            [['asset_group_name', 'union_code', 'reference_code'], 'required'],
             [['asset_group_name', 'asset_group_code', 'reference_code'], 'unique'],
             [['asset_group_name', 'created_by', 'updated_by'], 'string'],
             [['is_active'], 'default', 'value' => 1],
             [['created_at', 'updated_at', 'reference_code'], 'safe'],
             [['local_name'], function ($attribute, $params) {
-            Yii::$app->general->vaildateLocalField($this, $attribute, $params);
-        }, 'skipOnEmpty' => false],
+                    Yii::$app->general->vaildateLocalField($this, $attribute, $params);
+                }, 'skipOnEmpty' => false],
             [['union_code'], 'exist', 'skipOnError' => true, 'targetClass' => TblUnions::className(), 'targetAttribute' => ['union_code' => 'union_code']],
             [['asset_group_code'], 'integer'],
             [['asset_group_code'], 'string', 'min' => 8],
