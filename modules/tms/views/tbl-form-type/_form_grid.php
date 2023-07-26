1a66fafe3cf19e2bc3f9010292fa12038bdfcff5
Shyam Mor (IT39/CAPITAL) <it39@EIPL.LOCAL>
2023-07-26 17:17:15 +0530

    tms module code modified as per ma'am suggest and history model added

--- modules/tms/views/tbl-form-type/_form_grid.php
+++ modules/tms/views/tbl-form-type/_form_grid.php
@@ -1,8 +1,10 @@
 <?php
 
 $attribute = [
-    ['attribute' => 'union_code', 'filter' => true, 'visible' => false],
-    ['attribute' => 'task_type_code', 'value' => function($model) {
+    ['attribute' => 'union_code', 'value' => function ($model) {
+        return Yii::$app->general->getforeignkey($model->unionCode, 'union_name');
+    }, 'filter' => false, 'visible' => false],
+    ['attribute' => 'task_type_code', 'value' => function ($model) {
         return Yii::$app->general->getforeignkey($model->taskType, 'task_type');
     }],
     ['attribute' => 'form_name', 'filter' => true],
@@ -10,7 +12,7 @@ $attribute = [
 ];
 
 $grid_option = [
-    'id' => 'task-type-list',
+    'id' => 'form-type-list',
     'attributes' => $attribute,
     'active_column' => true,
     'actions' => [
--- modules/tms/views/tbl-form-type/create.php
+++ modules/tms/views/tbl-form-type/create.php
@@ -1,6 +1,5 @@
 <?php
-$this->title =Yii::$app->label->title('create', 'Form Type');
-
+$this->title = Yii::$app->label->title('create', 'Form Type');
 ?>
 <div class="panel panel-default panel-main">
     <div class="panel-heading"><?= $this->title ?></div>
