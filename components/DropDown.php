8988e1521c70fcb1a17097eef3cd98a712bbe38f
Tejas Thakkar <tejas.thakkar@yamatech.in>
2023-08-11 18:01:04 +0530

    DCS Manual Collection Range Approval mechanism Add

--- components/DropDown.php
+++ components/DropDown.php
@@ -1679,6 +1679,11 @@ class DropDown extends Component {
                 'prompt' => Yii::t('app', 'Select Repeat Interval'),
                 'data' => ['0' => Yii::t('app', 'DO not Repeat'), '1' => Yii::t('app', 'Daily'), '2' => Yii::t('app', 'Weekly')],
             ],
+            'dcs_manual_collection_range_status' => [
+                'name' => 'status',
+                'prompt' => Yii::t('app', 'Select'),
+                'data' => [1 => Yii::t('app', 'Pending'), 2 => Yii::t('app', 'Approved'), 3 => Yii::t('app', 'Reject'), 4 => Yii::t('app', 'Close')],
+            ],
         ];
         return $records[$l];
     }
--- modules/organisation/views/tbl-allow-dcs-manual-collection-range/_form_grid.php
+++ modules/organisation/views/tbl-allow-dcs-manual-collection-range/_form_grid.php
@@ -5,9 +5,10 @@ use kartik\grid\GridView;
 use webvimark\modules\UserManagement\components\GhostHtml;
 use yii\helpers\Url;
 use yii\web\View;
-
+use yii\widgets\ActiveForm;
 ?>
 <?php
+$form = ActiveForm::begin();
 
 $attribute = [
     ['attribute' => 'union_code', 'value' => function($model) {
@@ -65,6 +66,19 @@ $attribute = [
             return ($model->is_quality_manual == 1) ? 'Yes' : 'No';
         }
     ],
+    [
+        'attribute' => 'status',
+        'filter' => Yii::$app->dropdown->dropdownfilterStatic('dcs_manual_collection_range_status', $searchModel, 'status'),
+        'value' => function($model) {
+            return isset(Yii::$app->dropdown->getRecords('dcs_manual_collection_range_status')['data'][$model->status]) ? Yii::$app->dropdown->getRecords('dcs_manual_collection_range_status')['data'][$model->status] : 'N/A';
+        }
+    ],
+    [
+        'attribute' => 'remark', 'filter' => false,
+        'value' => function($model) {
+            return $model->remark;
+        }
+    ],
 ];
 
 $grid_option = [
@@ -73,9 +87,88 @@ $grid_option = [
     'active_column' => false,
     'actions' => [
 //        'view' => TRUE,
-        'update' => TRUE,
+        'update' => FALSE,
+        'status' => function ($url, $model) {
+            $id = $model->manual_collection_code;
+            $type = 'DCS';
+            $class = $model->status == 1 ? '' : 'link-disable disabled';
+            $options = [
+                'data-toggle' => 'tooltip',
+                'data-placement' => 'top',
+                'data-original-title' => 'Status',
+                'class' => 'update-status' . $class,
+                'data-val' => $id,
+                'data-model-id' => $id,
+                'data-name' => $type,
+                'onclick' => 'openStatusPopup(event, this)',
+            ];
+            return GhostHtml::a_alert('<i class="fa fa-info-circle"></i>', $url, $options);
+        },
     ]
 ];
 
 Yii::$app->grid->bind($dataProvider, $searchModel, $grid_option);
 ?>
+<div class="modal fade" id="status-popup-modal" tabindex="-1" role="dialog" aria-labelledby="status-popup-modal-label" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="status-popup-modal-label">Update Status</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <?= Yii::$app->dropdown->dropdownStatic('dcs_manual_collection_range_status', $model, $form, 'form-group', 'Status', false, 'status', false); ?>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="form-group">
+                            <label for="remark-input" style="font-size: 10px !important; white-space: nowrap;">Remark</label>
+                            <textarea class="form-control" id="remark-input" rows="4"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" onclick="saveStatus()">Save</button>
+            </div>
+        </div>
+    </div>
+</div>
+<?php ActiveForm::end(); ?>
+<script>
+    function openStatusPopup(event, element) {
+        event.preventDefault();
+        var modelId = $(element).data('model-id');
+        $('#status-popup-modal').data('model-id', modelId);
+        $('#tblallowdcsmanualcollectionrange-status').val('').trigger('change');
+        $('#remark-input').val('');
+        $('#status-popup-modal').modal('show');
+    }
+    function saveStatus() {
+        var selectedStatus = $('#tblallowdcsmanualcollectionrange-status').val();
+        var remark = $('#remark-input').val();
+        var modelId = $('#status-popup-modal').data('model-id');
+        $.ajax({
+            url: '" . Url::to(['/organisation/tbl-allow-dcs-manual-collection-range/update-status']) . "',
+            type: 'POST',
+            data: {
+                id: modelId,
+                status: selectedStatus,
+                remark: remark
+            },
+            success: function (response) {
+                $('#status-popup-modal').modal('hide');
+                location.reload();
+            },
+            error: function (xhr, status, error) {
+                console.error('Update failed:', error);
+            }
+        });
+    }
+</script>
