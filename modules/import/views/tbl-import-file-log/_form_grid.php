30c38bfeaaa273897da9c5fcb92569bcec7498c6
Asmita Chavda (IT36/CAPITAL) <it36@EIPL.LOCAL>
2023-08-12 16:08:27 +0530

    Temporary Resolve - Import File Error/Success download issue

--- modules/import/views/tbl-import-file-log/_form_grid.php
+++ modules/import/views/tbl-import-file-log/_form_grid.php
@@ -5,7 +5,7 @@ use yii\helpers\Url;
 use kartik\grid\GridView;
 
 $attribute = [
-    ['attribute' => 'union_code', 'value' => function($model) {
+        ['attribute' => 'union_code', 'value' => function($model) {
             return Yii::$app->general->getforeignkey($model->unionCode, 'union_name');
         }, 'filter' => false, 'visible' => FALSE],
     'log_id',
@@ -14,13 +14,13 @@ $attribute = [
     'total_count',
     'success_count',
     'error_count',
-    [
+        [
         'attribute' => 'status',
         'filter' => Yii::$app->dropdown->dropdownfilterStatic('file_status', $searchModel, 'status'),
         'value' => function($model) {
             return isset(Yii::$app->dropdown->getRecords('file_status')['data'][$model->status]) ? Yii::$app->dropdown->getRecords('file_status')['data'][$model->status] : '';
         }],
-    ['attribute' => 'created_at',
+        ['attribute' => 'created_at',
         'filterType' => GridView::FILTER_DATE,
         'filterWidgetOptions' => [
             'pluginOptions' => ['format' => 'dd-mm-yyyy',
@@ -29,7 +29,7 @@ $attribute = [
         'value' => function($model) {
             return Yii::$app->controls->view_datetime($model->created_at);
         }],
-    [
+        [
         'attribute' => 'created_by',
         'value' => function($model) {
             return Yii::$app->general->getforeignkey($model->userCode, 'name');
@@ -44,7 +44,7 @@ $attribute = [
         'value' => function($model) {
             return Yii::$app->controls->view_datetime($model->pick_datetime);
         }],
-    ['attribute' => 'cron_pick_datetime',
+        ['attribute' => 'cron_pick_datetime',
         'filterType' => GridView::FILTER_DATE,
         'filterWidgetOptions' => [
             'pluginOptions' => ['format' => 'dd-mm-yyyy',
@@ -53,7 +53,7 @@ $attribute = [
         'value' => function($model) {
             return Yii::$app->controls->view_datetime($model->cron_pick_datetime);
         }],
-    ['attribute' => 'response_datetime',
+        ['attribute' => 'response_datetime',
         'filterType' => GridView::FILTER_DATE,
         'filterWidgetOptions' => [
             'pluginOptions' => ['format' => 'dd-mm-yyyy',
@@ -62,39 +62,38 @@ $attribute = [
         'value' => function($model) {
             return Yii::$app->controls->view_datetime($model->response_datetime);
         }],
-    [
-    'attribute' => 'interval',
+        [
+        'attribute' => 'interval',
         'filter' => FALSE,
         'value' => function($model) {
-        $fromDate = !empty($model->cron_pick_datetime) ? $model->cron_pick_datetime : $model->pick_datetime;
-        if ($fromDate !== null && !empty($model->response_datetime)) {
-            $datetime1 = new DateTime($fromDate);
-            $datetime2 = new DateTime($model->response_datetime);
-            $interval = $datetime1->diff($datetime2);
-            return $interval->format('%h') . ":" . $interval->format('%i') . ":" . $interval->format('%s');
-        } else {
-            return '0:0:0';
+            $fromDate = !empty($model->cron_pick_datetime) ? $model->cron_pick_datetime : $model->pick_datetime;
+            if ($fromDate !== null && !empty($model->response_datetime)) {
+                $datetime1 = new DateTime($fromDate);
+                $datetime2 = new DateTime($model->response_datetime);
+                $interval = $datetime1->diff($datetime2);
+                return $interval->format('%h') . ":" . $interval->format('%i') . ":" . $interval->format('%s');
+            } else {
+                return '0:0:0';
+            }
         }
-    }
-],
-
+    ],
     'response_msg',
-    [
+        [
         'attribute' => 'error_file_path',
         'format' => 'raw',
         'value' => function($model) {
             if (!empty($model->error_file_path)) {
                 $absoluteBaseUrl = Url::base(true);
-                return Html::a('<i class="fa fa-download"><i/>', $absoluteBaseUrl . $model->error_file_path, ['target' => '_blank']);
+                return Html::a('<i class="fa fa-download"><i/>', str_replace('/web/web/', '/web/', $absoluteBaseUrl . $model->error_file_path), ['target' => '_blank']);
             }
         }],
-    [
+        [
         'attribute' => 'success_file_path',
         'format' => 'raw',
         'value' => function($model) {
             if (!empty($model->success_file_path)) {
                 $absoluteBaseUrl = Url::base(true);
-                return Html::a('<i class="fa fa-download"><i/>', $absoluteBaseUrl . $model->success_file_path, ['target' => '_blank']);
+                return Html::a('<i class="fa fa-download"><i/>', str_replace('/web/web/', '/web/', $absoluteBaseUrl . $model->success_file_path), ['target' => '_blank']);
             }
         }],
 ];
