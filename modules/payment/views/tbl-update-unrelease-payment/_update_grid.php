96f04c2032b4dcb23217a89dee9083e0c92eda04
Shyam Mor (IT39/CAPITAL) <it39@EIPL.LOCAL>
2023-08-16 18:41:56 +0530

    update unrelease payment validation add

--- modules/payment/views/tbl-update-unrelease-payment/_update_grid.php
+++ modules/payment/views/tbl-update-unrelease-payment/_update_grid.php
@@ -7,9 +7,9 @@ use yii\helpers\Html;
 use kartik\grid\GridView;
 
 $form = ActiveForm::begin([
-            'id' => 'update-unrelease-payment',
-            'action' => Url::to(['update-unrelease-payment']),
-        ]);
+    'id' => 'update-unrelease-payment',
+    'action' => Url::to(['update-unrelease-payment']),
+]);
 echo Html::hiddenInput('payment_type', $searchParameter->payment_type);
 
 $member_payment = ($searchParameter->payment_type == 'MEMBER') ? TRUE : FALSE;
@@ -20,7 +20,7 @@ $vendor_payment = ($searchParameter->payment_type == 'VENDOR') ? TRUE : FALSE;
 
     <?php
     $attribute = [
-            [
+        [
             'class' => 'kartik\grid\CheckboxColumn',
             'rowSelectedClass' => GridView::TYPE_SUCCESS,
             'headerOptions' => ['class' => 'skip-export'], 'contentOptions' => ['class' => 'skip-export'],
@@ -29,25 +29,25 @@ $vendor_payment = ($searchParameter->payment_type == 'VENDOR') ? TRUE : FALSE;
                 return ['class' => 'checkbox-collection', 'value' => $values];
             }
         ],
-            ['attribute' => 'customer_type', 'value' => 'customer_type', 'value' => function($model) {
-                return Yii::$app->general->getforeignkey($model->customerType, 'customer_desc');
-            }, 'visible' => $vendor_payment],
-            ['attribute' => 'customer_code', 'label' => Yii::t('app', 'Code'), 'visible' => $vendor_payment],
-            ['attribute' => 'customer_ex_code', 'label' => Yii::t('app', 'Code Ex.'), 'value' => function($model) {
-                return Yii::$app->general->getCustomer($model, $model->customer_type, TRUE);
-            }, 'visible' => $vendor_payment],
-            ['attribute' => 'customer_name', 'label' => Yii::t('app', 'Name'), 'value' => function($model) {
-                return Yii::$app->general->getCustomer($model, $model->customer_type);
-            }, 'visible' => $vendor_payment],
-            ['attribute' => 'dcs_code', 'label' => Yii::t('app', 'DCS Code'), 'visible' => $member_payment],
-            ['attribute' => 'ex_code', 'label' => Yii::t('app', 'Code Ex.'), 'value' => function($model) {
-                return Yii::$app->general->getforeignkey($model->dcsCode, 'dcs_code_ex');
-            }, 'visible' => $member_payment],
-            ['attribute' => 'dcs_name', 'value' => function($model) {
-                return Yii::$app->general->getforeignkey($model->dcsCode, 'dcs_name');
-            }, 'label' => Yii::t('app', 'DCS'), 'visible' => $member_payment],
-            ['attribute' => 'member_count', 'filter' => FALSE, 'visible' => $member_payment],
-            [
+        ['attribute' => 'customer_type', 'value' => 'customer_type', 'value' => function ($model) {
+            return Yii::$app->general->getforeignkey($model->customerType, 'customer_desc');
+        }, 'visible' => $vendor_payment],
+        ['attribute' => 'customer_code', 'label' => Yii::t('app', 'Code'), 'visible' => $vendor_payment],
+        ['attribute' => 'customer_ex_code', 'label' => Yii::t('app', 'Code Ex.'), 'value' => function ($model) {
+            return Yii::$app->general->getCustomer($model, $model->customer_type, TRUE);
+        }, 'visible' => $vendor_payment],
+        ['attribute' => 'customer_name', 'label' => Yii::t('app', 'Name'), 'value' => function ($model) {
+            return Yii::$app->general->getCustomer($model, $model->customer_type);
+        }, 'visible' => $vendor_payment],
+        ['attribute' => 'dcs_code', 'label' => Yii::t('app', 'DCS Code'), 'visible' => $member_payment],
+        ['attribute' => 'ex_code', 'label' => Yii::t('app', 'Code Ex.'), 'value' => function ($model) {
+            return Yii::$app->general->getforeignkey($model->dcsCode, 'dcs_code_ex');
+        }, 'visible' => $member_payment],
+        ['attribute' => 'dcs_name', 'value' => function ($model) {
+            return Yii::$app->general->getforeignkey($model->dcsCode, 'dcs_name');
+        }, 'label' => Yii::t('app', 'DCS'), 'visible' => $member_payment],
+        ['attribute' => 'member_count', 'filter' => FALSE, 'visible' => $member_payment],
+        [
             'attribute' => 'payment_cycle_code',
             'value' => function ($model) {
                 $fromDatetime = Yii::$app->controls->view_date($model->from_datetime);
@@ -56,18 +56,24 @@ $vendor_payment = ($searchParameter->payment_type == 'VENDOR') ? TRUE : FALSE;
             },
             'filter' => FALSE,
         ],
-            [
+        [
             'attribute' => $vendor_payment ? 'final_pay' : 'final_amount',
             'filter' => FALSE,
         ],
-            ['attribute' => 'disburse_amount', 'filter' => FALSE],
-            [
+        ['attribute' => 'disburse_amount', 'filter' => FALSE],
+        [
             'attribute' => 'disburse_date',
-            'value' => function ($model) {
-                return Yii::$app->controls->view_datetime($model->disburse_date);
+            'value' => function ($model) use ($vendor_payment) {
+                $tblId = $vendor_payment ? $model->vsp_payment_code : $model->payment_sumary_code;
+
+                echo Html::hiddenInput('disburse_date', $model->disburse_date,
+                    ['id' => $tblId . '-disburse-date']
+                );
+                
+                return Yii::$app->controls->view_date($model->disburse_date);
             }, 'filter' => FALSE
         ],
-            [
+        [
             'attribute' => 'hold_reason',
             'format' => 'raw',
             'contentOptions' => ['class' => 'no_padding_input hide_help_block'],
@@ -76,13 +82,20 @@ $vendor_payment = ($searchParameter->payment_type == 'VENDOR') ? TRUE : FALSE;
                 return '<span class=\'hold_reason\'>' . Yii::$app->dropdown->dropdown('hold_reason', $model, $form, '', FALSE, FALSE, '[' . $tblId . ']hold_reason', FALSE, TRUE, $model->hold_reason) . '</span>';
             },
         ],
-            [
+        [
             'attribute' => 'release_date',
             'format' => 'raw',
             'contentOptions' => ['class' => 'no_padding_input hide_help_block'],
             'value' => function ($model, $index) use ($form, $vendor_payment) {
                 $tblId = $vendor_payment ? $model->vsp_payment_code : $model->payment_sumary_code;
-                return '<span class=\'date_change\'>' . $form->field($model, '[' . $tblId . ']release_date')->textInput(['value' => $model->release_date, 'class' => 'form-control',])->label(FALSE) . '</span>';
+                $currentDate = date('d-m-Y');
+                return '<span class=\'date_change\'>' . $form->field($model, '[' . $tblId . ']release_date')->widget(\yii\widgets\MaskedInput::class, [
+                    'mask' => '99-99-9999',
+                    'options' => [
+                        'class' => 'form-control',
+                        'value' => $currentDate,
+                    ],
+                ])->label(false) . '</span>';
             },
         ],
     ];
@@ -97,12 +110,12 @@ $vendor_payment = ($searchParameter->payment_type == 'VENDOR') ? TRUE : FALSE;
     ?>
 </div>
 <div class="panel-footer">
-<?php
-if (!empty($dataProvider->getModels())) {
-    echo Html::button(Yii::t('app', 'Update'), ['class' => 'btn btn-primary', 'id' => 'update-selected']);
-    echo Yii::$app->controls->custombutton('Cancel', 'index');
-}
-?>
+    <?php
+    if (!empty($dataProvider->getModels())) {
+        echo Html::button(Yii::t('app', 'Update'), ['class' => 'btn btn-primary', 'id' => 'update-selected']);
+        echo Yii::$app->controls->custombutton('Cancel', 'index');
+    }
+    ?>
 </div>
 
 <?php ActiveForm::end(); ?>
@@ -117,6 +130,7 @@ $('#update-selected').click(function(e) {
     if (checkBoxCount > 0) {
         var paymentType = $('[name=\"payment_type\"]').val();
         var isValid = true;
+        var msg = '';
         $('.checkbox-collection:checked').each(function() {
             var rowId = $(this).val();
             if(paymentType=='VENDOR'){
@@ -127,23 +141,38 @@ $('#update-selected').click(function(e) {
                 var releaseDate = $('#tblmemberpaymentsummary-' + rowId + '-release_date').val();
             }
             // Apply validation only for selected rows
+            var disburseDate = document.getElementById(rowId + '-disburse-date').value;
+            disburseDate = new Date(disburseDate);
+            disburseDate.toString();            
+
+            var currentDate = new Date();
+            var parts = releaseDate.split('-');
+            var passedDateObj = new Date(parts[2], parts[1] - 1, parts[0]);
+            
             if (!holdReason || !releaseDate) {
                 isValid = false;
-              if(paymentType=='VENDOR'){
-                $('#tblvsppayment-' + rowId + '-hold_reason').addClass('has-error');
-                $('#tblvsppayment-' + rowId + '-release_date').addClass('has-error');
-              }else{
-                $('#tblmemberpaymentsummary-' + rowId + '-hold_reason').addClass('has-error');
-                $('#tblmemberpaymentsummary-' + rowId + '-release_date').addClass('has-error');
-             }   
+                if(paymentType=='VENDOR'){
+                    $('#tblvsppayment-' + rowId + '-hold_reason').addClass('has-error');
+                    $('#tblvsppayment-' + rowId + '-release_date').addClass('has-error');
+                }else{
+                    $('#tblmemberpaymentsummary-' + rowId + '-hold_reason').addClass('has-error');
+                    $('#tblmemberpaymentsummary-' + rowId + '-release_date').addClass('has-error');
+            }   
             } else {
-              if(paymentType=='VENDOR'){
-                $('#tblvsppayment-' + rowId + '-hold_reason').removeClass('has-error');
-                $('#tblvsppayment-' + rowId + '-release_date').removeClass('has-error');
-              }else{
-                $('#tblmemberpaymentsummary-' + rowId + '-hold_reason').removeClass('has-error');
-                $('#tblmemberpaymentsummary-' + rowId + '-release_date').removeClass('has-error');
-              }
+                if(passedDateObj > currentDate){
+                    isValid = false;
+                    msg = 'Release Date cannot be greater than Current Date';
+                }else if (passedDateObj < disburseDate) {
+                    isValid = false;
+                    msg = 'Release Date cannot be less than Disburse Date.';
+                }
+                if(paymentType=='VENDOR'){
+                    $('#tblvsppayment-' + rowId + '-hold_reason').removeClass('has-error');
+                    $('#tblvsppayment-' + rowId + '-release_date').removeClass('has-error');
+                }else{
+                    $('#tblmemberpaymentsummary-' + rowId + '-hold_reason').removeClass('has-error');
+                    $('#tblmemberpaymentsummary-' + rowId + '-release_date').removeClass('has-error');
+                }
             }
         });
 
@@ -151,7 +180,11 @@ $('#update-selected').click(function(e) {
             $('#update-unrelease-payment').append('<input type=\"hidden\" name=\"payment_type\" value=\"' + paymentType + '\"/>'); // Add hidden input
             $('#update-unrelease-payment').submit();
         } else {
-            bootbox.alert('<div class=\'bg-danger\'><i class=\'fa fa-times-circle\'></i></div><span>" . Yii::t('app', 'Hold Reason and Release Date are required for selected records.') . "</span>');
+            if(msg != ''){
+                bootbox.alert('<div class=\'bg-danger\'><i class=\'fa fa-times-circle\'></i></div><span>' + msg + '</span>');
+            } else {
+                bootbox.alert('<div class=\'bg-danger\'><i class=\'fa fa-times-circle\'></i></div><span>" . Yii::t('app', 'Hold Reason and Release Date are required for selected records.') . "</span>');
+            }
         }
     } else {
         bootbox.alert('<div class=\'bg-danger\'><i class=\'fa fa-times-circle\'></i></div><span>" . Yii::t('app', 'Please Select at least one Record') . "</span>');
