bb59242a8008da6869f1bdb3585312e2a58f88bf
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2023-09-06 18:09:31 +0530

    complain escalation process- IP

---
+++ modules/complaint/models/TblComplainEscalationTxnDetail.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace app\modules\complaint\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tbl_complain_escalation_txn_detail".
+ *
+ * @property integer $complain_escalation_txn_detail_code
+ * @property integer $complain_escalation_txn_code
+ * @property integer $complain_code
+ * @property integer $task_activity_code
+ * @property string $union_code
+ * @property string $user_type
+ * @property integer $escalation_time
+ * @property integer $level
+ * @property string $user_code
+ * @property string $status
+ * @property string $device_id
+ * @property string $created_at
+ * @property string $created_by
+ * @property string $updated_at
+ * @property string $updated_by
+ * @property integer $originating_type
+ * @property string $originating_org_code
+ * @property string $originating_org_type
+ */
+class TblComplainEscalationTxnDetail extends \app\models\ChildModel {
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName() {
+        return 'tbl_complain_escalation_txn_detail';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules() {
+        return [
+                [['union_code', 'user_type', 'status', 'complain_escalation_txn_code', 'device_id', 'complain_code', 'task_activity_code', 'escalation_time', 'level', 'originating_type', 'created_at', 'updated_at', 'user_code', 'created_by', 'updated_by', 'originating_org_code', 'originating_org_type'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels() {
+        return [
+            'complain_escalation_txn_detail_code' => Yii::t('app', 'Complain Escalation Txn Detail Code'),
+            'complain_escalation_txn_code' => Yii::t('app', 'Complain Escalation Txn Code'),
+            'complain_code' => Yii::t('app', 'Complain Code'),
+            'task_activity_code' => Yii::t('app', 'Task Activity Code'),
+            'union_code' => Yii::t('app', 'Union Code'),
+            'user_type' => Yii::t('app', 'User Type'),
+            'escalation_time' => Yii::t('app', 'Escalation Time'),
+            'level' => Yii::t('app', 'Level'),
+            'user_code' => Yii::t('app', 'User Code'),
+            'status' => Yii::t('app', 'Status'),
+            'device_id' => Yii::t('app', 'Device ID'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'created_by' => Yii::t('app', 'Created By'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+            'updated_by' => Yii::t('app', 'Updated By'),
+            'originating_type' => Yii::t('app', 'Originating Type'),
+            'originating_org_code' => Yii::t('app', 'Originating Org Code'),
+            'originating_org_type' => Yii::t('app', 'Originating Org Type'),
+        ];
+    }
+
+}
