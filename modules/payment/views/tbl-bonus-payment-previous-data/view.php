cb6c82cf4a3fcbda90abef1d79fd6b5eb449861a
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2023-09-18 16:59:33 +0530

    BonusPaymentPreviousData import dev - done

---
+++ modules/payment/views/tbl-bonus-payment-previous-data/view.php
@@ -0,0 +1,119 @@
+<?php
+
+use yii\helpers\Html;
+use kartik\detail\DetailView;
+use app\components\GeneralFunctions;
+
+$this->title = Yii::$app->label->title('view', 'Bill Head');
+?>
+
+<div class="panel panel-default panel-grid panel-main">
+    <div class="panel-heading">
+        <?= Yii::$app->controls->cancel($model); ?>
+        <?= Html::encode($this->title) ?>
+    </div>
+    <div class="panel-body">
+        <div class="form-grid">
+            <div class="table-responsive">
+                <?php
+                $attributes = [
+                        [
+                        'columns' => [
+                                [
+                                'attribute' => 'union_code',
+                                'value' => Yii::$app->general->getforeignkey($model->unionCode, 'union_name'),
+                                'valueColOptions' => ['style' => 'width:30%']
+                            ],
+                                [
+                                'attribute' => 'plant_code',
+                                'value' => Yii::$app->general->getforeignkey($model->plantCode, 'name'),
+                                'valueColOptions' => ['style' => 'width:30%']
+                            ],
+                        ],
+                    ],
+                        [
+                        'columns' => [
+                                [
+                                'attribute' => 'mcc_plant_code',
+                                'value' => Yii::$app->general->getforeignkey($model->mccPlantCode, 'name'),
+                                'valueColOptions' => ['style' => 'width:30%']
+                            ],
+                                [
+                                'attribute' => 'bmc_code',
+                                'value' => Yii::$app->general->getforeignkey($model->bmcCode, 'bmc_name'),
+                                'valueColOptions' => ['style' => 'width:30%']
+                            ],
+                        ],
+                    ],
+                        [
+                        'columns' => [
+                                [
+                                'attribute' => 'customer_type',
+                                'value' => Yii::$app->general->getforeignkey($model->customerType, 'customer_desc'),
+                                'valueColOptions' => ['style' => 'width:30%']
+                            ],
+                                [
+                                'attribute' => 'customer_code',
+                                'label' => Yii::t('app', 'Code'),
+                                'valueColOptions' => ['style' => 'width:30%']
+                            ],
+                        ],
+                    ],
+                        [
+                        'columns' => [
+                                [
+                                'attribute' => 'customer_name',
+                                'value' => Yii::$app->general->getCustomer($model, $model->customer_type),
+                                'valueColOptions' => ['style' => 'width:30%']
+                            ],
+                                [
+                                'attribute' => 'bill_head_code',
+                                'value' => Yii::$app->general->getforeignkey($model->billHeadCode, 'bill_head_name'),
+                                'valueColOptions' => ['style' => 'width:30%']
+                            ],
+                        ],
+                    ],
+                        [
+                        'columns' => [
+                                [
+                                'attribute' => 'transaction_date',
+                                'value' => Yii::$app->controls->view_date($model->transaction_date),
+                                'valueColOptions' => ['style' => 'width:30%']
+                            ],
+                                [
+                                'attribute' => 'bill_head_code',
+                                'value' => Yii::$app->general->getforeignkey($model->billHeadCode, 'bill_head_name'),
+                                'valueColOptions' => ['style' => 'width:30%']
+                            ],
+                        ],
+                    ],
+                        [
+                        'columns' => [
+                                [
+                                'attribute' => 'amount',
+                                'valueColOptions' => ['style' => 'width:100%']
+                            ],
+                        ],
+                    ],
+                ];
+
+                // View file rendering the widget
+                echo DetailView::widget([
+                    'model' => $model,
+                    'attributes' => $attributes,
+                    'mode' => 'view',
+                    'bordered' => true,
+                    'striped' => false,
+                    'responsive' => true,
+                    'hAlign' => 'left',
+                    'vAlign' => 'top',
+                    'deleteOptions' => [// your ajax delete parameters
+                        'params' => ['id' => 1000, 'kvdelete' => true],
+                    ],
+                    'container' => ['id' => 'kv-demo'],
+                ]);
+                ?>
+            </div>
+        </div>
+    </div>    
+</div>
