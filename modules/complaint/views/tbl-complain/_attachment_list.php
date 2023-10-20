488754110a4c8eb1284f2f1adc447f7e59042ff0
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2023-10-20 11:14:09 +0530

    complain attechment not show and user field blank isue - solved

--- modules/complaint/views/tbl-complain/_attachment_list.php
+++ modules/complaint/views/tbl-complain/_attachment_list.php
@@ -9,12 +9,12 @@ use yii\helpers\Url;
 <?php
 
 $attribute = [
-        ['attribute' => 'module_code', 'filter' => false],
-        ['attribute' => 'module_name', 'filter' => false],
-        ['attribute' => 'attachment_type', 'filter' => false],
-        ['attribute' => 'file_name', 'filter' => false],
-        ['attribute' => 'attachment', 'filter' => false],
-        ['attribute' => 'remarks', 'filter' => false],
+    ['attribute' => 'module_code', 'filter' => false],
+    ['attribute' => 'module_name', 'filter' => false],
+    ['attribute' => 'attachment_type', 'filter' => false],
+    ['attribute' => 'file_name', 'filter' => false],
+    ['attribute' => 'attachment', 'filter' => false],
+    ['attribute' => 'remarks', 'filter' => false],
 ];
 
 $grid_option = [
@@ -23,10 +23,9 @@ $grid_option = [
     'active_column' => false,
     'actions' => [
         'view-attachment' => function ($url, $model) {
-            $path = Yii::$app->params['complaint_dir_path'];
-            $attachemnt = '/' . $path . $model->file_name;
-            $url = Url::to([$attachemnt]);
-            return GhostHtml::a('<i class="fa fa-eye"></i>', $url, ['data-toggle' => 'tooltip', 'data-placement' => 'top', 'data-original-title' => 'View']);
+            $attachemnt = $model->attachment;
+            $url = !empty($attachemnt) ? $attachemnt : '';
+            return Html::a('<i class="fa fa-eye"></i>', $url, ['data-bs-toggle' => 'tooltip', 'data-placement' => 'top', 'title' => 'View Attechment', 'target' => '_blank']);
         },
     ]
 ];
