ee236914da3459107170baea77c70126535c25df
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2023-10-23 15:21:02 +0530

    tripcode not generated issue solved

--- modules/tankermovement/controllers/TblVehicleTripController.php
+++ modules/tankermovement/controllers/TblVehicleTripController.php
@@ -262,13 +262,11 @@ class TblVehicleTripController extends \app\controllers\ChildController {
         $out = [];
         if (isset($_POST['depdrop_parents'])) {
             $parents = $_POST['depdrop_parents'];
-            if (!empty($parents[0])) {
-                $bmc_code = isset($parents[1]) ? $parents[1] : '';
-                $transaction_date = isset($parents[2]) ? $parents[2] : '';
+            if (!empty($parents[0]) && !empty($parents[1]) && !empty($parents[2])) {
                 $tripCode = isset($parents[3]) ? $parents[3] : '';
 
                 $trip = new TblVehicleTripDetail();                
-                $data = $trip->getOpenTripList($bmc_code, $parents[0], $transaction_date, $tripCode);                
+                $data = $trip->getOpenTripList($parents[0], $parents[1], $parents[2], $tripCode);            
                 foreach ($data as $key => $val) {
                     $out[] = array('id' => $key, 'name' => $val);
                 }
