b832c1f7c97294db84b7ce718d3c3fd7bfdfce7a
Tejas Thakkar <tejas.thakkar@yamatech.in>
2023-12-06 12:23:16 +0530

    Milk Collection Code Dropdown Remove Extra dependency

--- modules/dcsoperation/controllers/TblMemberController.php
+++ modules/dcsoperation/controllers/TblMemberController.php
@@ -361,7 +361,7 @@ class TblMemberController extends \app\controllers\ChildController {
         $out = [];
         if (isset($_POST['depdrop_parents'])) {
             $parents = $_POST['depdrop_parents'];
-            if (!empty($parents[0]) && !empty($parents[1]) && !empty($parents[2]) && !empty($parents[3])) {
+            if (!empty($parents[0]) && !empty($parents[1]) && !empty($parents[2])) {
                 $member = new TblMember();
                 $data = $member->getActivateMemberCode($parents[0], $parents[1], $parents[2]);
                 foreach ($data as $key => $val) {
