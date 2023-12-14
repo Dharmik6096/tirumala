f3a18fdb70e3a32b668a98d2e64c9403a7b2350c
Atul Patel (IT46/CAPITAL) <it46@EIPL.LOCAL>
2023-12-14 15:04:24 +0530

    changes of scheme rate releted changes

--- modules/collection/views/tbl-bmc-collection/_form.php
+++ modules/collection/views/tbl-bmc-collection/_form.php
@@ -257,6 +257,8 @@ $script = "
                     {
                         var rtpl = parseFloat(obj.data.list.rtpl);
                         $('#tblbmccollection-actual_rate').val(rtpl.toFixed(2));
+                        $('#tblbmccollection-scheme_rate').val('');
+                        $('#tblbmccollection-scheme_rate_code').val('');
                         if(obj.data.list.scheme_rate_rtpl != '' && obj.data.list.scheme_rate_rtpl != null){
                             var scheme_rate_rtpl = parseFloat(obj.data.list.scheme_rate_rtpl);
                             rtpl = rtpl + scheme_rate_rtpl;
