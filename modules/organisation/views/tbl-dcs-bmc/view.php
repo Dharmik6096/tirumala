214efcbc0c5a2a0a6e80f7121546adaf6bd8ed3d
Tejas Thakkar <tejas.thakkar@yamatech.in>
2023-12-19 11:46:51 +0530

    Add fssi field In tblbmc - Done

--- modules/organisation/views/tbl-dcs-bmc/view.php
+++ modules/organisation/views/tbl-dcs-bmc/view.php
@@ -238,12 +238,16 @@ $this->params['menu'][] = GhostHtml::a(Yii::t('app', '<i class="fa fa-user-circl
                             ],
                         ],
                     ],
-                        [
+                    [
                         'columns' => [
-                                [
+                            [
                                 'attribute' => 'aadhaar_no',
                                 'format' => 'html',
-                                'valueColOptions' => ['style' => 'width:80%'],
+                                'valueColOptions' => ['style' => 'width:30%'],
+                            ],
+                            [
+                                'attribute' => 'fssi',
+                                'valueColOptions' => ['style' => 'width:30%'],
                             ],
                         ],
                     ],
