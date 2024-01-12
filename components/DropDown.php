8ef310de0b83b2f6b3f0124fba143500a8ae8145
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2024-01-12 16:53:31 +0530

    Create mis_user_attendance report and create user dropdown

--- components/DropDown.php
+++ components/DropDown.php
@@ -826,6 +826,9 @@ class DropDown extends Component {
             $where['is_active'] = 1;
         }
         if (isset($labelData['whereCondition'])) {
+//            echo '<pre>';
+//            print_r($labelData['whereCondition']);
+//            die;
             $where = array_merge($labelData['whereCondition'], $where);
         }
         $select_fields[] = $fields[0];
@@ -937,7 +940,7 @@ class DropDown extends Component {
             'multiSelectOptions' => [
                 'id' => $id,
                 'clientOptions' =>
-                    [
+                [
                     'includeSelectAllOption' => true,
                     'numberDisplayed' => 1,
                     'disableIfEmpty' => true,
@@ -1813,7 +1816,6 @@ class DropDown extends Component {
                 'prompt' => Yii::t('app', 'Select txn type'),
                 'data' => ['BIPL' => Yii::t('app', 'BIPL'), 'BIPLZIP' => Yii::t('app', 'BIPLZIP')],
             ],
-
         ];
         return $records[$l];
     }
@@ -1951,6 +1953,7 @@ class DropDown extends Component {
             'party_master_code' => ['name' => 'party_name', 'fields' => 'party_master_code,party_name', 'prompt' => 'Applicable Name', 'model' => 'TblPartyMaster'],
             'party_master' => ['name' => 'party_master_code', 'fields' => 'party_master_code,party_name,owner_name', 'prompt' => 'Select Party', 'model' => 'TblPartyMaster'],
             'user_code' => ['name' => 'user_code', 'fields' => 'user_code,name', 'prompt' => Yii::t('app', 'Select Parent'), 'model' => 'User'],
+            'app_login_id' => ['name' => 'user_code', 'fields' => 'app_login_id,user_name', 'prompt' => 'Select User Name', 'model' => 'TblEiplAppLogin', 'whereCondition' => ['master_type' => ['area', 'bmc', 'mccPlant', 'plant', 'region', 'routeMapping', 'union', 'user']]],
         ];
         return $label[$l];
     }
@@ -2037,7 +2040,7 @@ class DropDown extends Component {
             'multiSelectOptions' => [
                 'id' => $id,
                 'clientOptions' =>
-                    [
+                [
                     'includeSelectAllOption' => true,
                     'numberDisplayed' => 0,
                     'disableIfEmpty' => true,
