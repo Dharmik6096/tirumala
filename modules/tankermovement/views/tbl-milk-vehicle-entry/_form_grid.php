75e0b8c667a77239f81bb31b2aa0a048d1502226
Tejas Thakkar <tejas.thakkar@yamatech.in>
2024-01-12 15:14:21 +0530

    MilkVehicle Entry : Grid View Changes And generate challan View Screen Grid Changes

--- modules/tankermovement/views/tbl-milk-vehicle-entry/_form_grid.php
+++ modules/tankermovement/views/tbl-milk-vehicle-entry/_form_grid.php
@@ -19,12 +19,32 @@ $attribute = [
             if (!empty($rel))
                 return Yii::$app->general->getforeignkey($model->{$rel . 'Dest'}, $att) . '-' . strtoupper($model->receipt_at_code);
         }, 'vAlign' => 'middle', 'filter' => false],
+        ['attribute' => 'receipt_at_code',
+            'label' => Yii::t('app', 'Destination Code')],
+        ['attribute' => 'receipt_at_code',
+            'label' => (Yii::t('app', 'Destination Ref.Code')),
+        'value' => function($model) {
+            $rel = Yii::$app->general->getDestRelation($model->receipt_at);
+            $att = strtolower($model->receipt_at) == 'bmc' ? 'bmc_name' : (strtolower($model->receipt_at) == 'vendor' ? 'customer_name' : (strtolower($model->receipt_at) == 'party' ? 'party_name' : 'name'));
+            if (!empty($rel) && $att !== 'party_name')
+                return Yii::$app->general->getforeignkey($model->{$rel . 'Dest'}, 'ref_code');
+        }, 'vAlign' => 'middle', 'filter' => false],
         ['attribute' => 'dispatch_from'],    
         ['attribute' => 'dispatch_from_code', 'value' => function($model) {
             $rel = Yii::$app->general->getDestRelation($model->dispatch_from);
             $att = strtolower($model->dispatch_from) == 'bmc' ? 'bmc_name' : (strtolower($model->dispatch_from) == 'vendor' ? 'customer_name' : (strtolower($model->dispatch_from) == 'party' ? 'party_name' : 'name'));
             if (!empty($rel))
                 return Yii::$app->general->getforeignkey($model->{$rel . 'Source'}, $att) . '-' . strtoupper($model->dispatch_from_code);
+        }, 'vAlign' => 'middle', 'filter' => false],
+        ['attribute' => 'dispatch_from_code',
+            'label' => Yii::t('app', 'Source Code')],
+        ['attribute' => 'dispatch_from_code',
+            'label' => (Yii::t('app', 'Source Ref.Code')),
+            'value' => function($model) {
+            $rel = Yii::$app->general->getDestRelation($model->dispatch_from);
+            $att = strtolower($model->dispatch_from) == 'bmc' ? 'bmc_name' : (strtolower($model->dispatch_from) == 'vendor' ? 'customer_name' : (strtolower($model->dispatch_from) == 'party' ? 'party_name' : 'name'));
+            if (!empty($rel) && $att !== 'party_name')
+                return Yii::$app->general->getforeignkey($model->{$rel . 'Source'}, 'ref_code');
         }, 'vAlign' => 'middle', 'filter' => false],
         ['attribute' => 'plant_code', 'value' => function($model) {
             return Yii::$app->general->getforeignkey($model->plantCode, 'name');
