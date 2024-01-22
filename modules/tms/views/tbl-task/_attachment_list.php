d91a7999cc0c7b5c5b87fa52380464a50e6dea81
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2024-01-22 15:06:20 +0530

    attributes remove from attachment list

--- modules/tms/views/tbl-task/_attachment_list.php
+++ modules/tms/views/tbl-task/_attachment_list.php
@@ -9,11 +9,8 @@ use yii\helpers\Url;
 <?php
 
 $attribute = [
-    ['attribute' => 'module_code', 'filter' => false],
-    ['attribute' => 'module_name', 'filter' => false],
     ['attribute' => 'attachment_type', 'filter' => false],
     ['attribute' => 'file_name', 'filter' => false],
-    ['attribute' => 'attachment', 'filter' => false],
     ['attribute' => 'remarks', 'filter' => false],
     ['attribute' => 'attachment', 'value' => function($model) {
             return Yii::$app->general->openImage($model->attachment);
