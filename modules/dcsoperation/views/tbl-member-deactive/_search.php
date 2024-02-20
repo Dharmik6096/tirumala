eaae7dd70660a1255c5321381d0d983905f6c32a
Vivek <vivek.patel@yamatech.in>
2024-02-20 11:25:12 +0530

    member-deactive Design issue Resolved

--- modules/dcsoperation/views/tbl-member-deactive/_search.php
+++ modules/dcsoperation/views/tbl-member-deactive/_search.php
@@ -73,13 +73,13 @@ use yii\web\JsExpression;
                                                                 }
                                                  }'),
                                 ],
-                                'options' => ['class' => 'btn btn-default btn-raised',
+                                'options' => ['class' => 'btn-login btn btn-default btn-raised',
                                     'type' => 'submit'],
                             ]);
                             AjaxSubmitButton::end();
                             ?>
 
-                            <?= Html::resetButton('Reset', ['class' => 'btn btn-primary']) ?>
+                            <?= Html::resetButton('Reset', ['class' => 'btn-login btn btn-primary']) ?>
                         </div>
                     </div>
                 </div>
