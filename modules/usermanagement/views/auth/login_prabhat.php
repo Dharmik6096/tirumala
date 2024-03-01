a3e46db7e00a220b7ad391ed504dac7076afac6b
Vivek <vivek.patel@yamatech.in>
2024-03-01 15:55:12 +0530

    toggle-button remove in login page

--- modules/usermanagement/views/auth/login_prabhat.php
+++ modules/usermanagement/views/auth/login_prabhat.php
@@ -17,9 +17,6 @@ $org = ($model->type == 'UNION') ? 'block' : 'none';
                 src="<?= $this->theme->getUrl('/assets/images/nav_logo.png') ?>" alt="AMCS Logo"
                 class="logo img-responsive" /> 
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
     </div>
 </div>
 <div class="modal fade" id="loginModal" role="dialog">
