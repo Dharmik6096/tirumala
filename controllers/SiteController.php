54bb4fbfac827e7dc86922ec11f56987b17ed6e8
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2024-03-05 14:12:51 +0530

    Shift Add in Dashboard

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -89,7 +89,7 @@ class SiteController extends Controller {
                 'class' => AccessControl::className(),
                 'only' => ['rail-login,rail-logout'],
                 'rules' => [
-                        [
+                    [
                         'actions' => ['rail-login,rail-logout'],
                         'allow' => true,
                         'roles' => ['@'],
@@ -162,9 +162,16 @@ class SiteController extends Controller {
         if (Yii::$app->session->get('eiplCode') == 'GYAN') {
             $defaultWidget = 'rmrd';
         }
-        $model->shift = 3;
         if (!empty(Yii::$app->request->post('Dashboard')['shift'])) {
             $model->shift = Yii::$app->request->post('Dashboard')['shift'];
+        } else {
+            $cur_time = date_create(date('H:i:s'));
+            $morning_time = date_create('16:00:00');
+            $diff = date_diff($morning_time, $cur_time);
+            $model->shift = 1;
+            if (($diff->h > 0 || $diff->i > 0) && $diff->invert == 0) {
+                $model->shift = 2;
+            }
         }
         $model->widget_type = isset(Yii::$app->request->post('Dashboard')['widget_type']) ? Yii::$app->request->post('Dashboard')['widget_type'] : $defaultWidget;
         $model->mcc_code = isset(Yii::$app->request->post('Dashboard')['mcc_code']) ? Yii::$app->request->post('Dashboard')['mcc_code'] : '';
@@ -774,10 +781,10 @@ class SiteController extends Controller {
         if (!empty($post['mag_to_shift'])) {
             $post['mav_to_shift'] = $post['mag_to_shift'];
         }
-        if(!empty($post['shift'])){
+        if (!empty($post['shift'])) {
             $post['from_shift'] = $post['shift'];
             $post['to_shift'] = $post['shift'];
-            if($post['shift'] == 3){
+            if ($post['shift'] == 3) {
                 $post['from_shift'] = 1;
                 $post['to_shift'] = 2;
             }
@@ -786,6 +793,7 @@ class SiteController extends Controller {
         $spname = $input['name'];
         $in_array = explode(',', str_replace(' ', '', $input['input']));
         $param_str = '';
+        $is_from_date = true;
         foreach ($in_array as $in) {
             $data = explode('=', $in);
             $variable = explode('~', $data[0]);
@@ -812,29 +820,21 @@ class SiteController extends Controller {
                     $value = ',' . $value . ',';
                 }
             }
-            if (isset($val_type[1]) && $val_type[1] == 'date' && isset($input['appendTime']) && $input['appendTime']) {
-                $cur_time = date_create(date('H:i:s'));
-                $morning_time = date_create('16:00:00');
-                $diff = date_diff($morning_time, $cur_time);
-                $time = '06:00:00';
-                if (($diff->h > 0 || $diff->i > 0) && $diff->invert == 0) {
-                    $time = '18:00:00';
+            if (isset($val_type[1]) && $val_type[1] == 'date') {
+                if ($is_from_date) {
+                    $value .= ' ' . \Yii::$app->general->getshift($post['from_shift']);
+                    $is_from_date = FALSE;
+                } else {
+                    $value .= ' ' . \Yii::$app->general->getshift($post['to_shift']);
                 }
-                $value = $value . ' ' . $time;
-            }
-
-            if ($param1 == 'shift') {
-                $value .= (!empty($param1) && isset($post[$param1])) ? ' ' . \Yii::$app->general->getshift($post[$param1]) : ' 00:00:00';
-            }
-            if ($param1 == 'from_shift') {
-                $value .= (!empty($param1) && isset($post[$param1])) ? ' ' . \Yii::$app->general->getshift($post[$param1]) : ' 00:00:00';
-            }
-            if ($param1 == 'to_shift') {
-                $value .= (!empty($param1) && isset($post[$param1])) ? ' ' . \Yii::$app->general->getshift($post[$param1]) : ' 00:00:00';
             }
             $param_str .= "'" . $value . "',";
         }
         $param_str = rtrim($param_str, ",");
+
+        echo '<pre>';
+        print_r($param_str);
+        die;
         $query = \Yii::$app->db->createCommand("{CALL $spname($param_str)}");
 // echo $query->rawSql;exit;
         $results = $query->queryAll();
@@ -965,7 +965,7 @@ class SiteController extends Controller {
             ],
             'dashboard_farmer_rmrd_blocks' => [
                 'name' => 'sp_portal_dashboard_farmer_rmrd_blocks',
-                'input' => 'union_code=' . $union_str . '|list,plant_code=' . $plant_code . '|list,mcc_code=' . $mcc_code . '|list,bmc_code=' . $bmc_code . '|list,dcs_code=' . $dcs_code . '|list,date~from_shift=' . date('Y-m-d') . '|date,date~to_shift=' . date('Y-m-d') . '|date,widget_type=' . $widget_type,
+                'input' => 'union_code=' . $union_str . '|list,plant_code=' . $plant_code . '|list,mcc_code=' . $mcc_code . '|list,bmc_code=' . $bmc_code . '|list,dcs_code=' . $dcs_code . '|list,date=' . date('Y-m-d') . '|date,date=' . date('Y-m-d') . '|date,widget_type=' . $widget_type,
             ],
             'dashboard_farmer_rmrd_avg' => [
                 'name' => 'sp_portal_dashboard_farmer_rmrd_avg',
