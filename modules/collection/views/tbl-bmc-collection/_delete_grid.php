65024e95a78ed66c802e665b598f8fbe13efb26a
Vivek <vivek.patel@yamatech.in>
2024-03-11 17:13:54 +0530

    bmc-collection Design issue resolved

--- modules/collection/views/tbl-bmc-collection/_delete_grid.php
+++ modules/collection/views/tbl-bmc-collection/_delete_grid.php
@@ -84,7 +84,7 @@ $form = ActiveForm::begin([
 <div class="col-sm-12 margin-top-10 form-group" >
     <?php
     if (!empty($dataProvider->getModels()) && empty($msg)) {
-        echo Html::button(Yii::t('app', 'Delete'), ['class' => 'btn btn-primary', 'id' => 'delete']);
+        echo Html::button(Yii::t('app', 'Delete'), ['class' => 'btn-login btn btn-primary', 'id' => 'delete']);
     }
     ?>
     <?= Yii::$app->controls->custombutton('Cancel', 'index','','btn-login'); ?> 
