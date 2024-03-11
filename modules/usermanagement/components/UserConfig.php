2391bb0774dea46cd68ea48d672e4c63a1b906e2
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2024-03-11 16:16:40 +0530

    permission issue resolve in 8.2

--- modules/usermanagement/components/UserConfig.php
+++ modules/usermanagement/components/UserConfig.php
@@ -2,9 +2,16 @@
 
 namespace app\modules\usermanagement\components;
 
+use app\modules\usermanagement\components\AuthHelper;
+
 class UserConfig extends \webvimark\modules\UserManagement\components\UserConfig {
 
     public $identityClass = 'app\modules\usermanagement\models\User';
     public $loginUrl = ['/usermanagement/auth/login'];
+
+    protected function afterLogin($identity, $cookieBased, $duration) {
+        parent::afterLogin($identity, $cookieBased, $duration);
+        AuthHelper::updatePermissions($identity);
+    }
 
 }
