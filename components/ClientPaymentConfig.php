dbd44f9f08494b69f19dfc95871ad8f26b31c674
Asmita Chavda (IT36/CAPITAL) <it36@EIPL.LOCAL>
2024-03-12 16:33:54 +0530

    VRS SBD - Payment SP Add

--- components/ClientPaymentConfig.php
+++ components/ClientPaymentConfig.php
@@ -297,7 +297,7 @@ class ClientPaymentConfig extends Component {
             'VRS_SBD' => [
                 'member_payment' => [
                     'param' => 'from_datetime,to_datetime,union_code,bmc_code,payment_cycle_code,process_stop_payment,user_code',
-                    'sp_name' => 'sp_member_payment_vrs_newasa',
+                    'sp_name' => 'sp_member_payment_vrs_sbd',
                 ],
                 'vsp_payment' => [
                     'param' => 'union_code,from_datetime,from_shift,to_datetime,to_shift,payment_cycle_code,bmc_code,customer_type,process_stop_payment,user_code',
@@ -305,7 +305,7 @@ class ClientPaymentConfig extends Component {
                 ],
                 'remuneration_payment' => [
                     'param' => 'union_code,plant_code,mcc_plant_code,bmc_code,from_datetime,to_datetime,calculate_milk_recovey,calculate_other_head,process_stop_payment,user_code',
-                    'sp_name' => 'sp_remuneration_payment_vrs_newasa',
+                    'sp_name' => 'sp_remuneration_payment_vrs_sbd',
                 ],
                 'mcc_remuneration_payment' => [
                     'param' => 'union_code,plant_code,mcc_plant_code,bmc_code,from_datetime,to_datetime',
