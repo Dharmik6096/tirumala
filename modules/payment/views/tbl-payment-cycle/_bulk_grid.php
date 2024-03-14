c0533e5134aed42cb73165712778bb595886bc02
Yamini Bhatt (IT51/EIPL) <it51@EIPL.LOCAL>
2024-03-14 18:38:17 +0530

    Depecrated issue for null value on default grid resolved :done

--- modules/payment/views/tbl-payment-cycle/_bulk_grid.php
+++ modules/payment/views/tbl-payment-cycle/_bulk_grid.php
@@ -1,15 +1,16 @@
 <?php
 
-use yii\bootstrap\ActiveForm;
+use app\components\ActiveForm;
 use yii\web\View;
 use yii\helpers\Html;
 use kartik\grid\GridView;
 use yii\helpers\Url;
 
 $this->title = Yii::t('app', 'Bulk Data LOCK/UNLOCK');
-$button = !empty($searchModel->data_status) && $searchModel->data_status == 0 ? 'LOCK' : 'UNLOCK';
+$button = !empty($searchModel->data_status) && $searchModel->data_status == 1 ? 'UNLOCK' : 'LOCK';
 $data_status = !empty($searchModel->data_status) && $searchModel->data_status == 1 ? 0 : 1;
 $check_for = !empty($searchModel->check_for) ? $searchModel->check_for : '';
+$attr = $check_for=='' ? 'data_lock_bmc' : $check_for;
 ?>
 <div class=" no-effect">
     <?php
@@ -24,20 +25,20 @@ $check_for = !empty($searchModel->check_for) ? $searchModel->check_for : '';
             ['class' => 'kartik\grid\CheckboxColumn',
                 'rowSelectedClass' => GridView::TYPE_SUCCESS,
                 'headerOptions' => ['class' => 'skip-export'], 'contentOptions' => ['class' => 'skip-export'],
-                'checkboxOptions' => function($model) use($data_status, $check_for) {
+                'checkboxOptions' => function ($model) use ($data_status, $check_for) {
                     return ['class' => 'checkbox', 'value' => $model['payment_cycle_applicabilty_code'] . '###' . $data_status . '###' . $check_for];
                 }],
-            ['attribute' => 'applicable_type', 'value' => function($model) {
+            ['attribute' => 'applicable_type', 'value' => function ($model) {
                     return Yii::$app->general->getforeignkey($model->customerType, 'customer_desc');
                 }, 'filter' => false],
             ['attribute' => 'applicable_code', 'label' => Yii::t('app', 'Applicable Code'), 'filter' => false],
-            ['attribute' => 'applicable_code', 'label' => Yii::t('app', 'Code Ex.'), 'value' => function($model) {
+            ['attribute' => 'applicable_code', 'label' => Yii::t('app', 'Code Ex.'), 'value' => function ($model) {
                     return Yii::$app->general->getforeignkey($model->bmcCode, 'bmc_code_ex');
                 }, 'filter' => false],
-            ['attribute' => 'applicable_code', 'label' => Yii::t('app', 'Ref. Code'), 'value' => function($model) {
+            ['attribute' => 'applicable_code', 'label' => Yii::t('app', 'Ref. Code'), 'value' => function ($model) {
                     return Yii::$app->general->getforeignkey($model->bmcCode, 'ref_code');
                 }, 'filter' => false],
-            ['attribute' => 'applicable_code', 'label' => Yii::t('app', 'Name'), 'value' => function($model) {
+            ['attribute' => 'applicable_code', 'label' => Yii::t('app', 'Name'), 'value' => function ($model) {
                     return Yii::$app->general->getforeignkey($model->bmcCode, 'bmc_name');
                 }, 'filter' => false],
             ['attribute' => 'payment_cycle_code', 'filter' => false],
@@ -48,7 +49,7 @@ $check_for = !empty($searchModel->check_for) ? $searchModel->check_for : '';
                     'pluginOptions' => ['format' => 'dd-mm-yyyy',
                         'autoclose' => true]
                 ],
-                'value' => function($model) {
+                'value' => function ($model) {
                     return Yii::$app->controls->view_date($model->from_date);
                 }, 'filter' => FALSE],
             [
@@ -58,12 +59,12 @@ $check_for = !empty($searchModel->check_for) ? $searchModel->check_for : '';
                     'pluginOptions' => ['format' => 'dd-mm-yyyy',
                         'autoclose' => true]
                 ],
-                'value' => function($model) {
+                'value' => function ($model) {
                     return Yii::$app->controls->view_date($model->to_date);
                 }, 'filter' => FALSE],
-                  ['attribute' => ''.$check_for.'', 'value' => function ($model)use($check_for) {
-                     return $model->{$check_for} == 1 ? 'Lock' : 'Unlock';
-                },'filter' => false],
+            ['attribute' => $attr , 'value' => function ($model)use ($check_for) {
+                        return $model->{$check_for} == 1 ? 'Lock' : 'Unlock';
+                }, 'filter' => false],
 //            ['attribute' => 'data_lock_bmc', 'value' => function ($model) {
 //                    return $model->data_lock_bmc == 1 ? 'Unlock' : 'Lock';
 //                }, 'filter' => false],
@@ -71,7 +72,7 @@ $check_for = !empty($searchModel->check_for) ? $searchModel->check_for : '';
 //                    return $model->sync_lock_bmc == 1 ? 'Unlock' : 'Lock';
 //                }, 'filter' => false],
             ['attribute' => 'billing_lock_bmc', 'value' => function ($model) {
-                     return $model->billing_lock_bmc == 1 ? 'Lock' : 'Unlock';
+                    return $model->billing_lock_bmc == 1 ? 'Lock' : 'Unlock';
                 }, 'filter' => false],
 //            ['attribute' => 'data_lock_member', 'value' => function ($model) {
 //                    return $model->data_lock_member == 1 ? 'Unlock' : 'Lock';
@@ -80,7 +81,7 @@ $check_for = !empty($searchModel->check_for) ? $searchModel->check_for : '';
 //                    return $model->sync_lock_member == 1 ? 'Unlock' : 'Lock';
 //                }, 'filter' => false],
             ['attribute' => 'billing_lock_member', 'value' => function ($model) {
-                     return $model->billing_lock_member == 1 ? 'Lock' : 'Unlock';
+                    return $model->billing_lock_member == 1 ? 'Lock' : 'Unlock';
                 }, 'filter' => false],
         ];
 
@@ -95,10 +96,10 @@ $check_for = !empty($searchModel->check_for) ? $searchModel->check_for : '';
         <div class="panel-footer">
             <?php
             if (!empty($dataProvider->getModels())) {
-                echo Html::button(Yii::t('app', $button), ['class' => 'btn btn-primary', 'id' => 'delete']);
+                echo Html::button(Yii::t('app', $button), ['class' => 'btn-login btn btn-primary', 'id' => 'delete']);
             }
             ?>
-            <?= Yii::$app->controls->custombutton('Cancel', 'bulk-data-lock-unlock'); ?> 
+            <?= Yii::$app->controls->custombutton('Cancel', 'bulk-data-lock-unlock', '', 'btn-login'); ?> 
         </div>
         <?php ActiveForm::end(); ?>
     </div>
