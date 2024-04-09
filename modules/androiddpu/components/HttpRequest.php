2db11b681686681754cc8e0e5f00e175b747826e
Tejas Thakkar <tejas.thakkar@yamatech.in>
2024-04-09 10:25:03 +0530

    Implement dashboard Rmrd summary API endpoint For Android DPU module

--- modules/androiddpu/components/HttpRequest.php
+++ modules/androiddpu/components/HttpRequest.php
@@ -20,7 +20,7 @@ class HttpRequest extends \yii\base\Component {
     public $device_id;
     public $content = [];
     public $req_url;
-    public $is_free = ['android-dpu/register', 'android-dpu/verification', 'realtime-services/dpu-product-stock'];
+    public $is_free = ['android-dpu/register', 'android-dpu/verification', 'realtime-services/dpu-product-stock', 'dashboard/rmrd-summary'];
     public $request;
     public $allow_call = FALSE;
     public $action_url;
---
+++ modules/androiddpu/v4/controllers/DashboardController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\modules\androiddpu\v4\controllers;
+
+use app\modules\androiddpu\controllers\RestController;
+use Yii;
+
+class DashboardController extends RestController {
+
+    public function actionRmrdSummary() {
+        $data = $this->post_data;
+
+        $res_data = [];
+        if (!empty($data['organization_code']) && !empty($data['organization_type']) && in_array($data['organization_type'], ['BMC']) && !empty($data['content']['from_date']) && !empty($data['content']['to_date'])) {
+            $raw_data = Yii::$app->general->getSpData('sp_app_amcs_v4_dashboard_rmrd_summary', [$data['organization_type'], $data['organization_code'], $data['content']['from_date'], $data['content']['to_date']]);
+
+            foreach ($raw_data as $entry) {
+                $dataType = $entry['data_type'];
+                unset($entry['data_type']);
+                $res_data[$dataType][] = ($dataType === 'bmcCollection') ?
+                        $entry :
+                        ['collectionDatetime' => $entry['collectionDatetime'], 'totalSample' => $entry['totalSample']] +
+                        (($dataType !== 'qualityCollection') ? ['totalQuantity' => $entry['totalQuantity']] : []);
+            }
+        }
+        $this->response['data'] = $res_data;
+        return $this->response;
+    }
+
+}
+
+?>
