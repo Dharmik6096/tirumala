c8b1799c852dd2fdf12e78e77ab4d34b49f95d79
Tejas Thakkar <tejas.thakkar@yamatech.in>
2024-04-12 18:44:49 +0530

    Add Fields In TblMember And TblMemberProvisional List And View Screen

--- modules/dcsoperation/views/tbl-member-provisional/view.php
+++ modules/dcsoperation/views/tbl-member-provisional/view.php
@@ -23,359 +23,389 @@ if (Yii::$app->general->getUnionConfiguration($model->union_code, 'workflow_requ
         <div class = "table-responsive">
             <?php
             $attributes = [
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'union_code',
                             'value' => isset($model->unionCode) ? $model->unionCode->union_name : '',
                             'valueColOptions' => ['style' => 'width:80%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'bmc_code',
                             'value' => (string) $model->bmc_code,
                             'valueColOptions' => ['style' => 'width:30%']],
-                            [
+                        [
                             'attribute' => 'bmc_name',
                             'value' => isset($model->tblDcsBmc) ? $model->tblDcsBmc->bmc_name : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'society_code',
                             'value' => (string) $model->dcs_code,
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'dcs_code',
                             'value' => isset($model->dcsCode) ? $model->dcsCode->dcs_name : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'member_code',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'reference_code',
                             'value' => Yii::$app->general->getforeignkey($model->dcsCode, 'dcs_code_ex') . $model->ex_member_code,
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'pro_ex_member_code',
                             'valueColOptions' => ['style' => 'width:80%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'ex_member_code',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'member_type_code',
                             'value' => isset($model->memberTypeCode) ? $model->memberTypeCode->member_type_name : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'member_name',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'local_name',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'father_name',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'local_father_name',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'surname',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'local_surname',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'nominee_name',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'local_nominee_name',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'dob',
                             'value' => Yii::$app->controls->view_date($model->dob),
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'bloodgroup_code',
                             'value' => !empty($model->bloodGroupCode) ? $model->bloodGroupCode->blood_group : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'gender_code',
                             'value' => !empty($model->genderCode) ? $model->genderCode->gender : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'qualification_code',
                             'value' => !empty($model->qualificationCode) ? $model->qualificationCode->qualification_name : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'caste_category_code',
                             'value' => !empty($model->casteCategoryCode) ? $model->casteCategoryCode->caste_category_name : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'religion_code',
                             'value' => !empty($model->religionCode) ? $model->religionCode->religion : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'nominee_relation',
                             'value' => !empty($model->relationship) ? $model->relationship->relationship : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'voter_id',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'registration_date',
                             'value' => Yii::$app->controls->view_date($model->registration_date),
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'member_class',
                             'value' => ($model->member_class == 1) ? 'APL' : ($model->member_class == 2 ? 'BPL' : ''),
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
+                    'columns' => [
+                        [
+                            'attribute' => 'employee_code',
+                            'valueColOptions' => ['style' => 'width:30%']
+                        ],
+                        [
+                            'attribute' => 'employee_name',
+                            'valueColOptions' => ['style' => 'width:30%']
+                        ],
+                    ],
+                ],
+                [
+                    'columns' => [
+                        [
+                            'attribute' => 'created_by',
+                            'value' => Yii::$app->general->getforeignkey($model->userName, 'name'),
+                            'valueColOptions' => ['style' => 'width:80%']
+                        ],
+                    ],
+                ],
+                [
                     'group' => true,
                     'label' => 'Animal Details',
                     'rowOptions' => ['class' => 'bg-default']
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'animal_type_code',
                             'value' => !empty($model->animalTypeCode) ? $model->animalTypeCode->animal_type_name : '',
                             'valueColOptions' => ['style' => 'width:80%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'no_of_buffalo',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'no_of_cow_cross',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'no_of_cow_ind',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'total_animals',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
 //                        [
 //                            'attribute' => 'land_class',
 //                            'valueColOptions' => ['style' => 'width:30%']
 //                        ],
-                            [
+                        [
                             'attribute' => 'total_land',
                             'valueColOptions' => ['style' => 'width:80%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'group' => true,
                     'label' => 'Address Details',
                     'rowOptions' => ['class' => 'bg-default']
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'address',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'local_address',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'state_code',
                             'value' => !empty($model->stateCode) ? $model->stateCode->state_name : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'district_code',
                             'value' => !empty($model->districtCode) ? $model->districtCode->district_name : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'sub_district_code',
                             'value' => !empty($model->subDistrictCode) ? $model->subDistrictCode->sub_district_name : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'village_code',
                             'value' => !empty($model->villageCode) ? $model->villageCode->village_name : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'hamlet_code',
                             'value' => !empty($model->hamletCode) ? $model->hamletCode->hamlet_name : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'pincode',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'mobile_no',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'email',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
+                    'columns' => [
+                        [
+                            'attribute' => 'region_code',
+                            'value' => Yii::$app->general->getforeignkey($model->regionCode, 'region_name'),
+                            'valueColOptions' => ['style' => 'width:80%']
+                        ],
+                    ],
+                ],
+                [
                     'group' => true,
                     'label' => 'Bank Details',
                     'rowOptions' => ['class' => 'bg-default']
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'bank_code',
                             'value' => !empty($model->bankCode) ? $model->bankCode->bank_name : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'branch_code',
                             'value' => !empty($model->branchCode) ? $model->branchCode->branch_name : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'bank_account_no',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'ifsc',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'pan_no',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                            [
+                        [
                             'attribute' => 'adhar_no',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                    [
+                [
                     'columns' => [
-                            [
+                        [
                             'attribute' => 'annual_income',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
