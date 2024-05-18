ffa754f74cd6ce8295fa8c70ec73ae34bb901ed6
Asmita Chavda (IT36/CAPITAL) <it36@EIPL.LOCAL>
2024-05-18 16:54:14 +0530

    Payment Cycle Locking and Shift Locking Changes

--- modules/payment/controllers/TblPaymentCycleController.php
+++ modules/payment/controllers/TblPaymentCycleController.php
@@ -403,15 +403,15 @@ class TblPaymentCycleController extends ChildController {
         Yii::$app->operation->history($modelData, $historyModel, UPDATE);
         $title = $modelData->{$updateField} == 1 ? $unlockMessage : $lockMessage;
         $modelData->{$updateField} = $modelData->{$updateField} == 1 ? 0 : 1;
-        if ($updateField == 'billing_lock_member' && $modelData->{$updateField} == 1) {
-            $modelData->sync_lock_member = 1;
-            $modelData->data_lock_member = 1;
-        }
-        if ($updateField == 'data_lock_member' && $modelData->{$updateField} == 1) {
-            $modelData->sync_lock_member = 1;
-        }
-        if ($updateField == 'data_lock_bmc' && $modelData->{$updateField} == 1) {
-            $modelData->sync_lock_bmc = 1;
+        if (in_array($updateField, ['billing_lock_member', 'data_lock_member', 'data_lock_bmc'])) {
+            if ($updateField == 'billing_lock_member' && $modelData->{$updateField} == 1) {
+                $modelData->sync_lock_member = 1;
+                $modelData->data_lock_member = 1;
+            } else if ($updateField == 'data_lock_member' && $modelData->{$updateField} == 1) {
+                $modelData->sync_lock_member = 1;
+            } else if ($updateField == 'data_lock_bmc' && $modelData->{$updateField} == 1) {
+                $modelData->sync_lock_bmc = 1;
+            }
         }
         $modelData->scenario = 'lockUnlock';
         $transaction = $this->generalModel->saveTransaction([$modelData, $historyModel], [$title, 'edit']);
