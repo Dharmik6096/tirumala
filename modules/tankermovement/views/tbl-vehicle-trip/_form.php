f63a1b632feaf9a945789358353606a29153013d
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2024-06-05 15:47:48 +0530

    bug resolved for QAQC

--- modules/tankermovement/views/tbl-vehicle-trip/_form.php
+++ modules/tankermovement/views/tbl-vehicle-trip/_form.php
@@ -130,19 +130,21 @@ $('#vehicle-trip-form').submit(function(e) {
 ";
 $script .= "$('#tblvehicletrip-bmc_code').change(function () {
 var mccarray =  $('#tblvehicletrip-bmc_code option:selected');
-var nonselarray =  $('#tblvehicletrip-bmc_code option:not(:selected)').map(function () {return this.value;}).get();                        
 var bmc_array_sel = {};
     mccarray.each(function(){
-                var val = $(this).attr('value');
-                        var txt = $(this).text();
-                        bmc_array_sel[val]=txt;
-                    });   
+        var val = $(this).attr('value');
+        var txt = $(this).text();
+        bmc_array_sel[val]=txt;
+    });   
+    var options='';
     $.each(bmc_array_sel, function(index, value) {
-            if(jQuery.inArray(index,nonselarray) == -1){
-                $('#tblvehicletrip-bmc_code').append($('<option></option>').attr('value', index).text(value)); 
-            }
-            nonselarray.push(index);
-    });  
+        if(jQuery.inArray(index,mccarray) == -1){   
+            options += '<option value=\"'+index+'\">'+value+'</option>';
+       }else{
+            options += '<option value=\"'+index+'\" selected>'+value+'</option>';
+       }
+    });
+    $('#tbluserorganizationmapping-bmc_code').html(options);
     $('#tblvehicletrip-bmc_code').bootstrapDualListbox('refresh', true);      
 });
 ";
