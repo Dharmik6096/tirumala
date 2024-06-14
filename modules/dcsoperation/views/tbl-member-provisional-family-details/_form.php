f467aca0dbba68c99240da79c1fc36317d807b26
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2024-06-14 17:37:23 +0530

    resolve 8 version related bug

--- modules/dcsoperation/views/tbl-member-provisional-family-details/_form.php
+++ modules/dcsoperation/views/tbl-member-provisional-family-details/_form.php
@@ -49,7 +49,7 @@ echo $form->errorSummary($memberFamilyDetail);
                         <?= $form->field($memberFamilyDetail, 'remarks')->textarea() ?>
                     </div>
                     <div class="col-sm-2 mt15 icon-set-checkbox">
-                        <?= Yii::$app->general->getDisplayDocumentLink($model->provisional_member_code, 'tbl_member_provisional', ['aadharCard', 'aadharCardBack']) ?>
+                        <?= Yii::$app->general->getDisplayDocumentLink($model->provisional_member_code, 'tbl_member_provisional', ['nAddressProofback', 'nAddressProof']) ?>
                         <?= Yii::$app->controls->checkTemplateBootstrap5($memberFamilyDetail, $form, 'is_nominee'); ?>
                     </div>
                     <div class="col-sm-2">
