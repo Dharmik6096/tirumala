6978b5e3d6f284921174f6da7c3afebf43444c78
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2024-07-10 13:51:05 +0530

    member provisional sap import

--- modules/dcsoperation/views/tbl-member-provisional/index.php
+++ modules/dcsoperation/views/tbl-member-provisional/index.php
@@ -11,6 +11,7 @@ if (Yii::$app->general->getUnionConfiguration(explode(',', Yii::$app->session->g
         $this->params['menu'][] = Yii::$app->controls->custombutton('provisional member bank receipt export', ['/misreports/reports/export-provisional-member-bank-receipt'], '', 'btn btn-danger btn-block', '<i class="fa fa-upload"></i>');
         // $this->params['menu'][] = Yii::$app->controls->custombutton('provisional member bank receipt export', 'export-provisional-member-bank-receipt', '', 'btn btn-danger btn-block', '<i class="fa fa-upload"></i>');
         $this->params['menu'][] = Yii::$app->controls->custombutton('provisional member bank receipt import', 'import-provisional-member-bank-receipt', '', 'btn btn-danger btn-block', '<i class="fa fa-download"></i>');
+        $this->params['menu'][] = Yii::$app->controls->import('provisional-member-sap-import', $this, Yii::t('app', 'Provisional Member SAP Import'));
     }
 } else {
     $this->title = Yii::t('app', Yii::$app->label->title('list', 'provisional member'));
