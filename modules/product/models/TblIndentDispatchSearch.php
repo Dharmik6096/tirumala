91ae0ca74d12a929d40374b4a101eabf6fcd0bae
Tejas Thakkar <tejas.thakkar@yamatech.in>
2024-07-18 16:22:18 +0530

    index screen for New Indent dispatch New

--- modules/product/models/TblIndentDispatchSearch.php
+++ modules/product/models/TblIndentDispatchSearch.php
@@ -41,6 +41,65 @@ class TblIndentDispatchSearch extends TblIndentDispatch {
      * @return ActiveDataProvider
      */
     public function search($params) {
+        $query = TblIndentDispatch::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->joinWith(['routeCode', 'vehicleCode', 'productCode', 'indentCode']);
+
+        Yii::$app->general->filterByOrg($query, $this, 'tbl_indent_dispatch', 'tbl_indent_dispatch', 'tbl_indent_dispatch');
+
+        // grid filtering conditions
+        $from_date = !empty($this->from_date) ? date('Y-m-d', strtotime($this->from_date)) : date('Y-m-d');
+        $query->andFilterWhere(['>=', 'dispatch_date', $from_date]);
+
+        $to_date = !empty($this->to_date) ? date('Y-m-d', strtotime($this->to_date)) : date('Y-m-d');
+        $query->andFilterWhere(['<=', 'dispatch_date', $to_date]);
+
+        if (!empty($this->dispatch_date)) {
+            $query->andFilterWhere(['like', 'CONVERT(VARCHAR(25), dispatch_date, 126)', date('Y-m-d', strtotime($this->dispatch_date))]);
+        }
+
+        if (!empty($this->status_date)) {
+            $query->andFilterWhere(['like', 'CONVERT(VARCHAR(25), tbl_indent_master.status_date, 126)', date('Y-m-d', strtotime($this->status_date))]);
+        }
+
+        $query->andFilterWhere(['like', 'indent_dispatch_code', $this->indent_dispatch_code])
+                ->andFilterWhere(['like', 'reference_no', $this->reference_no])
+                ->andFilterWhere(['like', 'customer_type', $this->customer_type])
+                ->andFilterWhere(['like', 'customer_code', $this->customer_code])
+                ->andFilterWhere(['like', 'tbl_route_mapping.route_name', $this->route_code])
+                ->andFilterWhere(['like', 'tbl_product.product_name', $this->product_code])
+                ->andFilterWhere(['like', 'tbl_indent_dispatch.dispatch_qty', $this->dispatch_qty])
+//                ->andFilterWhere(['like', 'tbl_vehicle_master.parsing_no', $this->vehicle_no]);
+                ->andFilterWhere(['like', 'tbl_indent_dispatch.vehicle_no', $this->vehicle_no])
+                ->andFilterWhere(['like', 'tbl_indent_dispatch.dcs_code', $this->dcs_code]);
+
+        return $dataProvider;
+    }
+
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function searchNew($params) {
         // $query = TblIndentDispatch::find();
         $query = TblIndentDispatch::find()->select(['tbl_indent_dispatch.vehicle_no', 'tbl_indent_dispatch.reference_no']);
 
@@ -87,5 +146,6 @@ class TblIndentDispatchSearch extends TblIndentDispatch {
 
         return $dataProvider;
     }
+
 
 }
