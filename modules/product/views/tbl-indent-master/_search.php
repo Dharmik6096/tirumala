f20f5bf6f8c54079a388a6a298cd22f16146e1a8
Tejas Thakkar <tejas.thakkar@yamatech.in>
2024-07-22 13:04:20 +0530

    Resolve Design Issue

--- modules/product/views/tbl-indent-master/_search.php
+++ modules/product/views/tbl-indent-master/_search.php
@@ -8,37 +8,39 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="search-filter large-search">
+<div class="search-filter large-search d-inline-block">
 
     <?php
     $form = ActiveForm::begin([
-//                'action' => ['get-temp-data'],
-                'method' => 'get',
+        //                'action' => ['get-temp-data'],
+        'method' => 'get',
     ]);
     ?>
-    <div class="col-sm-2">
-        <?= Yii::$app->dropdown->federation_union($model, $form, 'union_code', $model->getAttributeLabel('union_code')); ?>
-    </div>
-    <div class="col-sm-2">
-        <?= Yii::$app->dropdown->union_plant($model, $form, 'tblindentmastersearch-union_code', 'plant_code', $model->getAttributeLabel('plant_code'), FALSE, ''); ?>
-    </div>
-    <div class="col-sm-2 ">
-        <?= Yii::$app->dropdown->plant_mcc($model, $form, 'tblindentmastersearch-plant_code', 'mcc_plant_code', $model->getAttributeLabel('mcc_plant_code'), FALSE, ''); ?>
-    </div>  
-    <div class="col-sm-2 ">
-        <?= Yii::$app->dropdown->mcc_bmc($model, $form, 'tblindentmastersearch-mcc_plant_code', 'bmc_code', Yii::t('app', 'BMC'), FALSE); ?>
-    </div>  
-    <div class="col-sm-2 ">
-        <?= Yii::$app->dropdown->bmc_society($model, $form, 'tblindentmastersearch-bmc_code', 'dcs_code', Yii::t('app', 'Society'), FALSE); ?>
-    </div>
-    <div class="col-sm-2">
-        <?= Yii::$app->controls->date($model, $form, 'from_date', 'form-group col-sm-2 padding-left-5 padding-right-5', false, false, false, TRUE); ?>
-    </div>
-    <div class="col-sm-2">
-        <?= Yii::$app->controls->date($model, $form, 'to_date', 'form-group col-sm-2 padding-left-5 padding-right-5', false, false, false, TRUE); ?>
-    </div>
-    <div class="col-sm-3 mt23">
-        <?= Yii::$app->controls->search(); ?>
+    <div class="row">
+        <div class="col-sm-2">
+            <?= Yii::$app->dropdown->federation_union($model, $form, 'union_code', $model->getAttributeLabel('union_code')); ?>
+        </div>
+        <div class="col-sm-2">
+            <?= Yii::$app->dropdown->union_plant($model, $form, 'tblindentmastersearch-union_code', 'plant_code', $model->getAttributeLabel('plant_code'), FALSE, ''); ?>
+        </div>
+        <div class="col-sm-2 ">
+            <?= Yii::$app->dropdown->plant_mcc($model, $form, 'tblindentmastersearch-plant_code', 'mcc_plant_code', $model->getAttributeLabel('mcc_plant_code'), FALSE, ''); ?>
+        </div>
+        <div class="col-sm-2 ">
+            <?= Yii::$app->dropdown->mcc_bmc($model, $form, 'tblindentmastersearch-mcc_plant_code', 'bmc_code', Yii::t('app', 'BMC'), FALSE); ?>
+        </div>
+        <div class="col-sm-2 ">
+            <?= Yii::$app->dropdown->bmc_society($model, $form, 'tblindentmastersearch-bmc_code', 'dcs_code', Yii::t('app', 'Society'), FALSE); ?>
+        </div>
+        <div class="col-sm-2">
+            <?= Yii::$app->controls->date($model, $form, 'from_date', 'form-group col-sm-2 padding-left-5 padding-right-5', false, false, false, TRUE); ?>
+        </div>
+        <div class="col-sm-2">
+            <?= Yii::$app->controls->date($model, $form, 'to_date', 'form-group col-sm-2 padding-left-5 padding-right-5', false, false, false, TRUE); ?>
+        </div>
+        <div class="col-sm-3 mt23">
+            <?= Yii::$app->controls->search(); ?>
+        </div>
     </div>
 
     <?php ActiveForm::end(); ?>
