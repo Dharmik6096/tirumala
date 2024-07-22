d133ac6f2710cecf98716b14af4294f11b55e2f7
Asmita Chavda (IT36/CAPITAL) <it36@EIPL.LOCAL>
2024-07-22 16:18:02 +0530

    PM Sale Lock - timeout issue

--- modules/payment/models/TblLoanProductSaleLockingSearch.php
+++ modules/payment/models/TblLoanProductSaleLockingSearch.php
@@ -161,7 +161,6 @@ class TblLoanProductSaleLockingSearch extends TblLoanProductSaleLocking {
             unset($sp_params['locking_date']);
 
             $output = \Yii::$app->general->getSpData('Portal_loan_product_sale_data_lock', $sp_params);
-            var_dump(count($output));die;
         }
         if (!$this->validate()) {
             // uncomment the following line if you do not want to return any records when validation fails
