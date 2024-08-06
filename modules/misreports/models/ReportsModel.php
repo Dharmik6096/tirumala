17063360651e511046a09ad9b35e6da5b60a73c8
Tejas Thakkar <tejas.thakkar@yamatech.in>
2024-08-06 16:54:02 +0530

    Add validation for from and to values in MilkCollectionFilterBased report

--- modules/misreports/models/ReportsModel.php
+++ modules/misreports/models/ReportsModel.php
@@ -159,6 +159,8 @@ return $('#reportsmodel-org_type').val() == 'DCS';
             [['union_code', 'user_code', 'from_date', 'to_date'], 'required', 'on' => ['FieldStaffActivity']],
             [['union_code', 'plant_code', 'mcc_code', 'as_on_date'], 'required', 'on' => 'ExportProvisionalMemberBankReceipt'],
             [['dcs_code', 'report_type', 'from_value', 'to_value'], 'required', 'on' => ['MilkCollectionFilterBased']],
+            [['from_value', 'to_value'], 'number'],
+            [['from_value'], 'validateFromValue', 'on' => ['MilkCollectionFilterBased']]
         ];
     }
 
@@ -258,6 +260,15 @@ return $('#reportsmodel-org_type').val() == 'DCS';
         $bmcModel = new TblDcsBmc();
         $bmcData = $bmcModel->find()->where(['bmc_code' => $bmcCode])->one();
         return $bmcData;
+    }
+
+    public function validateFromValue($attribute, $params) {
+        $fromValue = $this->$attribute;
+        $toValue = $this->to_value;
+        if ($fromValue >= $toValue) {
+            $this->addError($attribute, Yii::t('app/validation', 'To Value Must Greater than From Value'));
+            return false;
+        }
     }
 
 }
