ec61c0914fd1419fb3cb5b833804abb8ae92b756
Asmita Chavda (IT36/CAPITAL) <it36@EIPL.LOCAL>
2024-08-08 16:00:56 +0530

    AMCS - Cleaning & Calibration data do not add to collection

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -89,7 +89,7 @@ class SiteController extends Controller {
                 'class' => AccessControl::className(),
                 'only' => ['rail-login,rail-logout'],
                 'rules' => [
-                    [
+                        [
                         'actions' => ['rail-login,rail-logout'],
                         'allow' => true,
                         'roles' => ['@'],
@@ -890,7 +890,7 @@ class SiteController extends Controller {
         if (!empty(Yii::$app->request->post('performance_type'))) {
             $performance_type = Yii::$app->request->post('performance_type');
         }
-        if(!empty(Yii::$app->request->post('member_code'))){
+        if (!empty(Yii::$app->request->post('member_code'))) {
             $member_code = Yii::$app->request->post('member_code');
         }
         $array = [
@@ -1964,7 +1964,8 @@ class SiteController extends Controller {
                                                 $model->other_reading = str_replace('\r\n', '#####', $model->other_reading);
                                                 $model->other_reading = str_replace('\r', '#####', $model->other_reading);
                                                 $model->other_reading = str_replace('\n', '#####', $model->other_reading);
-                                                if (in_array(substr($model->member_code, -4), ['2097', '2098']) && $is_insert) {
+                                                if (in_array(substr($model->member_code, -4), ['2097', '2098'])) {
+                                                    $process_record = FALSE;
                                                     $model->setCleaningCalibration($model, $childModel);
                                                 }
                                             }
@@ -1991,7 +1992,9 @@ class SiteController extends Controller {
                                 }
                                 $generalModel = new GeneralModel();
                                 $masterSave = [];
-                                $masterSave[] = $model;
+                                if ($process_record) {
+                                    $masterSave[] = $model;
+                                }
                                 $transaction = $generalModel->saveDeleteTransaction($masterSave, $childModel, $delete, ['transactional data', 'create'], true);
                                 if ($transaction != 'customRedirect') {
                                     $transaction_data->error_log = !empty($transaction) ? (string) $transaction : 'error_occured';
