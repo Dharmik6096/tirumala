ae9fec6150832add05ec45529b9875c2f5bbac71
Vivek <vivek.patel@yamatech.in>
2024-09-03 10:37:32 +0530

    Label change middle name to Father Name for VRS

--- messages/VRS_GLT_en/app.php
+++ messages/VRS_GLT_en/app.php
@@ -275,4 +275,5 @@ return [
     'Member Bill Head Import' => 'Farmer Bill Head Import',
     'Delete Member Payment Restrict' => 'Delete Farmer Payment Restrict',
     'Bulk Delete Member Payment Restrict' => 'Bulk Delete Farmer Payment Restrict',
+    'Middle Name' => 'Father Name',
 ];
