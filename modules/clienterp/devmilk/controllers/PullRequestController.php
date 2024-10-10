c377d63df23ed7047103e03e253b1cba4935cef6
rajendra.patel <rajendra.patel@yamatech.in>
2024-10-10 17:31:46 +0530

    create api for devmilk bmc milk collection incentive

--- modules/clienterp/devmilk/controllers/PullRequestController.php
+++ modules/clienterp/devmilk/controllers/PullRequestController.php
@@ -30,4 +30,28 @@ class PullRequestController extends PullMasterController {
         return $this->response;
     }
 
+    public function actionBmcCollectionIncentive() {
+
+        $request = Yii::$app->request->getRawBody();
+        if (!empty($request) && !empty($request['bmc_code']) && !empty($request['from_date']) && !empty(!empty($request['to_date']))) {
+            try {
+                $sp_param = [];
+                $sp_param[] = $request['bmc_code'];
+                $sp_param[] = $request['from_date'];
+                $sp_param[] = $request['to_date'];
+                $sp_name = 'sp_clienterp_devmilk_pull_bmc_collection_incentive';
+                $response = \Yii::$app->general->getSpData($sp_name, $sp_param);
+                $this->response->setData($response, FALSE);
+            } catch (\Throwable $ex) {
+                $this->response->setStatusCode($this->eiplResponseCode->statusError);
+                $this->response->setMessage(['Error While Process Request.']);
+            }
+        } else {
+            $this->response->setStatusCode($this->eiplResponseCode->validationFail);
+            $this->response->setMessage(['Required Parameter Missing.']);
+        }
+
+        return $this->response;
+    }
+
 }
