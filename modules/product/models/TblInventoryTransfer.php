0208d00e432ba491f79b8330c85ac7417923ff78
Asmita Chavda <asmita.chavda@yamatech.in>
2024-10-13 01:49:56 +0530

    Inventory transfer import change

--- modules/product/models/TblInventoryTransfer.php
+++ modules/product/models/TblInventoryTransfer.php
@@ -204,8 +204,12 @@ class TblInventoryTransfer extends \app\models\ChildModel {
         $stockModel->setCodes($from_type, $from_code);
         $stockModel->product_code = $product;
         $stockModel->union_code = $union_code;
-
-        $existtoStock = $stockModel->getExistStock($from_type);
+        $batch_no='';
+        $batchNoWiseInventory = Yii::$app->general->getUnionConfiguration($this->union_code, 'batch_no_wise_inventory', 'PORTAL');
+        if ($batchNoWiseInventory == 1) {
+            $batch_no=$this->sap_batch_no;
+        }
+        $existtoStock = $stockModel->getExistStock($from_type, $batch_no);
         if (!empty($existtoStock->stock)) {
             $this->available_stock = $existtoStock->stock;
         } else {
@@ -283,7 +287,7 @@ class TblInventoryTransfer extends \app\models\ChildModel {
                     $fstockModel->product_code = $txModel->product_code;
                     $fstockModel->union_code = $txModel->union_code;
                     $batch = !empty($txModel->sap_batch_no) ? $txModel->sap_batch_no : '';
-                    $existfromStock = $fstockModel->getExistStock($this->from_type);
+                    $existfromStock = $fstockModel->getExistStock($this->from_type, $batch);
 
                     $f_stock = 0;
                     $qty = $txModel->qty;
