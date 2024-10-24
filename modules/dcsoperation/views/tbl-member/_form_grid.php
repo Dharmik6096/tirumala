fa634b938c00420dceb77a68b3994ad5d7f2c1aa
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2024-10-24 11:31:10 +0530

    member deactivate code comment

--- modules/dcsoperation/views/tbl-member/_form_grid.php
+++ modules/dcsoperation/views/tbl-member/_form_grid.php
@@ -183,49 +183,49 @@ Yii::$app->grid->bind($dataProvider, $searchModel, $grid_option, ['index'], true
 <?php
 $script = "
 $(document).ready(function(){
-    $(document).on('click','.deact-member',function(e){
-    var id= $(this).attr('data-val');
-    var name = $(this).attr('data-name');
-    bootbox.confirm({
-        message: '<div class=\'row\'><div class=\'col-sm-12\'><div class=\'bg-info\'><i class=\'fa fa-question\'></i></div><span>Are you sure you want to deactivate \"'+name+'\"?</span></div></div>',
-        buttons: {
-            'cancel': {
-                            label: 'Cancel',
-                            className: 'btn btn-danger'
-              },
-            'confirm': {
-                            label: 'Ok',
-                            className: 'btn btn-primary'
-             }
-        },
-        callback: function(result) {
-            if (result) {
-              $('#loader').show();
-                 $.ajax({
-                        type: 'get',
-                        url: '" . Url::to(['deactivate-user']) . "',
-                        data:{'id':id},
-                        success: function(data) {
-                            var obj1 = $.parseJSON(data);
-                            if (obj1.status == 'success')
-                            {
-                                $.pjax.reload({container: '#member-grid'});
-                                bootbox.alert(\"<div class=\'row\'><div class=\'col-sm-12\'><div class=\'bg-info\'><i class=\'fa fa-info\'></i></div><span>\"+obj1.msg+\"</span></div></div>\");
-                                //$.snackbar({content: 'Record successfully deleted.', timeout: 8000, style: 'successbar'});
-                            }
-                            else if (obj1.status == 'error'){
-                                bootbox.alert(\"<div class=\'row\'><div class=\'col-sm-12\'><div class=\'bg-danger\'><i class=\'fa fa-times\'></i></div><span>\"+obj1.msg+\"</span></div></div>\");
-                                //$.snackbar({content: 'Record is not deleted.', timeout: 8000, style: 'errorbar'});
-                            }
-                        },
-                        error:function(data){
-                                    //alert('Your data has not been submitted..Please try again');
-                                }
-            });
-            }
-        }
-    });
-    });
+//    $(document).on('click','.deact-member',function(e){
+//    var id= $(this).attr('data-val');
+//    var name = $(this).attr('data-name');
+//    bootbox.confirm({
+//        message: '<div class=\'row\'><div class=\'col-sm-12\'><div class=\'bg-info\'><i class=\'fa fa-question\'></i></div><span>Are you sure you want to deactivate \"'+name+'\"?</span></div></div>',
+//        buttons: {
+//            'cancel': {
+//                            label: 'Cancel',
+//                            className: 'btn btn-danger'
+//              },
+//            'confirm': {
+//                            label: 'Ok',
+//                            className: 'btn btn-primary'
+//             }
+//        },
+//        callback: function(result) {
+//            if (result) {
+//              $('#loader').show();
+//                 $.ajax({
+//                        type: 'get',
+//                        url: '" . Url::to(['deactivate-user']) . "',
+//                        data:{'id':id},
+//                        success: function(data) {
+//                            var obj1 = $.parseJSON(data);
+//                            if (obj1.status == 'success')
+//                            {
+//                                $.pjax.reload({container: '#member-grid'});
+//                                bootbox.alert(\"<div class=\'row\'><div class=\'col-sm-12\'><div class=\'bg-info\'><i class=\'fa fa-info\'></i></div><span>\"+obj1.msg+\"</span></div></div>\");
+//                                //$.snackbar({content: 'Record successfully deleted.', timeout: 8000, style: 'successbar'});
+//                            }
+//                            else if (obj1.status == 'error'){
+//                                bootbox.alert(\"<div class=\'row\'><div class=\'col-sm-12\'><div class=\'bg-danger\'><i class=\'fa fa-times\'></i></div><span>\"+obj1.msg+\"</span></div></div>\");
+//                                //$.snackbar({content: 'Record is not deleted.', timeout: 8000, style: 'errorbar'});
+//                            }
+//                        },
+//                        error:function(data){
+//                                    //alert('Your data has not been submitted..Please try again');
+//                                }
+//            });
+//            }
+//        }
+//    });
+//    });
     
     $(document).on('click','.block-unblock',function(e){
         $('#pageloader').show();
