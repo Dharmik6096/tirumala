4fa719d37d802d0f96ef6a4e4d1c4ef2d0fdd767
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2024-10-24 16:54:01 +0530

    allow manual collection api create

--- modules/webservice/eipl/v1/V1.php
+++ modules/webservice/eipl/v1/V1.php
@@ -722,7 +722,6 @@ class V1 extends \yii\base\Module {
             'branch/master' => [
                 'param' => 'bank_code',
                 'param' => 'select_param:*#organization_type:bank_code#bank_code#table:tbl_branch:condition:ifsc IS NOT NULL',
-
                 'sp' => 'sp_app_eipl_v1_master_data',
             ],
             'gender/master' => [
@@ -744,7 +743,7 @@ class V1 extends \yii\base\Module {
             'district/master' => [
                 'param' => 'select_param:*#organization_type:state_code#state_code#table:tbl_districts',
                 'sp' => 'sp_app_eipl_v1_master_data',
-            ],            
+            ],
             'sub-district/master' => [
                 'param' => 'select_param:*#organization_type:district_code#district_code#table:tbl_sub_districts',
                 'sp' => 'sp_app_eipl_v1_master_data',
@@ -761,6 +760,10 @@ class V1 extends \yii\base\Module {
             'report/milk-collection-audit' => [
                 'param' => 'from_datetime#to_datetime#union#plant#mcc#bmc#dcs#member',
                 'sp' => 'sp_app_eipl_v1_milk_collection_history',
+            ],
+            'manual-collection/master' => [
+                'param' => 'organization_type#organization_code',
+                'sp' => 'sp_app_eipl_v1_manual_collection',
             ],
         ];
         return $label;
