88a9b2a0c44f5fd704f678add62b06459c734c65
Atul Patel (IT46/CAPITAL) <it46@EIPL.LOCAL>
2024-10-25 17:37:47 +0530

    master hierarchy sendbox generate functionality developed

--- modules/organisation/models/TblMasterHierarchy.php
+++ modules/organisation/models/TblMasterHierarchy.php
@@ -6,6 +6,7 @@ use app\models\ChildModel;
 use app\modules\dcsoperation\models\TblMember;
 use Yii;
 use yii\base\UserException;
+use app\modules\syncutility\models\TblSentbox;
 
 class TblMasterHierarchy extends ChildModel {
     public $operation, $ref_code;
@@ -101,5 +102,55 @@ class TblMasterHierarchy extends ChildModel {
             ->andWhere(['<>','master_hierarchy_code', $this->master_hierarchy_code])
             ->count();
         // return $this->find()->where(['union_code' => $this->union_code, $key_reset_on => $this->{$key_reset_on}, 'convert(bigint,'.$key_name.')' => (int) $this->{$key_name}])
+    }
+
+    public function afterSave($insert, $changedAttributes) {
+        $flag = (isset($this->operation) && $this->operation == true) ? $this->operation : (($insert) ? 'INSERT' : 'UPDATE');
+        $sentboxArray = [];
+        $types = [];
+        if($this->master_type == 'tbl_plant'){
+            $types = ['vlc', 'bmc'];
+            $sentboxArray = Yii::$app->general->getSentBoxCodes($this->plant_code);
+        } else if($this->master_type == 'tbl_mcc_plant'){
+            $types = ['vlc', 'bmc'];
+            $sentboxArray = Yii::$app->general->getSentBoxCodes('', $this->mcc_plant_code);
+        } else if($this->master_type == 'tbl_bmc'){
+            $types = ['vlc', 'bmc'];
+            $sentboxArray = Yii::$app->general->getSentBoxCodes('', '', $this->bmc_code, '', '', true); // map_bmc, own_bmc, vlc
+        }   else if($this->master_type == 'tbl_dcs'){
+            $types = ['vlc', 'bmc'];
+            $sentboxArray = Yii::$app->general->getSentBoxCodes('', '', '', '', $this->dcs_code);
+        } else if($this->master_type == 'tbl_member'){
+            $types = ['vlc', 'bmc'];
+            $sentboxArray = Yii::$app->general->getSentBoxCodes('', '', '', '', $this->dcs_code); // parent vlc, parent bmc - is_bmc = true
+        } else if($this->master_type == 'tbl_customer_master'){
+            $types = ['bmc'];
+            $sentboxArray = Yii::$app->general->getSentBoxCodes('', '', $this->bmc_code, '', '', FALSE);
+        } else if($this->master_type == 'tbl_route_mapping'){
+            $types = ['vlc', 'bmc'];
+            if (!empty($this->dcs_code)) {
+                $sentboxArray = Yii::$app->general->getSentBoxCodes('', '', '', '', $this->dcs_code);
+            } else if (!empty($this->bmc_code)) {
+                $sentboxArray = Yii::$app->general->getSentBoxCodes('', '', $this->bmc_code);
+            }
+        }
+        foreach ($sentboxArray as $sent) {
+            if (in_array(strtolower($sent['type']), $types)) {
+                $sentbox = $this->sentboxModel($sent['code'], $sent['type']);
+                if (!isset($this->is_sentbox) || (isset($this->is_sentbox) && $this->is_sentbox === TRUE)) {
+                    if (!($sentbox->setSentbox($this, $flag))) {
+                        throw new UserException("SentBox Entry is not created so transaction is rollback!");
+                    }
+                }
+            }
+        }
+    }
+    
+    public function sentboxModel($code, $type) {
+        $sentbox = new TblSentbox();
+        $sentbox->dest_org_id = $code;
+        $sentbox->source_org_id = $this->union_code;
+        $sentbox->dest_org_type = $type;
+        return $sentbox;
     }
 }
