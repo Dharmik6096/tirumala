15bf71b8518ff380d43de7617edb59f73880eb64
Atul Patel (IT46/CAPITAL) <it46@EIPL.LOCAL>
2024-10-30 11:08:34 +0530

    complain functionality issue resolved

--- components/DropDown.php
+++ components/DropDown.php
@@ -1738,7 +1738,7 @@ class DropDown extends Component {
             'complain_status' => [
                 'name' => 'complain_status',
                 'prompt' => Yii::t('app', 'Select Complain Status'),
-                'data' => ['CREATED' => Yii::t('app', 'CREATED'), 'INPROGRESS' => Yii::t('app', 'INPROGRESS'), 'CLOSED' => Yii::t('app', 'CLOSED'), 'RESOLVED' => Yii::t('app', 'RESOLVED')],
+                'data' => ['CREATED' => Yii::t('app', 'CREATED'), 'INPROGRESS' => Yii::t('app', 'INPROGRESS'), 'CLOSED' => Yii::t('app', 'CLOSED'), 'RESOLVED' => Yii::t('app', 'RESOLVED'), 'ASSIGNED' => Yii::t('app', 'ASSIGNED')],
             ],
             'resolved_status' => [
                 'name' => 'resolved_status',
