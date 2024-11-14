c044032dd1c6830b0d1ae74b7a5f18d2a78fab72
Atul Patel (IT46/CAPITAL) <it46@EIPL.LOCAL>
2024-11-14 15:08:28 +0530

    Total payable amount set in the payment transaction pending approval list screen

--- modules/payment/views/tbl-payment-transaction-approval/pending_approval.php
+++ modules/payment/views/tbl-payment-transaction-approval/pending_approval.php
@@ -6,6 +6,7 @@ $this->title = Yii::t('app', Yii::$app->label->title('list', $title));
 <div class="panel panel-default panel-grid">
     <div class="panel-heading">
         <?= $this->title; ?>
+        <div class="right_align_date">Total Payable Amount: <span class="total_payable_amount">0.00</span></div>
     </div>
     <div class="panel-body">
         <?=
