118750f5f2c747a62aef7fea9ddbb2964bb32354
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2024-11-19 14:37:06 +0530

    version 8 permission issue resolve

--- modules/collection/views/tbl-allow-manual-collection-range/_form.php
+++ modules/collection/views/tbl-allow-manual-collection-range/_form.php
@@ -60,11 +60,11 @@ $form = ActiveForm::begin([
     <div class="col-sm-2 shift">
         <?= Yii::$app->dropdown->dropdown('shift_applicability', $model, $form, 'to_shift', true, FALSE, 'to_shift'); ?>
     </div>
-    <div class="col-sm-2 mt10">
-        <?= $form->field($model, 'is_weight_manual', ['checkboxTemplate' => "<div class='checkbox'>{input}{beginLabel}{labelTitle}{endLabel}</div>{error}{hint}"])->checkbox(['readonly' => TRUE]); ?>
+    <div class="col-sm-2 mt15">
+        <?= Yii::$app->controls->checkTemplateBootstrap5($model, $form, 'is_weight_manual'); ?>
     </div>
-    <div class="col-sm-2 mt10">
-        <?= $form->field($model, 'is_quality_manual', ['checkboxTemplate' => "<div class='checkbox'>{input}{beginLabel}{labelTitle}{endLabel}</div>{error}{hint}"])->checkbox(['readonly' => TRUE]); ?>
+    <div class="col-sm-2 mt15">
+        <?= Yii::$app->controls->checkTemplateBootstrap5($model, $form, 'is_quality_manual'); ?>
     </div>
     <div class="col-sm-4 padding_top_20 shortcut-main" shortcut="true" display_shortcut="false" hilight_shortcut="false">
         <div class="form-group">
