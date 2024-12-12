030412df4394fc51f032c4c6eda662ddebaa72c3
Tejas Thakkar <tejas.thakkar@yamatech.in>
2024-12-12 13:06:01 +0530

    Add Read Only Sap Vendor Code In invetory Transfer Create Screen Based on Selected DCS

--- modules/product/controllers/TblInventoryTransferController.php
+++ modules/product/controllers/TblInventoryTransferController.php
@@ -2,6 +2,7 @@
 
 namespace app\modules\product\controllers;
 
+use app\modules\organisation\models\TblDcs;
 use Yii;
 use app\modules\product\models\TblInventoryTransfer;
 use app\modules\product\models\TblInventoryTransferTxn;
@@ -409,6 +410,17 @@ class TblInventoryTransferController extends \app\controllers\ChildController {
         }
         Yii::$app->response->format = trim(Response::FORMAT_JSON);
         return Json::encode($record);
+    }
+
+    public function actionGetSapVendorCode() {
+        $dcsModel = new TblDcs();
+        $dcsModel->dcs_code = Yii::$app->request->post('dcs');
+        $dcsData = $dcsModel->tblDcs;
+        if (!empty($dcsData->sap_vendor_code)) {
+            return Json::encode(['status' => 'success', 'sap_vendor_code' => $dcsData->sap_vendor_code]);
+        } else {
+            return Json::encode(['status' => 'success', 'sap_vendor_code' => '']);
+        }
     }
 
 }
