4ec012d76fdb8b575b7bd9e4d1a4a46e24e36c2e
Vivek <vivek.patel@yamatech.in>
2024-12-17 18:30:44 +0530

    design issue resolved for TS Loss And Shortage

--- modules/vsp/views/transit-recovery/_recovery_grid.php
+++ modules/vsp/views/transit-recovery/_recovery_grid.php
@@ -135,7 +135,7 @@ $this->title = Yii::t('app', 'TS Loss And Shortage');
             }
         }
         ?>
-        <div onclick="exportThisWithParameter('recovery_grid', '<?= $this->title ?>')"class="btn-login btn btn-primary apply-shortcut widget_table_search_btnaa downloadDashboardExcel right_30aa mis_custom_report"><i class="far fa-file-excel"></i></div>
+        <?= Html::button('<i class="far fa-file-excel"></i>', ['class' => 'btn-login btn btn-primary apply-shortcut', 'onclick' => "exportThisWithParameter('recovery_grid', '{$this->title}')"]); ?>
         <?= Yii::$app->controls->custombutton('Cancel', 'transit-loss-shortage', false, 'btn-login btn-danger apply-shortcut'); ?> 
 
     </div>
