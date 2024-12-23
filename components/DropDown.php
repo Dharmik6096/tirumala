cf7b29271206fb6a45074487e176dc4730fa24fc
Tejas Thakkar <tejas.thakkar@yamatech.in>
2024-12-23 12:12:53 +0530

    Set default URL for UserList dropdown

--- components/DropDown.php
+++ components/DropDown.php
@@ -669,7 +669,7 @@ class DropDown extends Component {
         $this->dependedDropdown($model, $form, $depends, $name, $islable, '/general/tbl-banner/tap-event-list', Yii::t('app', 'Select Tap Event'), $multiple, '', $readonly);
     }
 
-    public function UserList($model, $form, $depends, $name = 'user_code', $islable = false, $multiple = false, $readonly = false, $url) {
+    public function UserList($model, $form, $depends, $name = 'user_code', $islable = false, $multiple = false, $readonly = false, $url = '/organisation/tbl-route-mapping/user-list') {
         $this->setClass($form, $name);
         $this->dependedDropdown($model, $form, $depends, $name, $islable, $url, Yii::t('app', 'Select User'), $multiple, '', $readonly);
     }
--- modules/organisation/views/tbl-route-mapping/_map_route_source.php
+++ modules/organisation/views/tbl-route-mapping/_map_route_source.php
@@ -50,7 +50,7 @@ $this->title = Yii::t('app', $title);
                         </div>
                     </div>
                     <div class="col-sm-2">
-                        <?= Yii::$app->dropdown->UserList($model, $form, 'tblroutemappingsources-route_code', 'user_code', FALSE, FALSE, FALSE, '/organisation/tbl-route-mapping/user-list'); ?>
+                        <?= Yii::$app->dropdown->UserList($model, $form, 'tblroutemappingsources-route_code', 'user_code', FALSE, FALSE, FALSE); ?>
                     </div>
                     <?php
                     $i = 0;
