1d9306551040272e943fc4b25ba9e7717ac79786
Vivek <vivek.patel@yamatech.in>
2024-12-27 10:27:31 +0530

    Design issues resolved for version Php82

--- modules/organisation/views/tbl-dcs/_verification_grid.php
+++ modules/organisation/views/tbl-dcs/_verification_grid.php
@@ -42,9 +42,9 @@ $this->title = Yii::t('app', 'Bank Verification');
                 , 'filter' => FALSE],
                 ['attribute' => 'remarks',
                 'format' => 'raw',
-                'contentOptions' => ['class' => 'no_padding_input hide_help_block mb-3-imp'],
+                'contentOptions' => ['class' => 'no_padding_input hide_help_block'],
                 'value' => function ($model, $key, $index) use ($form, $searchModel) {
-                    return $form->field($searchModel, '[' . $model['code'] . '@@' . $model['verify_for'] . ']remark')->textInput(['class' => 'mb-14'])->label(FALSE);
+                    return $form->field($searchModel, '[' . $model['code'] . '@@' . $model['verify_for'] . ']remark')->textInput()->label(FALSE);
                 }, 'filter' => false
             ],
         ];
