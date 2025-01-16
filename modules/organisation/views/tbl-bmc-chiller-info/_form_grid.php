cd4f523be07231d7b7af8152c7057b0983177f89
Vivek <vivek.patel@yamatech.in>
2025-01-16 10:39:16 +0530

    Icon changes for bmc chiller info

--- modules/organisation/views/tbl-bmc-chiller-info/_form_grid.php
+++ modules/organisation/views/tbl-bmc-chiller-info/_form_grid.php
@@ -45,13 +45,13 @@ $grid_option = [
         'edit' => function ($url, $model) {
             $class = ($model->is_active === 0) ? 'link-disable' : '';
             $options = ['data-bs-toggle' => 'tooltip', 'data-placement' => 'top', 'title' => 'Edit', 'class' => 'edit-record ' . $class, 'data-val' => $model->chiller_info_code, 'data-name' => $model->chiller_info_code, 'title' => Yii::t('app', 'Edit')];
-            return GhostHtml::a_alert('<i class="fa fa-pencil"></i>', ['/organisation/tbl-dcs-bmc/update-chiller-info'], $options);
+            return GhostHtml::a_alert('<i class="fa fa-pencil-alt"></i>', ['/organisation/tbl-dcs-bmc/update-chiller-info'], $options);
         },
         'deactive' => function ($url, $model) {
             $name = $model->owner_name;
             $class = ($model->is_active === 0) ? 'link-disable' : '';
             $options = ['data-bs-toggle' => 'tooltip', 'data-placement' => 'top', 'title' => 'Deactivate', 'class' => 'deact-member ' . $class, 'data-val' => $model->chiller_info_code, 'data-name' => $name];
-            return GhostHtml::a_alert('<i class="fa fa-close"></i>', ['/organisation/tbl-dcs-bmc/deactivate-bmc-chiller'], $options);
+            return GhostHtml::a_alert('<i class="fa fa-times"></i>', ['/organisation/tbl-dcs-bmc/deactivate-bmc-chiller'], $options);
         },
     ]
 ];
