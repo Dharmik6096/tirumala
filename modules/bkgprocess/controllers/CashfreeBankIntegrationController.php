4c5a401d467044193dbe9ae65e7aea747418a4cc
Asmita Chavda <asmita.chavda@yamatech.in>
2025-01-16 15:48:26 +0530

    add cash free controller

---
+++ modules/bkgprocess/controllers/CashfreeBankIntegrationController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\modules\bkgprocess\controllers;
+
+class CashfreeBankIntegrationController extends Controller {
+
+    public $freeAccessActions = ['upload-payment-data-bulk', 'get-transaction-status-bulk'];
+
+    public function actionUploadPaymentDataBulk() {
+        
+    }
+
+    public function actionGetTransactionStatusBulk() {
+        
+    }
+
+}
