8330a47aad33797fc01881b8bece2442cc1bb087
Tejas Thakkar <tejas.thakkar@yamatech.in>
2025-01-30 15:45:27 +0530

    Removed billing_method from required fields in chiller info

--- modules/organisation/models/TblBmcChillerInfo.php
+++ modules/organisation/models/TblBmcChillerInfo.php
@@ -48,7 +48,7 @@ class TblBmcChillerInfo extends \app\models\ChildModel
     {
         return [
             [['chilling_capacity','installation_date', 'agreement_from_date', 'agreement_to_date', 'created_at', 'updated_at', 'is_active', 'union_code', 'plant_code', 'mcc_plant_code', 'bmc_code', 'chiller_name', 'fix_rent', 'billing_method'], 'safe'],
-            [['owner_name', 'rate_type', 'chilling_capacity', 'min_qty', 'pan_no', 'tds_percentage', 'installation_date', 'agreement_no', 'agreement_from_date', 'agreement_to_date', 'chiller_name', 'billing_method'], 'required'],
+            [['owner_name', 'rate_type', 'chilling_capacity', 'min_qty', 'pan_no', 'tds_percentage', 'installation_date', 'agreement_no', 'agreement_from_date', 'agreement_to_date', 'chiller_name'], 'required'],
             [['owner_name', 'pan_no','rate_type','agreement_no','created_by', 'updated_by', 'originating_org_code', 'originating_org_type',], 'string'],
             [['is_active', 'originating_type'], 'integer'],
             [['min_qty', 'tds_percentage'], 'number'],
