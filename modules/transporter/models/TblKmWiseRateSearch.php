a9a2585878011a3e780ee704dfb94c84957d93bb
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2025-01-31 15:43:47 +0530

    Query correction and changes for Transporter Module all list grids

--- modules/transporter/models/TblKmWiseRateSearch.php
+++ modules/transporter/models/TblKmWiseRateSearch.php
@@ -49,6 +49,8 @@ class TblKmWiseRateSearch extends TblKmWiseRate {
             'query' => $query,
         ]);
 
+        $this->from_date = date('Y-m-d', strtotime('-30 days'));
+        $this->to_date = date('Y-m-d');
         $this->load($params);
         Yii::$app->general->filterByOrg($query, $this);
 
