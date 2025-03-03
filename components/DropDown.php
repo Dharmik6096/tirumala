61fdf3cd59760126d5cc9984ab0885ee30b3422d
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2025-03-03 15:51:22 +0530

    selected value variable and impect remove from static dropdown

--- components/DropDown.php
+++ components/DropDown.php
@@ -892,7 +892,7 @@ class DropDown extends Component {
                 });
     }
 
-    public function dropdownStatic($flag, $model, $form, $class = 'form-group padding-right-5 col-sm-2', $label = false, $disable = false, $name = '', $addAll = false, $removeKey = false, $searchable = true, $return = false, $multiple = false, $selectedValue = '') {
+    public function dropdownStatic($flag, $model, $form, $class = 'form-group padding-right-5 col-sm-2', $label = false, $disable = false, $name = '', $addAll = false, $removeKey = false, $searchable = true, $return = false, $multiple = false) {
         if (in_array($flag, ['organizations_type'])) {
             (Yii::$app->session->get('organizations_type') == 'UNION') ? $flag = 'organizations_type_union' : $flag = 'organizations_type_federation';
         }
@@ -920,25 +920,22 @@ class DropDown extends Component {
                 unset($records[$value]);
             }
         }
-        if($selectedValue == '' && isset($model->{$control_name})){
-            $selectedValue = $model->{$control_name};
-        }
         if ($return) {
             return $form->field($model, $control_name, ['options' => ['class' => $class]])->widget(Select2::classname(), [
-                        'data' => $records, 'pluginOptions' => ['allowClear' => true], 'options' => ['placeholder' => $data['prompt'], 'value' => $selectedValue, 'disabled' => $disable, 'class' => $class]]
+                        'data' => $records, 'pluginOptions' => ['allowClear' => true], 'options' => ['placeholder' => $data['prompt'], 'disabled' => $disable, 'class' => $class]]
                     )->label($label);
         }
         if ($multiple) {
             return $form->field($model, $control_name, ['options' => ['class' => $class]])->widget(Select2::classname(), [
-                        'data' => $records, 'pluginOptions' => ['allowClear' => true, 'multiple' => $multiple], 'options' => ['placeholder' => $data['prompt'], 'value' => $selectedValue, 'disabled' => $disable, 'class' => $class]]
+                        'data' => $records, 'pluginOptions' => ['allowClear' => true, 'multiple' => $multiple], 'options' => ['placeholder' => $data['prompt'], 'disabled' => $disable, 'class' => $class]]
                     )->label($label);
         }
         if (isset($searchable) && $searchable) {
             echo $form->field($model, $control_name, ['options' => ['class' => $class]])->widget(Select2::classname(), [
-                'data' => $records, 'pluginOptions' => ['allowClear' => true], 'options' => ['placeholder' => $data['prompt'], 'value' => $selectedValue, 'disabled' => $disable, 'class' => $class]]
+                'data' => $records, 'pluginOptions' => ['allowClear' => true], 'options' => ['placeholder' => $data['prompt'], 'disabled' => $disable, 'class' => $class]]
             )->label($label);
         } else {
-            $options = ['prompt' => Yii::t('app', $data['prompt']), 'value' => $selectedValue, 'disabled' => $disable];
+            $options = ['prompt' => Yii::t('app', $data['prompt']), 'disabled' => $disable];
             if ($data['prompt'] === FALSE) {
                 unset($options['prompt']);
             }
