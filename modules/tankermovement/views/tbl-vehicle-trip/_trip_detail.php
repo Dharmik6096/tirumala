3d0065ff7cbcc0a899d8853330c6687d2c664321
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2025-03-07 16:47:40 +0530

    version realated changes

--- modules/tankermovement/views/tbl-vehicle-trip/_trip_detail.php
+++ modules/tankermovement/views/tbl-vehicle-trip/_trip_detail.php
@@ -28,7 +28,7 @@ $sourceCode = !empty($rel) ? (Yii::$app->general->getforeignkey($tripDetail->{$r
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <button type="button" class="close" data-bs-dismiss="modal" aria-hidden="true">×</button>
                 <h4 class="modal-title"><?= strtoupper($actionType) . ' :: ' . $sourceCode . '/' . $sourceValue ?></h4>
             </div>
 
