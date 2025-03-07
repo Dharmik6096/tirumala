6b3e149413297219ba03fcd73e9eda2873b1080d
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2025-03-07 16:39:12 +0530

    vehicle trip get data condition chnage

--- modules/tankermovement/controllers/TblVehicleCleaningInspectionController.php
+++ modules/tankermovement/controllers/TblVehicleCleaningInspectionController.php
@@ -69,7 +69,7 @@ class TblVehicleCleaningInspectionController extends \app\controllers\ChildContr
                 $cnt++;
             }
             if (!empty($model->trip_code) && !empty($model->vehicle_code)) {
-                $tripDetail = TblVehicleTrip::find()->where(['trip_code' => $model->trip_code, 'vehicle_code' => $model->vehicle_code, 'is_active' => 1])->one();
+                $tripDetail = TblVehicleTrip::find()->where(['trip_code' => $model->trip_code, 'vehicle_code' => $model->vehicle_code, 'is_active' => 1, 'trip_sub_status' => 'cleaning_pending', 'trip_status' => 'closed'])->one();
                 $historyModel = new TblVehicleTripHistory();
                 Yii::$app->operation->history($tripDetail, $historyModel, UPDATE);
                 $tripDetail->sub_status_time = date('Y-m-d H:i:s');
