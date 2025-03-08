169480110ce0dc99bc8f5573f7d1d91a591039dc
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2025-03-08 12:55:15 +0530

    tanker milk qlty dynamic config txn result add and delete

--- modules/tankermovement/views/tbl-milk-vehicle-entry-qlty/_form_grid.php
+++ modules/tankermovement/views/tbl-milk-vehicle-entry-qlty/_form_grid.php
@@ -1,35 +1,44 @@
 <?php
 
 $attribute = [
-    ['attribute' => 'union_code', 'value' => function($model) {
+        ['attribute' => 'union_code', 'value' => function($model) {
             return Yii::$app->general->getforeignkey($model->unionCode, 'union_name');
         }, 'vAlign' => 'middle', 'filter' => false, 'visible' => false],
-    ['attribute' => 'plant_code', 'value' => function($model) {
+        ['attribute' => 'plant_code', 'value' => function($model) {
             return Yii::$app->general->getforeignkey($model->plantCode, 'name');
         }, 'visible' => true, 'filter' => false],
-    ['attribute' => 'plant_code', 'label' => Yii::t('app', 'Plant') . ' Ref Code', 'value' => function($model) {
+        ['attribute' => 'plant_code', 'label' => Yii::t('app', 'Plant') . ' Ref Code', 'value' => function($model) {
             return Yii::$app->general->getforeignkey($model->plantCode, 'ref_code');
         }, 'visible' => true, 'filter' => FALSE],
-    ['attribute' => 'vehicle_code', 'value' => function($model) {
+        ['attribute' => 'vehicle_code', 'value' => function($model) {
             return isset($model->vehicle) ? $model->vehicle->parsing_no . '/' . $model->vehicle->vehicleType->vehicle_type_name : '';
         }, 'vAlign' => 'middle', 'filter' => false, 'visible' => true],
-    ['attribute' => 'chamber_no'],
-    ['attribute' => 'trip_code', 'filter' => false],
-    ['attribute' => 'arrival_datetime', 'value' => function($model) {
+        ['attribute' => 'chamber_no'],
+        ['attribute' => 'trip_code', 'filter' => false],
+        ['attribute' => 'arrival_datetime', 'value' => function($model) {
             return Yii::$app->controls->view_datetime($model->arrival_datetime);
         }, 'filter' => false],
-    ['attribute' => 'lot_datetime', 'value' => function($model) {
-        return Yii::$app->controls->view_datetime($model->lot_datetime);
-    }, 'filter' => false],
-    ['attribute' => 'lot_no', 'filter' => false],
-    ['attribute' => 'status', 'filter' => false],
-    ['attribute' => 'status_datetime', 'value' => function($model) {
+        ['attribute' => 'lot_datetime', 'value' => function($model) {
+            return Yii::$app->controls->view_datetime($model->lot_datetime);
+        }, 'filter' => false],
+        ['attribute' => 'lot_no', 'filter' => false],
+        ['attribute' => 'status', 'filter' => false],
+        ['attribute' => 'status_datetime', 'value' => function($model) {
             return Yii::$app->controls->view_datetime($model->status_datetime);
         }, 'filter' => false],
-    ['attribute' => 'fat', 'filter' => false],
-    ['attribute' => 'snf', 'filter' => false],
+        ['attribute' => 'fat', 'filter' => false],
+        ['attribute' => 'snf', 'filter' => false],
 ];
-
+foreach ($config_list as $config) {
+    $attribute[] = [
+        'attribute' => 'config_code', 'label' => Yii::t('app', $config->config_name),
+        'value' => function ($model) use ($config) {
+            $model->config_code = $config->config_code;
+            $configResult = $model->configResult;
+            return !empty($configResult) ? (!empty($configResult->configResultCode->config_result) ? $configResult->configResultCode->config_result : $configResult->config_result) : '';
+        }
+    ];
+}
 $grid_option = [
     'id' => 'milk-vehicle-entry-qlty-grid',
     'attributes' => $attribute,
