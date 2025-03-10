7624a1fe63d63bc0916222f79e6748d0dfcb6e97
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2025-03-10 18:36:18 +0530

    raw material and tanker receipt add dock no impact

--- modules/tankermovement/models/TblMilkVehicleEntry.php
+++ modules/tankermovement/models/TblMilkVehicleEntry.php
@@ -60,9 +60,9 @@ class TblMilkVehicleEntry extends \app\models\ChildModel {
      */
     public function rules() {
         return [
-                [['union_code', 'receipt_at', 'arrival_time', 'receipt_at_code', 'dispatch_from', 'dispatch_from_code', 'receipt_datetime', 'receipt_shift_code'], 'required', 'except' => ['androidsync', 'importCsv']], 
+                [['union_code', 'receipt_at', 'arrival_time', 'receipt_at_code', 'dispatch_from', 'dispatch_from_code', 'receipt_datetime', 'receipt_shift_code'], 'required', 'except' => ['androidsync', 'importCsv']],
                 [['milk_vehicle_entry_code', 'trip_code', 'grn_no', 'receipt_at', 'vehicle_code', 'qty', 'union_code', 'plant_code', 'mcc_plant_code', 'bmc_code', 'customer_code', 'customer_type', 'created_by', 'updated_by', 'originating_org_code', 'originating_org_type'], 'string'],
-                [['vehicle_entry_date', 'arrival_time', 'tare_weight_time', 'created_at', 'updated_at', 'receipt_at_code', 'dispatch_from', 'dispatch_from_code', 'receipt_datetime', 'receipt_shift_code', 'tanker_no', 'plant_code', 'mcc_plant_code', 'approved_at', 'approved_by', 'approval_status', 'approval_remarks', 'process_approval_code', 'remarks'], 'safe'],
+                [['vehicle_entry_date', 'arrival_time', 'tare_weight_time', 'created_at', 'updated_at', 'receipt_at_code', 'dispatch_from', 'dispatch_from_code', 'receipt_datetime', 'receipt_shift_code', 'tanker_no', 'plant_code', 'mcc_plant_code', 'approved_at', 'approved_by', 'approval_status', 'approval_remarks', 'process_approval_code', 'remarks', 'dock_no'], 'safe'],
                 [['gross_weight', 'tare_weight'], 'number'],
                 [['originating_type'], 'integer'],
                 [['mcc_plant_code', 'bmc_code', 'customer_code', 'customer_type', 'receipt_at'], 'required', 'when' => function ($model) {
--- modules/tankermovement/views/tbl-milk-vehicle-entry/_form_grid.php
+++ modules/tankermovement/views/tbl-milk-vehicle-entry/_form_grid.php
@@ -20,16 +20,16 @@ $attribute = [
                 return Yii::$app->general->getforeignkey($model->{$rel . 'Dest'}, $att) . '-' . strtoupper($model->receipt_at_code);
         }, 'vAlign' => 'middle', 'filter' => false],
         ['attribute' => 'receipt_at_code',
-            'label' => Yii::t('app', 'Destination Code')],
+        'label' => Yii::t('app', 'Destination Code')],
         ['attribute' => 'receipt_at_code',
-            'label' => (Yii::t('app', 'Destination Ref.Code')),
+        'label' => (Yii::t('app', 'Destination Ref.Code')),
         'value' => function($model) {
             $rel = Yii::$app->general->getDestRelation($model->receipt_at);
             $att = strtolower($model->receipt_at) == 'bmc' ? 'bmc_name' : (strtolower($model->receipt_at) == 'vendor' ? 'customer_name' : (strtolower($model->receipt_at) == 'party' ? 'party_name' : 'name'));
             if (!empty($rel) && $att !== 'party_name')
                 return Yii::$app->general->getforeignkey($model->{$rel . 'Dest'}, 'ref_code');
         }, 'vAlign' => 'middle', 'filter' => false],
-        ['attribute' => 'dispatch_from'],    
+        ['attribute' => 'dispatch_from'],
         ['attribute' => 'dispatch_from_code', 'value' => function($model) {
             $rel = Yii::$app->general->getDestRelation($model->dispatch_from);
             $att = strtolower($model->dispatch_from) == 'bmc' ? 'bmc_name' : (strtolower($model->dispatch_from) == 'vendor' ? 'customer_name' : (strtolower($model->dispatch_from) == 'party' ? 'party_name' : 'name'));
@@ -37,10 +37,10 @@ $attribute = [
                 return Yii::$app->general->getforeignkey($model->{$rel . 'Source'}, $att) . '-' . strtoupper($model->dispatch_from_code);
         }, 'vAlign' => 'middle', 'filter' => false],
         ['attribute' => 'dispatch_from_code',
-            'label' => Yii::t('app', 'Source Code')],
+        'label' => Yii::t('app', 'Source Code')],
         ['attribute' => 'dispatch_from_code',
-            'label' => (Yii::t('app', 'Source Ref.Code')),
-            'value' => function($model) {
+        'label' => (Yii::t('app', 'Source Ref.Code')),
+        'value' => function($model) {
             $rel = Yii::$app->general->getDestRelation($model->dispatch_from);
             $att = strtolower($model->dispatch_from) == 'bmc' ? 'bmc_name' : (strtolower($model->dispatch_from) == 'vendor' ? 'customer_name' : (strtolower($model->dispatch_from) == 'party' ? 'party_name' : 'name'));
             if (!empty($rel) && $att !== 'party_name')
@@ -83,6 +83,7 @@ $attribute = [
         'value' => function($model) {
             return Yii::$app->controls->view_time($model->tare_weight_time);
         }, 'filter' => false],
+        ['attribute' => 'dock_no'],
 ];
 
 $grid_option = [
