28af764973985250b5eb15d441a41c88b99acaf0
Atul Patel <it46@EIPL.LOCAL>
2025-04-03 11:16:12 +0530

    trip update functionality added

--- modules/tankermovement/views/tbl-vehicle-trip/_form_grid.php
+++ modules/tankermovement/views/tbl-vehicle-trip/_form_grid.php
@@ -115,7 +115,7 @@ $grid_option = [
     'actions' => [
         'view' => TRUE,
         'update' => function ($url, $model) {
-            $class = ($model->trip_status != 'closed' && $model->is_auto_trip == 0) ? '' : 'link-disable';
+            $class = ($model->trip_status != 'closed' && $model->is_auto_trip == 1) ? '' : 'link-disable';
             $options = [
                 'class' => 'edit-trip ' . $class,
                 'title' => Yii::t('app', 'Edit Trip Detail'),
