cf0cf57c434f620948204392110507e82146462a
Atul Patel <it46@EIPL.LOCAL>
2025-04-03 11:10:13 +0530

    trip update functionality added

--- modules/tankermovement/models/TblVehicleTripSearch.php
+++ modules/tankermovement/models/TblVehicleTripSearch.php
@@ -41,7 +41,7 @@ class TblVehicleTripSearch extends TblVehicleTrip {
      */
     public function search($params) {
         $query = TblVehicleTrip::find()->alias('t')->select(['t.is_active', 't.vehicle_trip_code', 't.vehicle_code', 't.trip_code', 't.transaction_date', 't.grn_no', 't.trip_status',
-            't.trip_mode', 't.union_code', 't.plant_code', 't.mcc_plant_code', 't.bmc_code', 't.trip_sub_status',
+            't.trip_mode', 't.union_code', 't.plant_code', 't.mcc_plant_code', 't.bmc_code', 't.trip_sub_status', 't.trip_for',
             'challan_no' => "STUFF((
           SELECT ',' + d.challan_no
           FROM tbl_bmc_milk_dispatch d WHERE d.trip_code=t.trip_code
@@ -84,7 +84,7 @@ class TblVehicleTripSearch extends TblVehicleTrip {
                 ->andFilterWhere(['like', 't.trip_sub_status', $this->trip_sub_status])
                 ->andFilterWhere(['like', 't.trip_mode', $this->trip_mode]);
         $query->groupBy(['t.is_active', 't.vehicle_trip_code', 't.vehicle_code', 't.trip_code', 't.transaction_date', 't.grn_no', 't.trip_status',
-            't.trip_mode', 't.union_code', 't.plant_code', 't.mcc_plant_code', 't.bmc_code', 't.trip_sub_status']);
+            't.trip_mode', 't.union_code', 't.plant_code', 't.mcc_plant_code', 't.bmc_code', 't.trip_sub_status', 't.trip_for']);
         $query->orderBy(['transaction_date' => SORT_DESC, 'vehicle_trip_code' => SORT_ASC]);
         return $dataProvider;
     }
