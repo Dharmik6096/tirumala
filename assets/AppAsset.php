a77fe1a1464f9bc196c60533a1d8974f3e770e4f
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2025-04-09 12:23:09 +0530

    design issue resolved in vehicle trip

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -41,11 +41,10 @@ class AppAsset extends AssetBundle {
         'themes/emilk/assets/js/style.js',
         'themes/emilk/assets/js/jquery.CongelarFilaColumna.js',
         'themes/emilk/assets/js/html2excel.js',
-        // 'vendor/bower-asset/jquery-ui/ui/minified/core.min.js',
-        // 'vendor/bower-asset/jquery-ui/ui/minified/widget.min.js',
-        // 'vendor/bower-asset/jquery-ui/ui/minified/mouse.min.js',
-        // 'vendor/bower-asset/jquery-ui/ui/minified/sortable.min.js'
-        
+        'themes/emilk/assets/js/core.min.js',
+        'themes/emilk/assets/js/widget.min.js',
+        'themes/emilk/assets/js/mouse.min.js',
+        'themes/emilk/assets/js/sortable.min.js',        
     ];
     public $depends = [
         'yii\web\YiiAsset',
