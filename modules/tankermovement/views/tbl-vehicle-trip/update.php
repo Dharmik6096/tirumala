9ae02ab6465fbe1a6b2e0fe83167a557a965f2b5
Atul Patel <it46@EIPL.LOCAL>
2025-04-09 14:29:34 +0530

    title change in trip update form

--- modules/tankermovement/views/tbl-vehicle-trip/update.php
+++ modules/tankermovement/views/tbl-vehicle-trip/update.php
@@ -1,5 +1,5 @@
 <?php
-$this->title = Yii::$app->label->title('create', 'Vehicle Trip');
+$this->title = Yii::$app->label->title('edit', 'Vehicle Trip');
 ?>
 <div class="panel panel-default panel-main">
     <div class="panel-heading"><?= $this->title ?></div>
