24566d4b8d2acd34b97d0bb4af5e7e54926cb78d
Tejas Thakkar <tejas.thakkar@yamatech.in>
2025-04-22 15:19:53 +0530

    TankerMovement Change

--- modules/transporter/models/TblVehicleMaster.php
+++ modules/transporter/models/TblVehicleMaster.php
@@ -342,16 +342,15 @@ class TblVehicleMaster extends \app\models\ChildModel {
                 ->select(['tbl_vehicle_master.vehicle_code', 'tbl_vehicle_master.parsing_no'])
                 ->innerJoin('tbl_vehicle_trip', 'tbl_vehicle_master.vehicle_code = tbl_vehicle_trip.vehicle_code')
                 ->innerJoin('tbl_vehicle_trip_detail', 'tbl_vehicle_trip_detail.trip_code = tbl_vehicle_trip.trip_code')
-                ->where(['tbl_vehicle_master.union_code' => $unionCode, 'tbl_vehicle_trip_detail.source_org_type' => $type, 'tbl_vehicle_trip_detail.source_org_code' => $code]);
+                ->where(['tbl_vehicle_master.union_code' => $unionCode, 'tbl_vehicle_trip_detail.source_org_type' => $type, 'tbl_vehicle_trip_detail.source_org_code' => $code, 'tbl_vehicle_master.vehicle_use_type' => [1, 2]])
+                ->andWhere(['<=', 'tbl_vehicle_trip.transaction_date', $transaction_date])
+                ->andWhere(['IS NOT', 'arrival_time', null])
+                ->andWhere(['IS', 'departure_time', null]);
 
         if ($milkReceipt) {
-            $query->andWhere(['tbl_vehicle_master.vehicle_use_type' => [1, 2], 'tbl_vehicle_trip.trip_sub_status' => 'gate_in'])
-                    ->andWhere(['NOT', ['tbl_vehicle_trip.trip_status' => 'closed']]);
+            $query->andWhere(['NOT', ['tbl_vehicle_trip.trip_status' => 'closed']]);
         } else {
-            $query->andWhere(['tbl_vehicle_trip.trip_status' => ['generated', 'open']])
-                    ->andWhere(['<=', 'tbl_vehicle_trip.transaction_date', $transaction_date])
-                    ->andWhere(['IS NOT', 'arrival_time', null])
-                    ->andWhere(['IS', 'departure_time', null]);
+            $query->andWhere(['tbl_vehicle_trip.trip_status' => ['generated', 'open']]);
         }
         $data = $query->groupBy(['tbl_vehicle_master.vehicle_code', 'tbl_vehicle_master.parsing_no'])->all();
         return ArrayHelper::map($data, 'vehicle_code', function($value) {
