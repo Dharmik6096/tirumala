bc516383d452a3ca3dcb85cb469a347d2e2b55ad
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2025-05-15 18:07:02 +0530

    Create competitors applicability functionality

---
+++ modules/organisation/views/tbl-competitors/_search.php
@@ -0,0 +1,58 @@
+<?php
+
+use app\components\ActiveForm;
+?>
+
+<div class="modal modal-default fade" id="mis_report_search_filter" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close close-import" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title"><?php echo Yii::t('app', 'Search Competitors'); ?></h4>
+            </div>
+            <div class="">
+                <?php
+                $form = ActiveForm::begin(['options' => [
+                                'id' => 'competitor-form',
+                                'field-class' => 'form-group col-sm-4'
+                            ],
+                            'method' => 'get',
+                            'validateOnBlur' => FALSE,
+                            'validateOnChange' => FALSE,
+                            'enableClientValidation' => true,
+                            'validateOnSubmit' => true,
+                ]);
+                ?>   
+                <div class="row margin_0">
+
+                    <div class="modal-body">
+
+                        <div class="col-sm-4">
+                            <?= Yii::$app->dropdown->federation_union($model, $form, 'union_code'); ?>
+                        </div>
+
+                        <div class="col-sm-4">
+                            <?= Yii::$app->dropdown->union_plant($model, $form, 'tblcompetitorsapplicability-union_code', 'plant_code'); ?>
+                        </div>
+
+
+                        <div class="col-sm-4">
+                            <?= Yii::$app->dropdown->plant_mcc($model, $form, 'tblcompetitorsapplicability-plant_code', 'mcc_plant_code'); ?>
+                        </div>
+
+
+                        <div class="col-sm-4">
+                            <?= Yii::$app->dropdown->mcc_bmc($model, $form, 'tblcompetitorsapplicability-mcc_plant_code', 'bmc_code'); ?>
+                        </div>
+                    </div>
+
+                    <div class="modal-footer mt10 col-sm-12">
+                        <?= Yii::$app->controls->search(); ?>
+                        <button type="button" class="btn btn-danger close-import" data-dismiss="modal"><?= Yii::t('app', 'Cancel') ?></button>
+                    </div>
+                </div>
+                <?php ActiveForm::end(); ?>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ modules/organisation/views/tbl-competitors/create.php
@@ -0,0 +1,189 @@
+<?php
+
+use yii\helpers\Html;
+use app\components\ActiveForm;
+use yii\web\View;
+
+$button = Yii::$app->label->button('create');
+$this->title = Yii::t('app', 'Competitor : ' . $competitor->competitor_name);
+$defaultToggle = true;
+?>
+<div class="panel panel-default panel-main">
+
+    <div class="panel-heading"><?= Html::encode($this->title) ?></div>
+    <div class="panel-body">
+        <div class="padding-0">
+            <?php echo $this->render('_search', ['model' => $searchModel]); ?> 
+            <?php
+            $form = ActiveForm::begin(['options' => [
+                            'field-class' => 'form-group col-sm-3'
+                        ], 'validateOnBlur' => FALSE,
+                        'validateOnChange' => FALSE,
+                        'enableClientValidation' => true,
+                        'validateOnSubmit' => true,
+                        'fieldConfig' => [
+            ]]);
+            ?>
+            <?php echo $form->errorSummary($saveModel); ?>
+            <div class="panel-body set_checkbox padding_top_0 tbl_border">
+
+                <div class="search-filter padding_left_0 padding_right_0 searchBtnReport text-right beforeGridLoad">
+                    <div class="btn-group btn btn-default mis_report_modal_toggle"><i class="fa fa-search"></i></div>
+                </div>
+                <div class="clearfix"></div>
+                <div class="panel-subheading hide_help_block">
+                    <div class="row">
+                        <div class="table-responsive">
+                            <?php
+                            if (!empty($model)) {
+                                $defaultToggle = false;
+                                $count = count($model);
+                                $disp_table = $count / 3;
+                                $first_table = ceil($disp_table);
+                                $second_table = $first_table * 2;
+                                ?>                                
+                                <div class="col-sm-4 set_overflow">
+                                    <div class="whole_border">
+                                        <div class="col-sm-12 col-md-12 padding_10_0 theme-box padding_left_0 padding_right_0 margin-bottom-10 clearfix">
+                                            <h4 class="theme-box-heading"><?= Yii::t('app', 'Societies') ?></h4>
+                                        </div>
+                                        <table class="table table-bordered table-striped table-main table-language table-rate">
+                                            <thead>
+                                                <tr>
+                                                    <th width='17%' height='25' class='center-align center_text'>
+                                                        <?= Html::checkbox('allowCashCheckAll', false, ['id' => 'allowCashCheckAll', 'class' => 'checkbox checkboxHeight', 'label' => '']) ?></th>
+                                                    <th width='60%' height='25'><?php echo $model[0]->getAttributeLabel('dcs_name') ?></th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php for ($i = 0; $i < $first_table; $i++) {
+                                                    ?>
+                                                    <tr>
+                                                        <td class='center-align center_text'>
+                                                            <?= $form->field($saveModel[$i], '[' . $i . ']customer_code')->checkbox(['checked' => isset($existData[$model[$i]->dcs_code]), 'class' => 'allow-cash-checkbox checkboxHeight', 'value' => $model[$i]->dcs_code], false)->label(false); ?></td>
+                                                        <td><?= $model[$i]->dcs_name . '(' . $model[$i]->dcs_code_ex . ')'; ?></td>
+                                                    </tr>
+                                                <?php } ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4 set_overflow">
+                                    <div class="whole_border">
+                                        <div class="col-sm-12 col-md-12 padding_10_0 theme-box padding_left_0 padding_right_0 margin-bottom-10 clearfix">
+                                            <h4 class="theme-box-heading"><?= Yii::t('app', 'Societies') ?></h4>
+                                        </div>
+                                        <table class="table table-bordered table-striped table-main table-language table-rate">
+                                            <thead>
+                                                <tr>
+                                                    <th width='17%' height='25'></th>
+                                                    <th width='60%' height='25'><?php echo $model[0]->getAttributeLabel('dcs_name') ?></th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                for ($i = $first_table; $i < $second_table; $i++) {
+                                                    if (isset($model[$i])) {
+                                                        ?>
+                                                        <tr>
+                                                            <td class='center-align center_text'><?= $form->field($saveModel[$i], '[' . $i . ']customer_code')->checkbox(['checked' => isset($existData[$model[$i]->dcs_code]), 'class' => 'allow-cash-checkbox checkboxHeight', 'value' => $model[$i]->dcs_code], false)->label(false); ?></td>
+                                                            <td><?= $model[$i]->dcs_name . '(' . $model[$i]->dcs_code_ex . ')'; ?></td>
+                                                        </tr>
+                                                        <?php
+                                                    }
+                                                }
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4 set_overflow">
+                                    <div class="whole_border">
+                                        <div class="col-sm-12 col-md-12 padding_10_0 theme-box padding_left_0 padding_right_0 margin-bottom-10 clearfix">
+                                            <h4 class="theme-box-heading"><?= Yii::t('app', 'Societies') ?></h4>
+                                        </div>
+                                        <table class="table table-bordered table-striped table-main table-language table-rate">
+                                            <thead>
+                                                <tr>
+                                                    <th width='17%' height='25'></th>
+                                                    <th width='60%' height='25'><?php echo $model[0]->getAttributeLabel('dcs_name') ?></th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                for ($i = $second_table; $i < $count; $i++) {
+                                                    ?>
+                                                    <tr>
+                                                        <td class='center-align center_text'><?= $form->field($saveModel[$i], '[' . $i . ']customer_code')->checkbox(['checked' => isset($existData[$model[$i]->dcs_code]), 'class' => 'allow-cash-checkbox checkboxHeight', 'value' => $model[$i]->dcs_code], false)->label(false); ?></td>
+                                                        <td><?= $model[$i]->dcs_name . '(' . $model[$i]->dcs_code_ex . ')'; ?></td>
+                                                    </tr>
+                                                <?php } ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <?php
+                            } else {
+                                if (!empty($searchModel->f_mcc_code)) {
+                                    echo "<p class='text-center'>" . Yii::t('app', 'Selected MCC does not contains any DCS or Each DCS has already in Force Rate Download stage') . "</p>";
+                                } else {
+                                    echo "<p class='text-center'>" . Yii::t('app', 'Data Not Available') . "</p>";
+                                }
+                                $defaultToggle = true;
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="panel-footer shortcut-main" shortcut="true" display_shortcut="false" hilight_shortcut="false">
+
+                <?php if (!empty($model)) { ?>
+                    <?= Yii::$app->controls->save($button, $model[0]); ?>
+                    <?= Yii::$app->controls->reset(); ?>
+                <?php } ?>
+                <?= Yii::$app->controls->cancel($searchModel); ?>
+            </div>
+            <?php ActiveForm::end(); ?>
+
+        </div>
+    </div>
+</div>
+
+<?php
+$script = "
+
+    $('#allowCashCheckAll').click(function () {
+        var check =this.checked;
+        $('.allow-cash-checkbox').each(function () {
+            this.checked = check;
+        });
+    });
+    $('.allow-cash-checkbox').each(function () {
+        $('#allowCashCheckAll').prop('checked', true);
+        if(this.checked == false){
+            $('#allowCashCheckAll').prop('checked', false);
+        }
+    });
+    
+    $(document).ready(function () {
+        var lengthOfUl = $('.error-summary ul li').length;
+        if(lengthOfUl > 0) {
+            $('.error-summary ul li:eq(0)').before('<li>" . Yii::t('app', 'Member payment is Already restricted for following.') . "</li>');
+        }
+    });
+    $('.mis_report_modal_toggle').on('click', function(){
+        $('#mis_report_search_filter').modal('toggle');
+    });
+        
+";
+if ($defaultToggle) {
+    $script .= "
+        $(document).ready(function () {
+            $('#mis_report_search_filter').modal('toggle');
+        });
+        
+        
+    ";
+}
+$this->registerJs($script, View::POS_END, 'force-rate-download');
