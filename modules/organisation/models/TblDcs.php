b718feb799be3599e958eeddda4f24d787e06195
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2025-06-16 17:27:40 +0530

    route invalid issue resolved for dcs import

--- modules/organisation/models/TblDcs.php
+++ modules/organisation/models/TblDcs.php
@@ -878,7 +878,7 @@ class TblDcs extends ChildModel {
     }
 
     public function getRouteMapping() {
-        return $this->hasOne(TblRouteMapping::className(), ['route_code' => 'route_code']);
+        return $this->hasOne(TblRouteMapping::className(), ['route_code' => 'route_code', 'union_code' => 'union_code']);
     }
 
     public function getMccDCS($mccCode = [], $RLS = 'TRUE', $values = []) {
@@ -1242,7 +1242,7 @@ class TblDcs extends ChildModel {
     }
 
     public function getRouteRefCode() {
-        return $this->hasOne(TblRouteMapping::className(), ['ref_code' => 'route_code']);
+        return $this->hasOne(TblRouteMapping::className(), ['ref_code' => 'route_code', 'union_code' => 'union_code']);
     }
 
     public function validateRoute($attribute, $params) {
