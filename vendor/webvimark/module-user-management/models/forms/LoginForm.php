6d38ab67461bf60f4d611a62c52583d0307b582f
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2025-06-30 14:41:27 +0530

    amul to comman merge

--- vendor/webvimark/module-user-management/models/forms/LoginForm.php
+++ vendor/webvimark/module-user-management/models/forms/LoginForm.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace webvimark\modules\UserManagement\models\forms;
 
 use webvimark\helpers\LittleBigHelper;
@@ -8,92 +7,108 @@ use webvimark\modules\UserManagement\UserManagementModule;
 use yii\base\Model;
 use Yii;
 
-class LoginForm extends Model {
+class LoginForm extends Model
+{
+	public $username;
+	public $password;
+	public $rememberMe = false;
 
-    public $username;
-    public $password;
-    public $rememberMe = false;
-    private $_user = false;
+	private $_user = false;
 
-    /**
-     * @inheritdoc
-     */
-    public function rules() {
-        return [
-                [['username', 'password'], 'required'],
-                ['rememberMe', 'boolean'],
-                ['password', 'validatePassword'],
-                ['username', 'validateIP'],
-        ];
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function rules()
+	{
+		return [
+			[['username', 'password'], 'required'],
+			['rememberMe', 'boolean'],
+			['password', 'validatePassword'],
 
-    public function attributeLabels() {
-        return [
-            'username' => UserManagementModule::t('front', 'Username'),
-            'password' => UserManagementModule::t('front', 'Password'),
-            'rememberMe' => UserManagementModule::t('front', 'Remember me'),
-        ];
-    }
+			['username', 'validateIP'],
+		];
+	}
 
-    /**
-     * Validates the password.
-     * This method serves as the inline validation for password.
-     */
-    public function validatePassword() {
-        if (!Yii::$app->getModule('user-management')->checkAttempts()) {
-            $this->addError('password', UserManagementModule::t('front', 'Too many attempts'));
+	public function attributeLabels()
+	{
+		return [
+			'username'   => UserManagementModule::t('front', 'Username'),
+			'password'   => UserManagementModule::t('front', 'Password'),
+			'rememberMe' => UserManagementModule::t('front', 'Remember me'),
+		];
+	}
 
-            return false;
-        }
+	/**
+	 * Validates the password.
+	 * This method serves as the inline validation for password.
+	 */
+	public function validatePassword()
+	{
+		if ( !Yii::$app->getModule('user-management')->checkAttempts() )
+		{
+			$this->addError('password', UserManagementModule::t('front', 'Too many attempts'));
 
-        if (!$this->hasErrors()) {
-            $user = $this->getUser();
-            if (!$user || !$user->validatePassword($this->password)) {
-                $this->addError('password', UserManagementModule::t('front', 'Incorrect username or password.'));
-            }
-        }
-    }
+			return false;
+		}
 
-    /**
-     * Check if user is binded to IP and compare it with his actual IP
-     */
-    public function validateIP() {
-        $user = $this->getUser();
+		if ( !$this->hasErrors() )
+		{
+			$user = $this->getUser();
+			if ( !$user || !$user->validatePassword($this->password) )
+			{
+				$this->addError('password', UserManagementModule::t('front', 'Incorrect username or password.'));
+			}
+		}
+	}
 
-        if ($user AND $user->bind_to_ip) {
-            $ips = explode(',', $user->bind_to_ip);
+	/**
+	 * Check if user is binded to IP and compare it with his actual IP
+	 */
+	public function validateIP()
+	{
+		$user = $this->getUser();
 
-            $ips = array_map('trim', $ips);
+		if ( $user AND $user->bind_to_ip )
+		{
+			$ips = explode(',', $user->bind_to_ip);
 
-            if (!in_array(LittleBigHelper::getRealIp(), $ips)) {
-                $this->addError('password', UserManagementModule::t('front', "You could not login from this IP"));
-            }
-        }
-    }
+			$ips = array_map('trim', $ips);
 
-    /**
-     * Logs in a user using the provided username and password.
-     * @return boolean whether the user is logged in successfully
-     */
-    public function login() {
-        if ($this->validate()) {
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? Yii::$app->user->cookieLifetime : 0);
-        } else {
-            return false;
-        }
-    }
+			if ( !in_array(LittleBigHelper::getRealIp(), $ips) )
+			{
+				$this->addError('password', UserManagementModule::t('front', "You could not login from this IP"));
+			}
+		}
+	}
 
-    /**
-     * Finds user by [[username]]
-     * @return User|null
-     */
-    public function getUser() {
-        if ($this->_user === false) {
-            $u = new \Yii::$app->user->identityClass;
-            $this->_user = ($u instanceof User ? $u->findByUsername($this->username) : User::findByUsername($this->username));
-        }
+	/**
+	 * Logs in a user using the provided username and password.
+	 * @return boolean whether the user is logged in successfully
+	 */
+	public function login()
+	{
+		if ( $this->validate() )
+		{
+			return Yii::$app->user->login($this->getUser(), $this->rememberMe ? Yii::$app->user->cookieLifetime : 0);
+		}
+		else
+		{
+			return false;
+		}
+	}
 
-        return $this->_user;
-    }
+	/**
+	 * Finds user by [[username]]
+	 * @return User|null
+	 */
+	public function getUser()
+	{
+		if ( $this->_user === false )
+		{
+			$u = new \Yii::$app->user->identityClass;
+			$this->_user = ($u instanceof User ? $u->findByUsername($this->username) : User::findByUsername($this->username));
+		}
 
+		return $this->_user;
+	}
 }
