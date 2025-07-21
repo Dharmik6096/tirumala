a85355a8fe62719eec5877320b3fbf517aa881f9
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2025-07-21 11:32:29 +0530

    dcs local contact person issue resolve

--- modules/organisation/models/TblDcs.php
+++ modules/organisation/models/TblDcs.php
@@ -175,11 +175,11 @@ class TblDcs extends ChildModel {
             [['allow_multi_family_member', /* 'destination_type', */], 'integer', 'except' => ['routeMapping']],
             //  [['tin_no'], 'string', 'max' => 11, 'min' => 11],
             [['vendor_code', 'is_active', 'created_at', 'milk_type_code', 'destination_code', 'destination_type', 'effective_date', 'registration_date', 'updated_at', 'villages', 'branch_code', 'route_code', 'federation_code', 'upi_no', 'hamlet_code', 'secretory_info', 'gst_no', 'fssi', 'organisation_type_code', 'scheme_type_code', 'is_registered', 'street1', 'street2', 'valid_from', 'bipl_code', 'vendor', 'data_post_status', 'bmc_code', 'mcc_plant_code', 'plant_code', 'is_name_request', 'rate_flag', 'dpu_type', 'rate_chart_member', 'is_live', 'dcs_code_ex', 'ref_code', 'credit_sale_allow', 'default_milk_type', 'milk_type_auto', 'auto_member_create', 'beneficiary_name', 'operation', 'file_name', 'aadhaar_no', 'sap_vendor_code', 'antibiotic_check', 'ts_code_m', 'ts_code_e', 'cutoff', 'lower_milk_type', 'cutoff_val', 'employee_id', 'fssi_expiry_date'], 'safe'],
-            [['fssi_expiry_date'], 'required', 'when' => function ($model) {
+                [['fssi_expiry_date'], 'required', 'when' => function ($model) {
                     return !empty($model->fssi);
                 }, 'whenClient' => "function (attribute, value) {return $('#tbldcs-fssi').val() !== '';
             }"],
-            [['sap_vendor_code'], 'unique', 'targetAttribute' => ['sap_vendor_code', 'union_code'], 'skipOnEmpty' => true, 'message' => Yii::t('app/validation', '{attribute} has already been taken.'), 'except' => ['routeMapping'], 'when' => function ($model) {
+                [['sap_vendor_code'], 'unique', 'targetAttribute' => ['sap_vendor_code', 'union_code'], 'skipOnEmpty' => true, 'message' => Yii::t('app/validation', '{attribute} has already been taken.'), 'except' => ['routeMapping'], 'when' => function ($model) {
                     return $model->isAttributeChanged('sap_vendor_code', FALSE);
                 }],
             //[['destination_code'],'bmcValidate','skipOnEmpty'=> false],
@@ -259,7 +259,7 @@ class TblDcs extends ChildModel {
             // [['department'], 'exist', 'skipOnError' => true, 'targetClass' => TblDepartment::className(), 'targetAttribute' => ['department' => 'department_id'], 'on' => ['importCsv']],
             [['local_contact_person', 'local_middlename', 'local_surname'], function ($attribute, $params) {
                     Yii::$app->general->vaildateLocalField($this, $attribute, $params);
-                }, 'skipOnEmpty' => false, 'except' => ['importCsv', 'routeMapping']],
+                }, 'skipOnEmpty' => false, 'except' => ['routeMapping']],
                 [['ifsc'], 'setBankDetail', 'on' => ['importCsv']],
                 [['dcs_type_code'], function ($attribute, $params) {
                     Yii::$app->general->validateGlobalData($this, $attribute, 'dcs_type_code');
