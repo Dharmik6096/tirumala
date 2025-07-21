9242d291483ef3a4b606fb822e8462fab9b28402
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2025-07-21 15:54:58 +0530

    Create login page for suddh

---
+++ modules/usermanagement/views/auth/login_suddh.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ * @var $this yii\web\View
+ * @var $model webvimark\modules\UserManagement\models\forms\LoginForm
+ */
+use app\components\ActiveForm;
+use yii\helpers\Html;
+use kartik\depdrop\DepDrop;
+use yii\helpers\Url;
+use yii\web\View;
+
+$state = $model->getStateCode();
+$org = ($model->type == 'UNION') ? 'block' : 'none';
+?>
+
+<div class="navbar fixed-top menu-wrap">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="javascript:void(0)">
+            <img
+                src="<?= $this->theme->getUrl('/assets/images/logo.png') ?>" alt="AMCS Logo"
+                class="logo img-responsive" /> 
+        </a>
+    </div>
+</div>
+
+<div class="modal fade" id="loginModal" role="dialog">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <!-- <button type="button" class="close" data-bs-dismiss="modal">&times;</button> -->
+                <h4 class="modal-title"><?= Yii::t('app', 'EVEREST Application') ?></h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <?php
+                $form = ActiveForm::begin([
+                            'id' => 'login-form',
+                            'options' => ['autocomplete' => 'off'],
+                            'validateOnBlur' => false,
+                            'fieldConfig' => [
+                                'template' => "{label}\n{input}\n{error}",
+                            ],
+                        ])
+                ?>
+
+                <?php echo $form->errorSummary($model); ?>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <?=
+                                $form->field($model, 'username')
+                                ->textInput(['placeholder' => $model->getAttributeLabel('username'), 'autocomplete' => 'off'])
+                        ?>
+                    </div>
+                    <div class="col-sm-12">
+                        <?=
+                                $form->field($model, 'password')
+                                ->passwordInput(['placeholder' => $model->getAttributeLabel('password'), 'autocomplete' => 'off'])
+                        ?>
+                    </div>
+                    <?php //if($identity['organization_type']!='NATIONAL'){  ?>
+
+                    <div id="org" class="col-sm-12">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <?php //$form->field($model, 'type', ['options' => ['class' => 'form-group']])->dropDownList(['PCDF' => 'PCDF', 'UNION' => 'UNION'], ['prompt' => 'Select Organization']); ?>
+                            </div>
+                        </div>
+                        <?= Html::activeHiddenInput($model, 'organization'); ?>
+                        <?= Html::hiddenInput('state', $state); ?>
+                    </div>
+                    <?php //}  ?>
+                    <div class="col-sm-12">
+                        <?= (isset(Yii::$app->user->enableAutoLogin) && Yii::$app->user->enableAutoLogin) ? \Yii::$app->controls->checkTemplateBootstrap5($model, $form, 'rememberMe', true) : '' ?>
+                    </div>
+                    <div class="col-sm-12">
+                        <?=
+                        Html::button(
+                                Yii::t('app', 'Login'), ['class' => 'btn-login btn btn-primary btn-block login-submit']
+                        )
+                        ?>
+                    </div>
+                </div>
+                <?php ActiveForm::end() ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="panel panel-default panel-grid panel-main">
+                    <div class="panel-body">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="login-content text-center saahaj_img">
+                                        <h3>Welcome to EIPL Milk Procurement Portal</h3>
+                                        <hr>
+                                        <?php $image_path = Yii::$app->request->baseUrl . '/themes/emilk/assets/images/'; ?>
+                                        <div class="col-sm-12">
+                                            <?= Html::img($image_path . 'suddh.png', ['class' => 'img-responsive']); ?>
+                                        </div>
+                                        <h3>About Us</h3>
+                                        <p class="text-justify">Everest brings to the industry, the most precise and advanced technology to simplify dairy and food testing process. All our products facilitate better functionality, which ultimately leads to better products reaching the consumers. This is only possible by making our products and services technologically innovative and accurate. Our vow to serve the purest evokes in us, the zeal to deliver the best, through precision, whatever we do. Our endeavor is to be the personification of the concept of "Precision Behind Purity"</p>
+                                        <a href="javascript:void(0)" class="btn-login  btn btn-danger" data-bs-toggle="modal" data-bs-target="#loginModal"><i class="fas fa-sign-in-alt"></i> Login</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?= $this->render('paymentCheck') ?>
+<?php
+if (!empty($model->getErrors())) {
+    $script = "
+        $('#loginModal').modal('show');";
+    $this->registerJs($script, View::POS_READY, 'login-code');
+}
+
+$css = <<<CSS
+        .loginError i {
+            font-size: 25px;
+            height: 30px;
+            width: 30px;
+            border: 2px solid #fff;
+            color: #fff;
+            /* line-height: 46px; */
+            border-radius: 50%
+        }
+CSS;
+
+$this->registerCss($css);
+?>
