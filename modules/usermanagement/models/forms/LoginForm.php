80995a1614e072a853c535a990b5e995f8468799
Tejas Thakkar <tejas.thakkar@yamatech.in>
2025-07-25 19:03:25 +0530

    resolve issue in login attempt dev

--- modules/usermanagement/models/forms/LoginForm.php
+++ modules/usermanagement/models/forms/LoginForm.php
@@ -481,6 +481,10 @@ class LoginForm extends \webvimark\modules\UserManagement\models\forms\LoginForm
                         } else if ($suspensionDatetime > $currentDateTime) {
                             $interval = $currentDateTime->diff($suspensionDatetime);
                             $minutesLeft = ($interval->days * 24 * 60) + ($interval->h * 60) + $interval->i;
+                            $secondsLeft = $interval->s;
+                            if ($minutesLeft == 0 && $secondsLeft > 0) {
+                                $minutesLeft = 1;
+                            }
                             $maxLoginAttempts = $minutesLeft;
                         }
                     } else if ($user->max_login_attempts > 0) {
@@ -535,6 +539,10 @@ class LoginForm extends \webvimark\modules\UserManagement\models\forms\LoginForm
                 if ($user->max_login_attempts == 0 && !empty($user->suspension_datetime) && $suspensionDatetime > $currentDateTime) {
                     $interval = $currentDateTime->diff($suspensionDatetime);
                     $minutesLeft = ($interval->days * 24 * 60) + ($interval->h * 60) + $interval->i;
+                    $secondsLeft = $interval->s;
+                    if ($minutesLeft == 0 && $secondsLeft > 0) {
+                        $minutesLeft = 1;
+                    }
                     $maxLoginAttempts = $minutesLeft;
                 } else {
                     $user->max_login_attempts = $user->suspension_datetime = NULL;
