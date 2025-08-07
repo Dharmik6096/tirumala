22de7f598d4b96f9b85cd64346086aa782cb6b08
Atul Patel (IT46/CAPITAL) <it46@EIPL.LOCAL>
2025-08-07 13:03:13 +0530

    sentbox denerate master crone change

--- modules/bkgprocess/controllers/SchedulerController.php
+++ modules/bkgprocess/controllers/SchedulerController.php
@@ -669,7 +669,6 @@ class SchedulerController extends ChildController {
                             $row->response_datetime = date('Y-m-d H:i:s');
                             $row->resp_desc = 'Sentbox Generated';
                             if (!empty($uniqueUnionConfigData[$unionCode]) && $status == '0') {
-                                $modelHistory->save();
                                 $decrypt = $modelMaster->decryptModel($existData);
                                 $existData->setAttributes($decrypt);
                                 $row->remarks = trim($row->remarks . ' Deactivation CBPA Removed');
@@ -702,6 +701,7 @@ class SchedulerController extends ChildController {
                         }
                     }
                     if (!empty($uniqueUnionConfigData[$unionCode]) && $status == '0') {
+                        $modelHistory->save();
                         $existData->is_active = $is_active;
                         $existData->is_sentbox = FALSE;
                         $existData->save(TRUE, FALSE);
