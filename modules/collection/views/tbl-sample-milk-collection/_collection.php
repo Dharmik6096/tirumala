ce9d779219a0d632233038dde1286ffbe77e1d5e
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2025-09-12 12:20:40 +0530

    Version related design changes

--- modules/collection/views/tbl-sample-milk-collection/_collection.php
+++ modules/collection/views/tbl-sample-milk-collection/_collection.php
@@ -145,12 +145,12 @@ $form = ActiveForm::begin([
                                                                 }
                                                  }'),
                     ],
-                    'options' => ['class' => 'btn btn-default btn-raised',
+                    'options' => ['class' => 'btn btn-default btn-raised btn-login',
                         'type' => 'submit'],
                 ]);
                 AjaxSubmitButton::end();
                 ?>
-                <?= Yii::$app->controls->custombutton('Cancel', 'index'); ?> 
+                <?= Yii::$app->controls->custombutton('Cancel', 'index', '', 'btn-login'); ?> 
             </div>
         </div>
     </div>
