88cce178c0d46760d8b5ad38694dc1b479afe791
Tejas Thakkar <tejas.thakkar@yamatech.in>
2025-09-17 12:27:14 +0530

    resolved version issue

--- modules/veterinary/views/tbl-medicine-stock/index.php
+++ modules/veterinary/views/tbl-medicine-stock/index.php
@@ -1,7 +1,7 @@
 <?php
 $this->title = Yii::t('app', Yii::$app->label->title('list', 'Medicine Stock'));
 $this->params['menu'][] = Yii::$app->controls->import('medicine-stock', $this);
-$this->params['menu'][] = Yii::$app->controls->custombutton('Medicine Stock Transfer', 'create', '', 'btn btn-danger btn-block', '<i class="fa fa-exchange"></i>');
+$this->params['menu'][] = Yii::$app->controls->custombutton('Medicine Stock Transfer', 'create', '', 'btn btn-danger btn-block', '<i class="fa fa-exchange-alt"></i>');
 
 ?>
 <div class="panel panel-default panel-grid panel-main">
