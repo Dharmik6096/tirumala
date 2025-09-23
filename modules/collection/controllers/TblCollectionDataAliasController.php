36c7ad4434f4310fb2ff2ab942febfa307eeeebf
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2025-09-23 15:25:56 +0530

    milk coll & bmc coll & milk dispatch action perform non mandate

--- modules/collection/controllers/TblCollectionDataAliasController.php
+++ modules/collection/controllers/TblCollectionDataAliasController.php
@@ -18,6 +18,8 @@ use app\modules\collection\models\TblDcsMilkDispatchTxn;
 use app\modules\collection\models\TblMilkCollectionHistory;
 use app\modules\general\models\TblProcessApproval;
 use app\modules\general\models\TblProcessApprovalHistory;
+use app\modules\collection\models\TblDcsMilkDispatchTxnHistory;
+use app\modules\collection\models\TblDcsMilkDispatchHistory;
 
 /**
  * TblCollectionDataAliasController implements the CRUD actions for TblCollectionDataAlias model.
@@ -37,15 +39,15 @@ class TblCollectionDataAliasController extends \app\controllers\ChildController 
                     $codes = explode('###', $value_code);
                     $value = $codes[0];
                     $approval_code = !empty($codes[1]) ? $codes[1] : '';
+                    $action = !empty($codes[2]) ? $codes[2] : '';
                     $saveModel = [];
                     $deleteModel = [];
-                    $action = Yii::$app->request->post('TblCollectionDataAlias')['action_perform'];
                     $operation = Yii::$app->request->post('TblCollectionDataAlias')['operation'];
                     $existData = $this->findModel($value);
                     $approval_status = $existData->approval_status;
                     $status = '';
                     ($operation == 'approve' && ($action == 'CREATE' || $action == 'UPDATE')) ? $existData->scenario = 'MilkCollection' : '';
-                    if ($collection_config = 1) {
+                    if ($collection_config == 1) {
                         $existData->approval_status = !empty($operation == 'approve') ? 'Approve' : 'Reject';
                         $existData->approved_at = date('Y-m-d H:i:s');
                         $existData->approved_by = Yii::$app->session['UserCode'];
@@ -156,14 +158,13 @@ class TblCollectionDataAliasController extends \app\controllers\ChildController 
             $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
         }
         $searchModel->scenario = 'approvalCollection';
-        $showField = $searchModel->action_perform == 'UPDATE' ? TRUE : FALSE;
-        $id = 'milk-collection-approve-' . strtolower($searchModel->action_perform);
+        $actionPerform = $searchModel->action_perform ? strtolower($searchModel->action_perform) : 'default';
+        $id = 'milk-collection-approve-' . $actionPerform;
         $url = 'milk-collection-approve';
         return $this->render('milk_collection', [
                     'searchModel' => $searchModel,
                     'dataProvider' => $dataProvider,
                     'showFarmer' => $showFarmer,
-                    'showField' => $showField,
                     'id' => $id,
                     'url' => $url
         ]);
@@ -182,9 +183,9 @@ class TblCollectionDataAliasController extends \app\controllers\ChildController 
                     $codes = explode('###', $value_code);
                     $value = $codes[0];
                     $approval_code = !empty($codes[1]) ? $codes[1] : '';
+                    $action = !empty($codes[2]) ? $codes[2] : '';
                     $saveModel = [];
                     $deleteModel = [];
-                    $action = Yii::$app->request->post('TblCollectionDataAlias')['action_perform'];
                     $operation = Yii::$app->request->post('TblCollectionDataAlias')['operation'];
                     $existData = $this->findModel($value);
                     $approval_status = $existData->approval_status;
@@ -298,13 +299,12 @@ class TblCollectionDataAliasController extends \app\controllers\ChildController 
             $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
         }
         $searchModel->scenario = 'approvalCollection';
-        $showField = $searchModel->action_perform == 'UPDATE' ? TRUE : FALSE;
-        $id = 'bmc-collection-approve-' . strtolower($searchModel->action_perform);
+        $actionPerform = $searchModel->action_perform ? strtolower($searchModel->action_perform) : 'default';
+        $id = 'bmc-collection-approve-' . $actionPerform;
         $url = 'bmc-collection-approve';
         return $this->render('bmc_collection', [
                     'searchModel' => $searchModel,
                     'dataProvider' => $dataProvider,
-                    'showField' => $showField,
                     'id' => $id,
                     'url' => $url,
                     'showType' => $showType,
@@ -318,9 +318,11 @@ class TblCollectionDataAliasController extends \app\controllers\ChildController 
                 $errorCount = 0;
                 $deletedata = Yii::$app->request->post('selection');
                 foreach ($deletedata as $key => $value) {
+                    $codes = explode('###', $value);
+                    $value = $codes[0];
+                    $action = !empty($codes[1]) ? $codes[1] : '';
                     $saveModel = [];
                     $deleteModel = [];
-                    $action = Yii::$app->request->post('TblCollectionDataAlias')['action_perform'];
                     $operation = Yii::$app->request->post('TblCollectionDataAlias')['operation'];
                     $existData = $this->findModel($value);
                     ($operation == 'approve' && ($action == 'CREATE' || $action == 'UPDATE')) ? $existData->scenario = 'MilkDispatch' : '';
@@ -346,27 +348,43 @@ class TblCollectionDataAliasController extends \app\controllers\ChildController 
                             } else if ($action == 'UPDATE') {
                                 $MainModel = new TblDcsMilkDispatch();
                                 $existMainData = $MainModel->getExistingDispatch($existData);
-                                $existMainData->attributes = $existData->attributes;
-                                $saveModel[] = $existMainData;
+                                if (!empty($existMainData)) {
+                                    $historyModel = new TblDcsMilkDispatchHistory();
+                                    Yii::$app->operation->history($existMainData, $historyModel, 'UPDATE');
+                                    $saveModel[] = $historyModel;
+                                    $existMainData->attributes = $existData->attributes;
+                                    $saveModel[] = $existMainData;
 
-                                $TxModel = new TblDcsMilkDispatchTxn();
-                                $TxModel->dcs_milk_dispatch_code = $existMainData->dcs_milk_dispatch_code;
-                                $existTxModel = $TxModel->getTxnExistingCollection($existData);
-                                $existTxModel->attributes = $existData->attributes;
-                                $existTxModel->setModelData($existData, $existTxModel);
-                                $saveModel[] = $existTxModel;
+                                    $TxModel = new TblDcsMilkDispatchTxn();
+                                    $TxModel->dcs_milk_dispatch_code = $existMainData->dcs_milk_dispatch_code;
+                                    $existTxModel = $TxModel->getTxnExistingCollection($existData);
+                                    if (!empty($existTxModel)) {
+                                        $historyTxnModel = new TblDcsMilkDispatchTxnHistory();
+                                        Yii::$app->operation->history($existTxModel, $historyTxnModel, 'UPDATE');
+                                        $saveModel[] = $historyTxnModel;
+
+                                        $existTxModel->attributes = $existData->attributes;
+                                        $existTxModel->setModelData($existData, $existTxModel);
+                                        $saveModel[] = $existTxModel;
+                                    }
+                                }
                             } else if ($action == 'DELETE') {
                                 $MainModel = new TblDcsMilkDispatch();
                                 $existMainData = $MainModel->getExistingDispatch($existData);
-                                $txCount = TblDcsMilkDispatchTxn::find()->where(['dcs_milk_dispatch_code' => $existMainData->dcs_milk_dispatch_code])->count();
-                                if (!empty($existMainData) && $txCount <= 1) {
-                                    $deleteModel[] = $existMainData;
-                                }
-                                $TxModel = new TblDcsMilkDispatchTxn();
-                                $TxModel->dcs_milk_dispatch_code = $existMainData->dcs_milk_dispatch_code;
-                                $existTxModel = $TxModel->getTxnExistingCollection($existData);
-                                if (!empty($existTxModel)) {
-                                    $deleteModel[] = $existTxModel;
+                                if (!empty($existMainData)) {
+                                    $txCount = TblDcsMilkDispatchTxn::find()->where(['dcs_milk_dispatch_code' => $existMainData->dcs_milk_dispatch_code])->count();
+                                    if (!empty($existMainData) && $txCount <= 1) {
+                                        $deleteModel[] = $existMainData;
+                                    }
+                                    $TxModel = new TblDcsMilkDispatchTxn();
+                                    $TxModel->dcs_milk_dispatch_code = $existMainData->dcs_milk_dispatch_code;
+                                    $existTxModel = $TxModel->getTxnExistingCollection($existData);
+                                    if (!empty($existTxModel)) {
+                                        $historyTxnModel = new TblDcsMilkDispatchTxnHistory();
+                                        Yii::$app->operation->history($existTxModel, $historyTxnModel, 'UPDATE');
+                                        $saveModel[] = $historyTxnModel;
+                                        $deleteModel[] = $existTxModel;
+                                    }
                                 }
                             }
                         }
@@ -410,13 +428,12 @@ class TblCollectionDataAliasController extends \app\controllers\ChildController 
         $searchModel->table_name = 'tbl_dcs_milk_dispatch';
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
         $searchModel->scenario = 'approvalDispatch';
-        $showField = $searchModel->action_perform == 'UPDATE' ? TRUE : FALSE;
-        $id = 'milk-dispatch-approve-' . strtolower($searchModel->action_perform);
+        $actionPerform = $searchModel->action_perform ? strtolower($searchModel->action_perform) : 'default';
+        $id = 'milk-dispatch-approve-' . $actionPerform;
         $url = 'milk-dispatch-approve';
         return $this->render('milk_dispatch', [
                     'searchModel' => $searchModel,
                     'dataProvider' => $dataProvider,
-                    'showField' => $showField,
                     'id' => $id,
                     'url' => $url,
         ]);
