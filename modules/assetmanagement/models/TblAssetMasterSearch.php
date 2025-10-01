48a681df48104dc4c1e0086017b23e5e80c659c9
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2025-10-01 12:10:12 +0530

    Add RLS for Asset nodule

--- modules/assetmanagement/models/TblAssetMasterSearch.php
+++ modules/assetmanagement/models/TblAssetMasterSearch.php
@@ -47,6 +47,7 @@ class TblAssetMasterSearch extends TblAssetMaster {
         ]);
 
         $this->load($params);
+        Yii::$app->general->filterByOrg($query, $this);
 
         if (!$this->validate()) {
             // uncomment the following line if you do not want to return any records when validation fails
