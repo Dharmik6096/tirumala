4bebb8d022825b1ef54deff7c7cb7aa9e714e352
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2025-10-06 14:59:43 +0530

    member provisional filter optimization

--- modules/dcsoperation/models/TblMemberProvisionalSearch.php
+++ modules/dcsoperation/models/TblMemberProvisionalSearch.php
@@ -95,7 +95,7 @@ class TblMemberProvisionalSearch extends TblMemberProvisional {
         if (!$date_search) {
             $from_date = !empty($this->from_date) ? $this->from_date : date('Y-m-d');
             $to_date = !empty($this->to_date) ? $this->to_date : date('Y-m-d');
-            $query->andFilterWhere(['and', ['>=', 'tbl_member_provisional.created_at', date('Y-m-d', strtotime($from_date)) . ' 00:00:00.000'], ['<=', 'tbl_member_provisional.created_at', date('Y-m-d', strtotime($to_date)) . ' 23:59:59.000']]);
+            $query->andFilterWhere(['between', 'tbl_member_provisional.created_at', date('Y-m-d', strtotime($from_date)) . ' 00:00:00.000', date('Y-m-d', strtotime($to_date)) . ' 23:59:59.000']);
         }
 
         Yii::$app->general->filterByOrg($query, $this);
