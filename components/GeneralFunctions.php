9d4b287270277f3efd6fdf8733e8ae779b1a8ac5
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2025-11-24 18:06:49 +0530

    Change Theme for ITC

--- components/GeneralFunctions.php
+++ components/GeneralFunctions.php
@@ -2557,12 +2557,12 @@ class GeneralFunctions extends Component {
             $unionData = TblUnions::find()->where(['is_active' => 1])->one();
             $eiplCode = !empty($unionData) && !empty($unionData->eipl_code) ? ($unionData->eipl_code) : '';
         }
-        $eipl_css_file_path = 'themes/pcdf/assets/css/style.css';
-        $eipl_js_file_path = 'themes/pcdf/assets/js/style.js';
-        $client_css_file_path = 'themes/pcdf/assets/css/style_' . strtolower($eiplCode) . '.css';
-        $client_js_file_path = 'themes/pcdf/assets/js/style_' . strtolower($eiplCode) . '.js';
-        $check_client_css_file_path = \Yii::$app->basePath . '/' . $client_css_file_path;
-        $check_client_js_file_path = \Yii::$app->basePath . '/' . $client_js_file_path;
+        $eipl_css_file_path = 'themes/emilk/assets/css/style.css';
+        $eipl_js_file_path = 'themes/emilk/assets/js/style.js';
+        $client_css_file_path = 'themes/emilk/assets/css/style_' . strtolower($eiplCode) . '.css';
+        $client_js_file_path = 'themes/emilk/assets/js/style_' . strtolower($eiplCode) . '.js';
+        $check_client_css_file_path = Yii::getAlias('@webroot') . '/' . $client_css_file_path;
+        $check_client_js_file_path = Yii::getAlias('@webroot') . '/' . $client_js_file_path;
         if (file_exists($check_client_css_file_path)) {
             if (($key = array_search($eipl_css_file_path, $layout->css)) !== false) {
                 unset($layout->css[$key]);
