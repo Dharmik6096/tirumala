8ec1fa536b7edd3042fa5092db2dad29a60a267d
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2025-12-16 15:12:38 +0530

    role and mapping changes

--- modules/usermanagement/views/permission/_form.php
+++ modules/usermanagement/views/permission/_form.php
@@ -12,8 +12,8 @@ use yii\helpers\Html;
 
 $title = Yii::$app->label->title($type, 'Permission');
 $button = Yii::$app->label->button($type);
-
 $this->title = Yii::t('app', $title);
+$readonly = $type == 'create' ? FALSE : TRUE;
 ?>
 
 <?php
@@ -30,7 +30,7 @@ $form = ActiveForm::begin([
         <?= $form->field($model, 'description')->textInput(['maxlength' => 255, 'autofocus' => $model->isNewRecord ? true : false]) ?>
     </div>
     <div class="col-sm-2">
-        <?= $form->field($model, 'name')->textInput(['maxlength' => 64]) ?>
+        <?= $form->field($model, 'name')->textInput(['maxlength' => 64, 'readonly' => $readonly]) ?>
     </div>
     <div class="col-sm-2">
         <?=
