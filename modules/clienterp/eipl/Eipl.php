dd827cf5ce85fc67986c84d4143b8a0552cdeeba
Atul Patel (IT46/CAPITAL) <it46@EIPL.LOCAL>
2025-12-29 12:55:58 +0530

    device data store api created

---
+++ modules/clienterp/eipl/Eipl.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\clienterp\eipl;
+
+/**
+ * eipl module definition class
+ */
+class Eipl extends \yii\base\Module {
+
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\clienterp\eipl\controllers';
+
+}
---
+++ modules/clienterp/eipl/controllers/PushMasterController.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace app\modules\clienterp\eipl\controllers;
+
+class PushMasterController extends \app\modules\clienterp\controllers\PushMasterController {
+    
+}
