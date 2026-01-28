1bb9fac8b60baa2f2bd46470a4cb10ced5b8448f
Vivek Patel (IT33/CAPITAL) <it33@EIPL.LOCAL>
2026-01-28 13:55:26 +0530

    Label changes

--- modules/dcsoperation/models/TblBiplSmart.php
+++ modules/dcsoperation/models/TblBiplSmart.php
@@ -17,6 +17,9 @@ class TblBiplSmart extends \app\models\ChildModel {
 
     public function attributeLabels() {
         return [
+            'f_plant_code' => Yii::t('app', 'Plant'),
+            'f_mcc_plant_code' => Yii::t('app', 'MCC'),
+            'f_bmc_code' => Yii::t('app', 'BMC'),
         ];
     }
 
