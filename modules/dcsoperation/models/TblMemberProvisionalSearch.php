b904c9e3f49d87b4c19b7bc6bb3856acb8720553
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2026-02-04 11:46:39 +0530

    member provisional ftp upload validate date

--- modules/dcsoperation/models/TblMemberProvisionalSearch.php
+++ modules/dcsoperation/models/TblMemberProvisionalSearch.php
@@ -24,7 +24,8 @@ class TblMemberProvisionalSearch extends TblMemberProvisional {
                 [['member_code', 'is_active', 'payment_mode', 'caste_category_code', 'member_type_code', 'bank_account_no', 'mobile_no', 'created_at', 'gender_code', 'milk_quality_type_code', 'ifsc', 'animal_type_code', 'member_name', 'nominee_name', 'pincode', 'updated_at', 'bank_code', 'branch_code', 'created_by', 'dcs_code', 'district_code', 'federation_code', 'hamlet_code', 'state_code', 'sub_center_code', 'sub_district_code', 'union_code', 'updated_by', 'village_code', 'email', 'is_download', 'download_date_time', 'reference_code', 'union_code', 'plant_code', 'mcc_plant_code', 'bmc_code', 'provisional_from', 'provisional_status', 'remarks', 'land_class', 'bank_name', 'branch_name', 'data_post_id', 'data_post_status', 'picked_datetime', 'resp_status', 'resp_desc', 'approved_at', 'from_date', 'to_date', 'approved_status', 'employee_code', 'employee_name', 'region_code', 'aadhaar_card_address', 'is_contact_verified', 'is_email_verify', 'is_verify', 'email_relation', 'member_identity_no', 'applicant_relation', 'post_office', 'is_aadhar_verify', 'is_operator_aggre', 'application_no', 'witness_name', 'place', 'payment_type', 'recipt_ref_no', 'route_code', 'supervisor_employee_id', 'supervisor_employee_name', 'sap_status'], 'safe'],
                 [['union_code', 'plant_code', 'mcc_plant_code', 'as_on_date'], 'required', 'on' => ['export_search']],
                 [['union_code', 'plant_code', 'mcc_plant_code'], 'required', 'on' => ['bulk_approval']],
-                [['union_code', 'plant_code', 'from_date', 'to_date', 'sap_status'], 'required', 'on' => ['sapFtpUpload']]
+                [['union_code', 'plant_code', 'from_date', 'to_date', 'sap_status'], 'required', 'on' => ['sapFtpUpload']],
+                [['to_date'], 'validateToDate', 'on' => ['sapFtpUpload']],
         ];
     }
 
@@ -207,6 +208,17 @@ class TblMemberProvisionalSearch extends TblMemberProvisional {
             ]);
         }
         return $dataProvider;
+    }
+
+    public function validateToDate($attribute, $params) {
+        if (!empty($this->from_date) && !empty($this->to_date)) {
+            $fDate = date('Y-m-d', strtotime($this->from_date));
+            $tDate = date('Y-m-d', strtotime($this->to_date));
+            if ($tDate < $fDate) {
+                $this->addError($attribute, Yii::t('app/validation', 'To Date must be greater than From Date'));
+                return false;
+            }
+        }
     }
 
 }
