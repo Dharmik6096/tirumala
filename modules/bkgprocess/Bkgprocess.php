0cc82fd95e13e0236ed9f841b9405ad87afa4850
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2026-02-16 17:37:41 +0530

    member provisional ftp upload v8 path issue resolve

--- modules/bkgprocess/Bkgprocess.php
+++ modules/bkgprocess/Bkgprocess.php
@@ -140,7 +140,7 @@ class Bkgprocess extends \yii\base\Module {
                 'param' => 'union_code,plant_code,mcc_plant_code,bmc_code,dcs_code,from_date,to_date,sap_status',
                 'sp_name' => 'member_provisional_sap_ftp_data_upload_mpcrmrd',
                 'export_title' => '',
-                'file_path' => str_replace('\\', '/', realpath(\Yii::$app->basePath . '/../')) . \Yii::$app->params['FTPDirPath'] . 'upload/',
+                'file_path' => str_replace('\\', '/', realpath(\Yii::$app->basePath)) . \Yii::$app->params['FTPDirPath'] . 'upload/',
                 'ftp_path' => '/PRD/ZPIB',
                 'append_ftp_path' => TRUE,
                 'xml_tag' => 'MAF_MT_Sen,Header,item'
