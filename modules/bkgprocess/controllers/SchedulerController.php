f34e5ec265b564e3cc9f68cb494741d558e6ec6b
Atul Patel (IT46/CAPITAL) <it46@EIPL.LOCAL>
2026-03-26 12:20:36 +0530

    bypass unique warning check for crone in create dcs provisional to dcs

--- modules/bkgprocess/controllers/SchedulerController.php
+++ modules/bkgprocess/controllers/SchedulerController.php
@@ -1453,7 +1453,7 @@ class SchedulerController extends ChildController {
                                 $all_doc = [];
                                 $dcsdoc = [];
                                 $msgArr = [];
-                                $processed = ($dcsCtrl->createDcs($model, [$model, $historyModel], $all_doc, $dcsdoc, $msgArr) === 'customRedirect');
+                                $processed = ($dcsCtrl->createDcs($model, [$model, $historyModel], $all_doc, $dcsdoc, $msgArr, true) === 'customRedirect');
                                 if ($processed) {
                                     $baseDir = \Yii::$app->basePath . '/' . \Yii::$app->params['document_upload'];
                                     $dcsDir = $baseDir . 'dcs';
