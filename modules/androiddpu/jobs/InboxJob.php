9c0ec4bbe4e687b0414409ab95403d41f2895b5e
Asmita Chavda (IT36/CAPITAL) <it36@EIPL.LOCAL>
2026-04-16 12:56:06 +0530

    Inbox queue log message changes

--- modules/androiddpu/jobs/InboxJob.php
+++ modules/androiddpu/jobs/InboxJob.php
@@ -26,35 +26,36 @@ class InboxJob extends BaseObject implements JobInterface {
 
                 $generalModel = new GeneralModel();
                 $transaction = $generalModel->saveDeleteTransaction([$model], [], [], ['transactional data', 'create'], true);
-                \Yii::info("AMCS Inbox Queue Fill : " . $this->transaction_data['uuid']);
                 if ($transaction !== 'customRedirect') {
                     $errorData = !empty($transaction) ? (string) $transaction : 'error_occured';
                     if (strstr(strtolower($errorData), 'cannot insert duplicate key')) {
-                        \Yii::info("AMCS Inbox Queue Parse : " . $this->transaction_data['uuid'] . " : " . $errorData);
+                        \Yii::info("AMCS Inbox Queue Parse : Error " . $this->transaction_data['uuid'] . " : " . $errorData);
                         return true;
                     } else {
-                        \Yii::info("AMCS Inbox Queue Parse : Re-processing UUID-" . $this->transaction_data['uuid'] . " : " . $errorData);
+                        \Yii::info("AMCS Inbox Queue Parse Refill : Re-processing UUID-" . $this->transaction_data['uuid'] . " : " . $errorData);
                         $queue->push(new self([
                             'transaction_data' => $this->transaction_data,
                             'sync_timestamp' => $this->sync_timestamp,
                         ]));
                     }
+                } else {
+                    \Yii::info("AMCS Inbox Queue Parse : Success " . $this->transaction_data['uuid']);
                 }
             } else {
-                \Yii::info("AMCS Inbox Queue Fill : uuid missing in InboxJob");
+                \Yii::info("AMCS Inbox Queue Parse Refill : uuid missing");
                 $queue->push(new self([
                     'transaction_data' => $this->transaction_data,
                     'sync_timestamp' => $this->sync_timestamp,
                 ]));
             }
         } catch (\Exception $e) {
-            \Yii::info("AMCS Inbox Queue Fill : Exception in InboxJob: " . $this->transaction_data['uuid'] . " : " . $e->getMessage());
+            \Yii::info("AMCS Inbox Queue Parse Refill : Exception : " . $this->transaction_data['uuid'] . " : " . $e->getMessage());
             $queue->push(new self([
                 'transaction_data' => $this->transaction_data,
                 'sync_timestamp' => $this->sync_timestamp,
             ]));
         } catch (\Throwable $e) {
-            \Yii::info("AMCS Inbox Queue Fill : Exception in InboxJob: " . $this->transaction_data['uuid'] . " : " . $e->getMessage());
+            \Yii::info("AMCS Inbox Queue Parse Refill : Exception : " . $this->transaction_data['uuid'] . " : " . $e->getMessage());
             $queue->push(new self([
                 'transaction_data' => $this->transaction_data,
                 'sync_timestamp' => $this->sync_timestamp,
