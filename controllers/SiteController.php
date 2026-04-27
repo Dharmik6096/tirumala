f3fcdc27d74335753c56c65d07b259eae6f50484
Atul_Patel <atul.patel@yamatech.in>
2026-04-27 17:41:14 +0530

    depend dropdown add all in dropdown list correction

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -498,10 +498,7 @@ class SiteController extends Controller {
                     $out[] = array('id' => $r['id'],
                         'name' => $value);
             }
-            if (\Yii::$app->request->get('addAll') == 1) {
-                if ($out === NULL) {
-                    $out = [];
-                }
+            if (!empty($out) && \Yii::$app->request->get('addAll') == 1) {
                 array_unshift($out, ['id' => '0', 'name' => 'All']);
             }
             return Json::encode(['output' => $out, 'selected' => '']);
