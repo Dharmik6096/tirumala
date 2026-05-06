0cb265e9aaed37a1a23d479639860919eff51825
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2026-05-06 17:31:35 +0530

    weight manual and quality manual add in dcs and customer master

--- modules/organisation/views/tbl-customer-master/view.php
+++ modules/organisation/views/tbl-customer-master/view.php
@@ -15,76 +15,76 @@ $this->title = Yii::$app->label->title('view', 'Customer Master');
         <div class="table-responsive">
             <?php
             $attributes = [
-                [
+                    [
                     'columns' => [
-                        [
+                            [
                             'attribute' => 'customer_code',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                        [
+                            [
                             'attribute' => 'ref_code',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                [
+                    [
                     'columns' => [
-                        [
+                            [
                             'attribute' => 'customer_type',
                             'value' => Yii::$app->general->getforeignkey($model->customerType, 'customer_desc'),
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                        [
+                            [
                             'attribute' => 'customer_code_ex',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                [
+                    [
                     'columns' => [
-                        [
+                            [
                             'attribute' => 'customer_name',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                        [
+                            [
                             'attribute' => 'local_name',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                [
+                    [
                     'columns' => [
-                        [
+                            [
                             'attribute' => 'address',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                        [
+                            [
                             'attribute' => 'local_address',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                [
+                    [
                     'columns' => [
-                        [
+                            [
                             'attribute' => 'gst_no',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                        [
+                            [
                             'attribute' => 'state_code',
                             'value' => Yii::$app->general->getforeignkey($model->stateCode, 'state_name'),
                             'valueColOptions' => ['style' => 'width:30%'],
                         ],
                     ],
                 ],
-                [
+                    [
                     'columns' => [
-                        [
+                            [
                             'attribute' => 'district_code',
                             'value' => Yii::$app->general->getforeignkey($model->districtCode, 'district_name'),
                             'valueColOptions' => ['style' => 'width:30%'],
                         ],
-                        [
+                            [
                             'attribute' => 'sub_district_code',
                             'value' => Yii::$app->general->getforeignkey($model->unionCode, 'union_name'),
                             'value' => isset($model->subDistrictCode) ? $model->subDistrictCode->sub_district_name : '',
@@ -92,101 +92,117 @@ $this->title = Yii::$app->label->title('view', 'Customer Master');
                         ],
                     ],
                 ],
-                [
+                    [
                     'columns' => [
-                        [
+                            [
                             'attribute' => 'village_code',
                             'value' => Yii::$app->general->getforeignkey($model->villageCode, 'village_name'),
                             'valueColOptions' => ['style' => 'width:30%'],
                         ],
-                        [
+                            [
                             'attribute' => 'hamlet_code',
                             'value' => Yii::$app->general->getforeignkey($model->hamletCode, 'hamlet_name'),
                             'valueColOptions' => ['style' => 'width:30%'],
                         ],
                     ],
                 ],
-                [
+                    [
                     'columns' => [
-                        [
+                            [
                             'attribute' => 'route_code',
                             'value' => Yii::$app->general->getforeignkey($model->routeCode, 'route_name'),
                             'valueColOptions' => ['style' => 'width:30%'],
                         ],
-                        [
+                            [
                             'attribute' => 'bmc_code',
                             'value' => Yii::$app->general->getforeignkey($model->bmcCode, 'bmc_name'),
                             'valueColOptions' => ['style' => 'width:30%'],
                         ],
                     ],
                 ],
-                [
+                    [
                     'columns' => [
-                        [
+                            [
                             'attribute' => 'mcc_plant_code',
                             'value' => Yii::$app->general->getforeignkey($model->mccPlantCode, 'name'),
                             'valueColOptions' => ['style' => 'width:30%'],
                         ],
-                        [
+                            [
                             'attribute' => 'plant_code',
                             'value' => Yii::$app->general->getforeignkey($model->plantCode, 'name'),
                             'valueColOptions' => ['style' => 'width:80%']
                         ],
                     ],
                 ],
-                [
+                    [
                     'columns' => [
-                        [
+                            [
                             'attribute' => 'union_code',
                             'value' => Yii::$app->general->getforeignkey($model->unionCode, 'union_name'),
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                        [
+                            [
                             'attribute' => 'sap_vendor_code',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                [
+                    [
                     'columns' => [
-                        [
+                            [
                             'attribute' => 'ts_code_m',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                        [
+                            [
                             'attribute' => 'ts_code_e',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                [
+                    [
                     'columns' => [
-                        [
+                            [
                             'attribute' => 'x_col2',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                        [
+                            [
                             'attribute' => 'route_code',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                [
+                    [
                     'columns' => [
-                        [
+                            [
                             'attribute' => 'animal_type_code',
                             'value' => !empty($model->animalTypeCode) ? $model->animalTypeCode->animal_type_name : '',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
-                        [
+                            [
                             'attribute' => 'distance_from_mcc',
                             'valueColOptions' => ['style' => 'width:30%']
                         ],
                     ],
                 ],
-                [
+                    [
                     'columns' => [
-                        [
+                            [
+                            'attribute' => 'is_weight_manual',
+                            'format' => 'html',
+                            'value' => isset($model->is_weight_manual) ? Yii::$app->dropdown->getRecords('allow_app_login')['data'][$model->is_weight_manual] : '',
+                            'valueColOptions' => ['style' => 'width:30%'],
+                        ],
+                            [
+                            'attribute' => 'is_quality_manual',
+                            'format' => 'html',
+                            'value' => isset($model->is_quality_manual) ? Yii::$app->dropdown->getRecords('allow_app_login')['data'][$model->is_quality_manual] : '',
+                            'valueColOptions' => ['style' => 'width:30%'],
+                        ],
+                    ],
+                ],
+                    [
+                    'columns' => [
+                            [
                             'attribute' => 'pan_no',
                             'valueColOptions' => ['style' => 'width:30%'],
                         ],
