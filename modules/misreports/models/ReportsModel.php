d6363d9e9c6be49d32033c669458754ca3a5c6e4
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2026-05-11 12:57:59 +0530

    from time to time validation add for banas report

--- modules/misreports/models/ReportsModel.php
+++ modules/misreports/models/ReportsModel.php
@@ -32,6 +32,8 @@ class ReportsModel extends Model {
                 [['to_code'], 'default', 'value' => 99999, 'on' => ['LocalSaleReport', 'MilkRateDetailReport', 'DateWiseMilkPurchaseSummary', 'FatWiseQtyAnalysis', 'UnionWiseMessageDetailReport', 'SocietyWiseSummaryReport', 'FarmerNotSubmittingMilkReport']],
                 [['to_soc'], 'default', 'value' => 100, 'on' => ['FarmerListReport', 'MilkRatePublishReport', 'ManualCollectionSummaryReport', 'MilkEditForFarmerReport']],
                 [['from_time', 'to_time'], 'default', 'value' => date('H:i'), 'on' => ['SocietySampleReport']],
+                [['from_time', 'to_time'], 'time', 'format' => 'php:H:i', 'message' => Yii::t('app/validation', 'The format of {attribute} is invalid. eg. 01:30'), 'on' => ['SocietySampleReport']],
+                [['from_time', 'to_time'], 'validateExceedTime', 'params' => ['min' => '00:00', 'max' => '23:59'], 'skipOnError' => true, 'on' => ['SocietySampleReport']],
                 [['from_code', 'from_soc', 'to_code', 'to_soc'], 'number', 'min' => 1, 'when' => function($model) {
                     return $model->member_types != 0;
                 }, 'whenClient' => "function (attribute, value) {
@@ -265,7 +267,7 @@ class ReportsModel extends Model {
                 [['language_code', 'union_code', 'from_code', 'to_code', 'from_date', 'to_date', 'from_shift', 'to_shift'], 'required', 'on' => ['SocietyWiseSummaryReport']],
                 [['language_code', 'union_code', 'from_code', 'to_code', 'from_date', 'to_date', 'show_only_received_data'], 'required', 'on' => ['FarmerNotSubmittingMilkReport']],
                 [['language_code', 'union_code', 'from_soc', 'to_soc', 'from_date', 'to_date', 'from_shift', 'to_shift'], 'required', 'on' => ['ManualCollectionSummaryReport']],
-                [['language_code', 'union_code', 'from_soc', 'to_soc', 'from_date', 'region_code', 'edit_type', 'no_of_farmer_edit', 'no_of_individual_farmer_edit'], 'required', 'on' => ['MilkEditForFarmerReport']],
+                [['language_code', 'union_code', 'from_soc', 'to_soc', 'from_date', 'edit_type', 'no_of_farmer_edit', 'no_of_individual_farmer_edit'], 'required', 'on' => ['MilkEditForFarmerReport']],
                 [['language_code', 'union_code', 'report_app_type', 'registered_type', 'status_type'], 'required', 'on' => ['MuAppVdcsAppUserReport']],
                 [['language_code', 'union_code', 'region_code', 'from_date', 'to_date', 'from_shift', 'to_shift', 'from_time', 'to_time', 'is_show_zero_val', 'is_group_by_society', 'last_rate'], 'required', 'on' => ['SocietySampleReport']],
                 [['to_soc'], 'validateToSoc', 'on' => ['ManualCollectionSummaryReport', 'MilkEditForFarmerReport', 'FarmerListReport', 'MilkRatePublishReport']],
@@ -432,6 +434,33 @@ class ReportsModel extends Model {
             if ($this->from_soc > $this->to_soc) {
                 $this->addError($attribute, Yii::t('app/validation', 'Minimum ' . $this->getAttributeLabel('to_soc') . ' is ' . $this->from_soc . '.'));
                 return false;
+            }
+        }
+    }
+
+    public function validateExceedTime($attribute, $params) {
+        $value = $this->$attribute;
+        $minStr = $params['min'];
+        $maxStr = $params['max'];
+
+        if (!empty($value) && empty($this->getErrors())) {
+            if ($value < $minStr || $value > $maxStr) {
+                $this->addError($attribute, $this->getAttributeLabel($attribute) . " must be between $minStr and $maxStr.");
+                return false;
+            }
+
+            if (!empty($this->from_time) && !empty($this->to_time)) {
+                $from = strtotime($this->from_time);
+                $to = strtotime($this->to_time);
+
+                if ($from >= $to) {
+                    if ($attribute == 'from_time') {
+                        $this->addError($attribute, "From Time must be smaller than To Time.");
+                    } else {
+                        $this->addError($attribute, "To Time must be greater than From Time.");
+                    }
+                    return false;
+                }
             }
         }
     }
