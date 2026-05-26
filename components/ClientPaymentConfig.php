6ac406b66fe37e7b27297ca48a90063a293a4aaa
Asmita Chavda (IT36/CAPITAL) <it36@EIPL.LOCAL>
2026-05-26 13:03:54 +0530

    Add SMC client payment config

--- components/ClientPaymentConfig.php
+++ components/ClientPaymentConfig.php
@@ -480,6 +480,12 @@ class ClientPaymentConfig extends Component {
                     'sp_name' => 'sp_remuneration_payment_srilaxmi',
                 ],
             ],
+            'SMCFOODS' => [
+                'vsp_payment' => [
+                    'param' => 'union_code,from_datetime,from_shift,to_datetime,to_shift,payment_cycle_code,bmc_code,customer_type,process_stop_payment,user_code',
+                    'sp_name' => 'sp_vsp_payment_smc',
+                ],
+            ],
         ];
     }
 
