019f468db05303fb664367c799327ffb21cba475
Atul Patel (IT46/CAPITAL) <it46@EIPL.LOCAL>
2026-05-28 11:22:09 +0530

    complain dashboard php82 related changes

--- views/site/widget_dashboard_complain_summary_dashboard.php
+++ views/site/widget_dashboard_complain_summary_dashboard.php
@@ -50,7 +50,7 @@ use yii\helpers\Url;
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="complainSummaryModalLabel">Complain Summary</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
@@ -58,7 +58,7 @@ use yii\helpers\Url;
                     <!-- Table content will be injected here -->
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
