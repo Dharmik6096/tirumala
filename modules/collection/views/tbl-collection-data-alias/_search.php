b5bda5d5fc55cd384e63241a8485b6b3f4bfdae1
Atul Patel (IT46/CAPITAL) <it46@EIPL.LOCAL>
2026-06-12 17:51:30 +0530

    design issue in php82 fixed

--- modules/collection/views/tbl-collection-data-alias/_search.php
+++ modules/collection/views/tbl-collection-data-alias/_search.php
@@ -49,7 +49,7 @@ use yii\web\View;
         </div>  
     <?php } ?>
     <!-- <div class="clearfix"></div> -->
-    <div class="col-sm-2">
+    <div class="col-sm-2 mb-2">
         <?= Yii::$app->controls->date($model, $form, 'from_date', 'form-group col-sm-2 padding-left-5 padding-right-5', false, false, false, TRUE); ?>
     </div>
     <div class="col-sm-2 shift">
