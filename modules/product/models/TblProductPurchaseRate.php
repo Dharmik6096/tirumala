4339c0cde19c197a38817fd7de486007cc02b873
Seema Tank (IT22/CAPITAL) <it22@EIPL.LOCAL>
2026-06-19 15:10:28 +0530

    product purchase rate data not parse issue resolve

--- modules/product/models/TblProductPurchaseRate.php
+++ modules/product/models/TblProductPurchaseRate.php
@@ -46,6 +46,7 @@ class TblProductPurchaseRate extends \app\models\ChildModel {
      */
     public function rules() {
         return [
+            [['product_code'], 'required', 'on' => ['androidsync']],
             [['product_code', 'purchase_rate', 'wef_date', 'union_code'], 'required'],
             [['purchase_rate'], 'number', 'min' => 0, 'message' => Yii::t('app/validation', '{attribute} must be a digit.e.g."10"')],
             [['product_code'], 'exist', 'skipOnError' => true, 'targetClass' => TblProduct::className(), 'targetAttribute' => ['product_code' => 'product_code']],
@@ -58,134 +59,133 @@ class TblProductPurchaseRate extends \app\models\ChildModel {
             [['wef_date'], 'validateDate', 'on' => ['importCsv']],
             [['wef_date', 'created_at', 'updated_at'], 'safe'],
             [['product_purchase_rate_code'], 'validateProductPurchaseRate', 'skipOnEmpty' => false],
-            ['wef_date', 'unique', 'message' => Yii::t('app/validation', 'Rate is already taken on this WEF Date.'), 'when' => function($model) {
-                    $data = $this->find()->where(['union_code' => $model->union_code, 'product_code' => $model->product_code, 'wef_date' => Yii::$app->formatter->asDate($this->wef_date, DATE_FORMAT)])->andWhere(['<>', 'product_purchase_rate_code', $model->product_purchase_rate_code])->count();
+            ['wef_date', 'unique', 'message' => Yii::t('app/validation', 'Rate is already taken on this WEF Date.'), 'when' => function ($model) {
+                    $data = $this->find()->where(['union_code' => $model->union_code, 'product_code' => $model->product_code, 'wef_date' => Yii::$app->formatter->asDate($this->wef_date, 'php:Y-m-d')])->andWhere(['<>', 'product_purchase_rate_code', $model->product_purchase_rate_code])->count();
                     return ($data == 1) ? true : false;
                 }],
-                    [['originating_type'], 'integer'],
-                ];
-            }
+            [['originating_type'], 'integer'],
+        ];
+    }
 
-            /**
-             * @inheritdoc
-             */
-            public function attributeLabels() {
-                return [
-                    'product_purchase_rate_code' => Yii::t('app', 'Product Purchase Rate Code'),
-                    'product_code' => Yii::t('app', 'Product'),
-                    'purchase_rate' => Yii::t('app', 'Rate'),
-                    'wef_date' => Yii::t('app', 'WEF Date'),
-                    'union_code' => Yii::t('app', 'Union'),
-                    'plant_code' => Yii::t('app', 'Plant'),
-                    'mcc_plant_code' => Yii::t('app', 'MCC'),
-                    'bmc_code' => Yii::t('app', 'BMC'),
-                    'dcs_code' => Yii::t('app', 'DCS'),
-                    'created_at' => Yii::t('app', 'Created At'),
-                    'created_by' => Yii::t('app', 'Created By'),
-                    'updated_at' => Yii::t('app', 'Updated At'),
-                    'updated_by' => Yii::t('app', 'Updated By'),
-                    'originating_org_code' => Yii::t('app', 'Originating Org Code'),
-                    'originating_org_type' => Yii::t('app', 'Originating Org Type'),
-                    'originating_type' => Yii::t('app', 'Originating Type'),
-                    'x_col1' => Yii::t('app', 'X Col1'),
-                    'x_col2' => Yii::t('app', 'X Col2'),
-                    'x_col3' => Yii::t('app', 'X Col3'),
-                    'x_col4' => Yii::t('app', 'X Col4'),
-                    'x_col5' => Yii::t('app', 'X Col5'),
-                ];
-            }
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels() {
+        return [
+            'product_purchase_rate_code' => Yii::t('app', 'Product Purchase Rate Code'),
+            'product_code' => Yii::t('app', 'Product'),
+            'purchase_rate' => Yii::t('app', 'Rate'),
+            'wef_date' => Yii::t('app', 'WEF Date'),
+            'union_code' => Yii::t('app', 'Union'),
+            'plant_code' => Yii::t('app', 'Plant'),
+            'mcc_plant_code' => Yii::t('app', 'MCC'),
+            'bmc_code' => Yii::t('app', 'BMC'),
+            'dcs_code' => Yii::t('app', 'DCS'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'created_by' => Yii::t('app', 'Created By'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+            'updated_by' => Yii::t('app', 'Updated By'),
+            'originating_org_code' => Yii::t('app', 'Originating Org Code'),
+            'originating_org_type' => Yii::t('app', 'Originating Org Type'),
+            'originating_type' => Yii::t('app', 'Originating Type'),
+            'x_col1' => Yii::t('app', 'X Col1'),
+            'x_col2' => Yii::t('app', 'X Col2'),
+            'x_col3' => Yii::t('app', 'X Col3'),
+            'x_col4' => Yii::t('app', 'X Col4'),
+            'x_col5' => Yii::t('app', 'X Col5'),
+        ];
+    }
 
-            /**
-             * @return \yii\db\ActiveQuery
-             */
-            public function validateProductPurchaseRate($attribute, $params) {
-                $this->product_purchase_rate_code = Yii::$app->general->getPrimaryCode($this);
-            }
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function validateProductPurchaseRate($attribute, $params) {
+        $this->product_purchase_rate_code = Yii::$app->general->getPrimaryCode($this);
+    }
 
-            public function getProductCode() {
-                return $this->hasOne(TblProduct::className(), ['product_code' => 'product_code']);
-            }
+    public function getProductCode() {
+        return $this->hasOne(TblProduct::className(), ['product_code' => 'product_code']);
+    }
 
-            public function getProductPurchaseRateAppCode() {
-                return $this->hasMany(TblProductPurchaseRateApplicability::className(), ['product_purchase_rate_code' => 'product_purchase_rate_code']);
-            }
+    public function getProductPurchaseRateAppCode() {
+        return $this->hasMany(TblProductPurchaseRateApplicability::className(), ['product_purchase_rate_code' => 'product_purchase_rate_code']);
+    }
 
-            /**
-             * @return \yii\db\ActiveQuery
-             */
-            public function getUnionCode() {
-                return $this->hasOne(TblUnions::className(), ['union_code' => 'union_code']);
-            }
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUnionCode() {
+        return $this->hasOne(TblUnions::className(), ['union_code' => 'union_code']);
+    }
 
-            /**
-             * @return \yii\db\ActiveQuery
-             */
-            public function getDcsCode() {
-                return $this->hasOne(TblDcs::className(), ['dcs_code' => 'dcs_code']);
-            }
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDcsCode() {
+        return $this->hasOne(TblDcs::className(), ['dcs_code' => 'dcs_code']);
+    }
 
-            public function getMinDate() {
+    public function getMinDate() {
 
-                $query = $this->find()->select('wef_date')->where(['product_code' => $this->product_code, 'union_code' => $this->union_code]);
-                if (!empty($this->product_purchase_rate_code)) {
-                    $query->andWhere(['<>', 'product_purchase_rate_code', $this->product_purchase_rate_code]);
-                }
-                $date = $query->orderBy(['wef_date' => SORT_DESC])->one();
-                return $date ? Yii::$app->controls->view_date($date->wef_date) : date('d-m-Y');
-            }
-
-            public function disableProduct() {
-
-                $mdate = $this->getMinDate();
-                $dis = ($mdate < Yii::$app->controls->view_date($this->wef_date) || $mdate == date('d-m-Y')) ? true : false;
-                return ($dis) ? TRUE : FALSE;
-            }
-
-            public function productList() {
-                $rate_data = $this->find()
-                        ->where(['<=', 'wef_date', date('Y-m-d')])
-                        ->all();
-                $product = [];
-                $i = 0;
-                foreach ($rate_data as $rate_code) {
-                    $product[$i]['product_code'] = $rate_code['product_code'];
-                    $product[$i]['product_name'] = Yii::$app->general->getforeignkey($rate_code->productCode, 'product_name');
-                    $product[$i]['product_rate'] = $rate_code['purchase_rate'];
-                    $i++;
-                }
-                return $product;
-            }
-
-            public function disableDelete() {
-                $app = TblProductPurchaseRateApplicability::find()->where(['product_purchase_rate_code' => $this->product_purchase_rate_code])->one();
-                if (!empty($app))
-                    return false;
-                else
-                    return true;
-            }
-
-            public function convertDateDot() {
-                try {
-                    $this->wef_date = Yii::$app->controls->view_date($this->wef_date, 'php:d.m.Y');
-                } catch (\Exception $e) {
-                    $this->wef_date = '-';
-                }
-            }
-
-            public function convertDate() {
-                if (empty($this->getErrors())) {
-                    $this->wef_date = !empty($this->wef_date) ? Yii::$app->controls->view_date($this->wef_date, 'php:Y-m-d') : NULL;
-                }
-            }
-
-            public function validateDate($attribute, $params) {
-                if (empty($this->getErrors())) {
-                    if ($this->wef_date < date('Y-m-d')) {
-                        $this->addError($attribute, Yii::t('app/validation', 'Wef Date Must Not Allow Past Date'));
-                        return false;
-                    }
-                }
-            }
-
+        $query = $this->find()->select('wef_date')->where(['product_code' => $this->product_code, 'union_code' => $this->union_code]);
+        if (!empty($this->product_purchase_rate_code)) {
+            $query->andWhere(['<>', 'product_purchase_rate_code', $this->product_purchase_rate_code]);
         }
-        
+        $date = $query->orderBy(['wef_date' => SORT_DESC])->one();
+        return $date ? Yii::$app->controls->view_date($date->wef_date) : date('d-m-Y');
+    }
+
+    public function disableProduct() {
+
+        $mdate = $this->getMinDate();
+        $dis = ($mdate < Yii::$app->controls->view_date($this->wef_date) || $mdate == date('d-m-Y')) ? true : false;
+        return ($dis) ? TRUE : FALSE;
+    }
+
+    public function productList() {
+        $rate_data = $this->find()
+                ->where(['<=', 'wef_date', date('Y-m-d')])
+                ->all();
+        $product = [];
+        $i = 0;
+        foreach ($rate_data as $rate_code) {
+            $product[$i]['product_code'] = $rate_code['product_code'];
+            $product[$i]['product_name'] = Yii::$app->general->getforeignkey($rate_code->productCode, 'product_name');
+            $product[$i]['product_rate'] = $rate_code['purchase_rate'];
+            $i++;
+        }
+        return $product;
+    }
+
+    public function disableDelete() {
+        $app = TblProductPurchaseRateApplicability::find()->where(['product_purchase_rate_code' => $this->product_purchase_rate_code])->one();
+        if (!empty($app))
+            return false;
+        else
+            return true;
+    }
+
+    public function convertDateDot() {
+        try {
+            $this->wef_date = Yii::$app->controls->view_date($this->wef_date, 'php:d.m.Y');
+        } catch (\Exception $e) {
+            $this->wef_date = '-';
+        }
+    }
+
+    public function convertDate() {
+        if (empty($this->getErrors())) {
+            $this->wef_date = !empty($this->wef_date) ? Yii::$app->controls->view_date($this->wef_date, 'php:Y-m-d') : NULL;
+        }
+    }
+
+    public function validateDate($attribute, $params) {
+        if (empty($this->getErrors())) {
+            if ($this->wef_date < date('Y-m-d')) {
+                $this->addError($attribute, Yii::t('app/validation', 'Wef Date Must Not Allow Past Date'));
+                return false;
+            }
+        }
+    }
+
+}
